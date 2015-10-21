f96a9cc52362b9450dd9673178a6348a0fefecda
sebastian <sebmuell@phys.ethz.ch>
2015-10-21 15:15:04 +0200

    removing noise

--- test/ToolTest.cpp
+++ test/ToolTest.cpp
@@ -1,44 +1,9 @@
-#include <iostream> 
-#include <string>
-#include <math.h>
-
 #include "gtest/gtest.h"
 #include "Tools/StringTools.h"
 #include "Tools/FileTools.h"
 #include "Tools/StringTools.h"
 
-using namespace std;
-
-// The fixture for testing class Foo.
-class Tools : public ::testing::Test {
- protected:
-  // You can remove any or all of the following functions if its body
-  // is empty.
-
-	
-  Tools() {
-    // You can do set-up work for each test here.
-  }
-
-  virtual ~Tools() {
-    // You can do clean-up work that doesn't throw exceptions here.
-  }
-
-  // If the constructor and destructor are not enough for setting up
-  // and cleaning up each test, you can define the following methods:
-
-  virtual void SetUp() {
-    // Code here will be called immediately after the constructor (right
-    // before each test).
-  }
-
-  virtual void TearDown() {
-    // Code here will be called immediately after each test (right
-    // before the destructor).
-  }
-
-  // Objects declared here can be used by all tests in the test case for Foo.
-};
+class Tools : public ::testing::Test {};
 //----------------------------------------------------------------------
 TEST_F(Tools, Deg2Rad_Rad2Deg) {
   for(int i=-721; i<721; i++){
