eb61dccbabc67dc68cdce26ed7ea8c5a67830d42
Dominik Neise <dominik.neise@udo.edu>
2015-10-21 08:14:47 +0200

    forgot to commit adding EventIoTest.cpp to test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -35,6 +35,7 @@
 #include "Func1DSpeedTest.cpp"
 #include "PhotonBunchTest.cpp"
 #include "PostInitFrameSpeed.cpp"
+#include "EventIoTest.cpp"
 
 int main(int argc, char **argv) {
 	UserInteraction::print_welcome_screen();
