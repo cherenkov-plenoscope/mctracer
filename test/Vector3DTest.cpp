d1c6925f019a061fbe0b2f350aa6ef2f317b33e0
sebastian <sebmuell@phys.ethz.ch>
2015-10-21 13:21:17 +0200

    removed unused stuff

--- test/Vector3DTest.cpp
+++ test/Vector3DTest.cpp
@@ -6,40 +6,7 @@
 #include "Core/Vector3D.h"
 #include "Tools/Tools.h"
 
-// The fixture for testing class Foo.
-class Vector3DTest : public ::testing::Test {
- protected:
-  // You can remove any or all of the following functions if its body
-  // is empty.
-
-	Vector3D RandVec1;
-	Vector3D RandVec2;	
-	
-  Vector3DTest() {
-    // You can do set-up work for each test here.
-  }
-
-  virtual ~Vector3DTest() {
-    // You can do clean-up work that doesn't throw exceptions here.
-  }
-
-  // If the constructor and destructor are not enough for setting up
-  // and cleaning up each test, you can define the following methods:
-
-  virtual void SetUp() {
-    // Code here will be called immediately after the constructor (right
-    // before each test).
-    RandVec1.set((rand()-.5),(rand()-.5),(rand()-.5));
-    RandVec2.set((rand()-.5),(rand()-.5),(rand()-.5));
-  }
-
-  virtual void TearDown() {
-    // Code here will be called immediately after each test (right
-    // before the destructor).
-  }
-
-  // Objects declared here can be used by all tests in the test case for Foo.
-};
+class Vector3DTest : public ::testing::Test {};
 //----------------------------------------------------------------------
 TEST_F(Vector3DTest, NullVector) {
   Vector3D a = Vector3D::null;
