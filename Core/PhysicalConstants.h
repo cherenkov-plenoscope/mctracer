9bb92057bce18362c0ca523b62795b587a197249
sebastian <sebmuell@phys.ethz.ch>
2015-10-31 16:27:06 +0100

    si units are common in mctracer, does not need to state it

---
+++ Core/PhysicalConstants.h
@@ -0,0 +1,11 @@
+//=================================
+// include guard
+#ifndef __PhysicalConstants_H_INCLUDED__
+#define __PhysicalConstants_H_INCLUDED__
+
+
+namespace PhysicalConstants{
+	static const double speed_of_light_in_vacuum = 299792458.0;
+}
+
+#endif // __PhysicalConstants_H_INCLUDED__
