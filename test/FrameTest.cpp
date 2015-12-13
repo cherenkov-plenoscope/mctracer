3cc7ef04fea0c883783ecad6cdaf1240dd510f70
sebastian <sebmuell@phys.ethz.ch>
2015-12-13 23:23:57 +0100

    Actually this artefact is used but was never used. However no we need it to have volume objects with an inner and outer volume property as refraction.
    Revert "removed unused artefact"
    
    This reverts commit 68c74a7ffcb692bb5325989ab1e1a50669250c75.

--- test/FrameTest.cpp
+++ test/FrameTest.cpp
@@ -10,7 +10,75 @@
 
 using namespace std;
 
-class CartesianFrameTest : public ::testing::Test {};
+// The fixture for testing class Foo.
+class CartesianFrameTest : public ::testing::Test {
+ protected:
+  // You can remove any or all of the following functions if its body
+  // is empty.
+	
+  CartesianFrameTest() {
+    // You can do set-up work for each test here.
+  }
+
+  virtual ~CartesianFrameTest() {
+    // You can do clean-up work that doesn't throw exceptions here.
+  }
+
+  // If the constructor and destructor are not enough for setting up
+  // and cleaning up each test, you can define the following methods:
+
+  virtual void SetUp() {
+    // Code here will be called immediately after the constructor (right
+    // before each test).
+  }
+
+  virtual void TearDown() {
+    // Code here will be called immediately after each test (right
+    // before the destructor).
+  }
+
+  // Objects declared here can be used by all tests in the test case for Foo.
+};
+//------------------------------------------------------------------------------
+/*TEST_F(CartesianFrameTest, find_specific_frame) {
+  
+  // load some world
+  string xml_file = "./test_scenery/including_other_xml_files.xml";
+  WorldFactory file2world;
+  file2world.load(xml_file);
+  Frame *world = file2world.world();
+
+  // Now get the pointer to the frame we are looking for
+  // In the case of "tree/pole" we know that it must be in the test xml file.
+  const Frame* SpecificFrameInWorldWeAreLookingFor = 
+  world->get_frame_in_tree_by_path( "/tree/pole" );
+
+  EXPECT_EQ("pole", SpecificFrameInWorldWeAreLookingFor->get_name_of_frame());
+
+  // Lets do a second test with one additional layer
+  SpecificFrameInWorldWeAreLookingFor = world->
+  get_frame_in_tree_by_path( "/house/chimney/chimney_wall_4" );
+
+  if(SpecificFrameInWorldWeAreLookingFor == NULL)
+    std::cout << "OMG the Pointer is NULL!!!\n";
+
+  EXPECT_EQ(
+    "chimney_wall_4", 
+    SpecificFrameInWorldWeAreLookingFor->get_name_of_frame()
+  );
+  // In addition we test if the path is restored correctly by the frame
+  std::string RestoredPath = 
+  SpecificFrameInWorldWeAreLookingFor->get_path_in_tree_of_frames();
+
+  EXPECT_EQ( "/house/chimney/chimney_wall_4" , RestoredPath );
+
+  // Now lets look something up which is not in our test xml file.
+  // In this case the void_frame is returned
+  SpecificFrameInWorldWeAreLookingFor = 
+  world->get_frame_in_tree_by_path( "your/Mama" );
+
+  EXPECT_EQ( Frame::void_frame, SpecificFrameInWorldWeAreLookingFor );
+}*/
 //------------------------------------------------------------------------------
 TEST_F(CartesianFrameTest, assert_name_is_valid) {
 
@@ -65,10 +133,68 @@ TEST_F(CartesianFrameTest, set_frame) {
   EXPECT_EQ(T_frame2mother, *Peter.frame2mother());
 }
 //------------------------------------------------------------------------------
+TEST_F(CartesianFrameTest, root_of_world_on_complete_tree) {
+
+  //-----define frames
+  Frame tree;
+  tree.set_name_pos_rot("tree" ,Vector3D::null, Rotation3D::null);
+
+  Frame leaf1;
+  leaf1.set_name_pos_rot("leaf1" ,Vector3D(1.0,0.0,0.0), Rotation3D::null);
+
+  Frame leaf2;
+  leaf2.set_name_pos_rot("leaf2" ,Vector3D(-1.0,0.0,0.0), Rotation3D::null);
+
+  Frame branch;
+  branch.set_name_pos_rot("branch" ,Vector3D(0.0,0.0,1.0), Rotation3D::null);
+
+  Frame leaf1_on_branch;
+  leaf1_on_branch.set_name_pos_rot(
+    "leaf1_on_branch",
+    Vector3D(1.0,0.0,0.0),
+    Rotation3D::null
+  );
+
+  Frame leaf2_on_branch;
+  leaf2_on_branch.set_name_pos_rot(
+    "leaf2_on_branch",
+    Vector3D(0.0,1.0,0.0),
+    Rotation3D::null
+  );
+
+  //-----declare relationschips
+  branch.set_mother_and_child(&leaf1_on_branch);
+  branch.set_mother_and_child(&leaf2_on_branch);
+
+  tree.set_mother_and_child(&leaf1);
+  tree.set_mother_and_child(&leaf2);
+
+  tree.set_mother_and_child(&branch);
+
+  //-----post initialize 
+  tree.init_tree_based_on_mother_child_relations();
+
+  //-----test
+  EXPECT_EQ(&tree, tree.get_root_of_world());
+  EXPECT_EQ(&tree, leaf1.get_root_of_world());
+  EXPECT_EQ(&tree, leaf2.get_root_of_world());
+  EXPECT_EQ(&tree, branch.get_root_of_world());
+  EXPECT_EQ(&tree, leaf1_on_branch.get_root_of_world());
+  EXPECT_EQ(&tree, leaf2_on_branch.get_root_of_world());
+}
+//------------------------------------------------------------------------------
+TEST_F(CartesianFrameTest, root_of_world_default) {
+
+  Frame tree;
+  tree.set_name_pos_rot("tree" ,Vector3D::null, Rotation3D::null);
+  EXPECT_EQ(&tree, tree.get_root_of_world());
+}
+//------------------------------------------------------------------------------
 TEST_F(CartesianFrameTest, cluster_frames_during_tree_initializing) {
 
     Frame tree;
     tree.set_name_pos_rot("tree" ,Vector3D::null, Rotation3D::null);
+    EXPECT_EQ(&tree, tree.get_root_of_world());
 
     double qube_edge = 10.0;
 
