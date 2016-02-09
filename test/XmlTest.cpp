35614b3bc19e7cfee042368f6df659c53c1e3208
sebastian <sebmuell@phys.ethz.ch>
2016-02-09 21:51:35 +0100

    xml test files in own folder, no cout but testing

--- test/XmlTest.cpp
+++ test/XmlTest.cpp
@@ -4,23 +4,29 @@ class XmlTest : public ::testing::Test {};
 //------------------------------------------------------------------------------
 TEST_F(XmlTest, open_not_existing_file) {
 	
-	const std::string path = "test_scenery/nein_nein.xml";	
+	const std::string path = "xml/nein_nein.xml";	
 	EXPECT_THROW(Xml::Document doc(path), Xml::IoError);
 }
 //------------------------------------------------------------------------------
-TEST_F(XmlTest, path_name) {
+TEST_F(XmlTest, loop_over_children) {
 
-	const std::string path = "test_scenery/loop_over_children.xml";
+	const std::string path = "xml/loop_over_children.xml";
 
 	Xml::Document doc(path);
 	Xml::Node tree = doc.get_tree();
 
+	std::vector<std::string> children_names = {
+		"child1", "child2", "child3", "dude"
+	};
+
+	int i=0;
 	for(
 		Xml::Node child = tree.get_first_child(); 
 		child; 
 		child = child.get_next_child()
-	)
-		std::cout << child.get_name() << "\n";
+	){
+		EXPECT_EQ(children_names.at(i++), child.get_name());
+	}
 }
 //------------------------------------------------------------------------------
 TEST_F(XmlTest, minimal_node) {
