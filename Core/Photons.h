f314bcf983115070d2c287f86af0664923e5eba3
sebastian <sebmuell@phys.ethz.ch>
2016-04-04 17:01:10 +0200

    less std::noise also in header

--- Core/Photons.h
+++ Core/Photons.h
@@ -11,58 +11,50 @@
 #include <iostream>
 #include "Photon.h"
 #include <vector>
+using std::string;
+using std::stringstream;
+using std::vector;
 
 //=================================
 namespace Photons {
 
-	std::string get_print(
-		const std::vector<Photon*> *photons
-	);
+	string get_print(const vector<Photon*> *photons);
 	// propagation
 	void propagate_photons_in_world_with_settings(
-		std::vector<Photon*> *photons,
+		vector<Photon*> *photons,
 		const Frame *world, 
 		const TracerSettings *settings,
 		Random::Generator* prng
 	);	
 
 	void propagate_photons_using_single_thread(
-		std::vector<Photon*> *photons,
+		vector<Photon*> *photons,
 		const Frame* world, 
 		const TracerSettings* settings,
 		Random::Generator* prng
 	);
 
 	void propagate_photons_using_multi_thread(
-		std::vector<Photon*> *photons,
+		vector<Photon*> *photons,
 		const Frame* world, 
 		const TracerSettings* settings
 	);
 
 	// In Out to raw matrix vec<vec<d>>
 	// Can be given to AsciiIO to write/read to/from text file
-	std::vector<Photon*> *raw_matrix2photons(
-		std::vector<std::vector<double> > raw_matrix
-	);
-
-	std::vector<std::vector<double> > photons2raw_matrix(
-		std::vector<Photon*> *photons
-	);
-
-	std::vector<double> photon2raw_row(Photon* ph);
-
-	Photon* raw_row2photon(std::vector<double> &raw_row);
-	
-	void assert_raw_row_size_matches_photon(std::vector<double> &raw_row);
-
+	vector<Photon*> *raw_matrix2photons(vector<vector<double> > raw_matrix);
+	vector<vector<double> > photons2raw_matrix(vector<Photon*> *photons);
+	vector<double> photon2raw_row(Photon* ph);
+	Photon* raw_row2photon(vector<double> &raw_row);
+	void assert_raw_row_size_matches_photon(vector<double> &raw_row);
 	// Trajectories
 	class Trajectories {
 		uint number_of_trajectories_handed_out_already;
-		std::vector<Photon*> *photons;
+		vector<Photon*> *photons;
 		const TracerSettings *settings;
 	public:
 		Trajectories(
-			std::vector<Photon*> *_photon_bunch, 
+			vector<Photon*> *_photon_bunch, 
 			const TracerSettings *settings
 		);
 		bool has_still_trajectories_left()const;
@@ -72,29 +64,25 @@ namespace Photons {
 	// creation of photons
 	namespace Source {
 
-		std::vector<Photon*> *parallel_towards_z_from_xy_disc(
+		vector<Photon*> *parallel_towards_z_from_xy_disc(
 			const double disc_radius,
 			const uint number_of_photons
 		);
 
-		std::vector<Photon*> *point_like_towards_z_opening_angle_num_photons(
+		vector<Photon*> *point_like_towards_z_opening_angle_num_photons(
 			const double opening_angle,
 			const uint number_of_photons 
 		);
 	}
 
-	void transform_all_photons(
-		const HomoTrafo3D Trafo, 
-		std::vector<Photon*> *photons
-	);
-
+	void transform_all_photons(const HomoTrafo3D Trafo, vector<Photon*> *photons);
 	void transform_all_photons_multi_thread(
 		const HomoTrafo3D Trafo, 
-		std::vector<Photon*> *photons
+		vector<Photon*> *photons
 	);
 
 	// delete all history
-	void delete_photons_and_history(std::vector<Photon*> *photons);
+	void delete_photons_and_history(vector<Photon*> *photons);
 }
 
 #endif // __PHOTONBUNCH_H_INCLUDED__
