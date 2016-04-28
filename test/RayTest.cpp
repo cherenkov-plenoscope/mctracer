dfe869ea158ec43216594ebde4e87d34cb1b0070
sebastian <sebmuell@phys.ethz.ch>
2016-04-28 21:52:42 +0200

    tidy up ray test

--- test/RayTest.cpp
+++ test/RayTest.cpp
@@ -2,58 +2,40 @@
 #include "Core/Ray.h"
 #include "Geometry/Sphere.h"
 
-using namespace std;
-
-class RayTest : public ::testing::Test {
- protected:
-
-  Vec3 support;
-  Vec3 direction;
-
-  RayTest() {
-    support.set(0.0,0.0,1.0);
-    direction.set(0.0,0.0,1.0);
-  }
-
-  virtual ~RayTest() {
-  }
-
-  virtual void SetUp() {
-  }
-
-  virtual void TearDown() {
-  }
-};
+class RayTest : public ::testing::Test {};
 //------------------------------------------------------------------------------
 TEST_F(RayTest, Constructor_using_vectors) {
 
-  Ray my_ray(support, direction);
-
-  EXPECT_EQ(support, my_ray.get_support());
-  EXPECT_EQ(direction/direction.norm(), my_ray.get_direction()); 
-  EXPECT_EQ(1.0, my_ray.get_direction().norm());
+  const Vec3 sup(1.2, 3.4, 5.6);
+  const Vec3 dir(7.8, 9.0, 4.2);
+  const Ray ray(sup, dir);
+  EXPECT_EQ(sup, ray.get_support());
+  EXPECT_EQ(dir/dir.norm(), ray.get_direction()); 
+  EXPECT_NEAR(1.0, ray.get_direction().norm(), 1e-6);
 }
 //------------------------------------------------------------------------------
 TEST_F(RayTest, position_on_ray) {
-  Ray my_ray(support, direction);
 
-  double ray_parameter = 5.0;
-  Vec3 pos_on_ray = support + direction*ray_parameter;
-    
-  EXPECT_EQ(pos_on_ray, my_ray.get_pos_at(ray_parameter));
+  const Vec3 sup(1.2, 3.4, 5.6);
+  const Vec3 dir(7.0, 9.0, 4.0);
+  const Ray ray(sup, dir);
+  const double alpha = 5.0;
+  const Vec3 pos_on_ray = sup+dir/dir.norm()*alpha;
+  EXPECT_NEAR(0.0, (pos_on_ray - ray.get_pos_at(alpha) ).norm(), 1e-6);
 }
 //------------------------------------------------------------------------------
 TEST_F(RayTest, closest_distance_to_point) {
-  Ray my_ray(Vec3::null, Vec3(0.0, 0.0, 1.0));
 
-  EXPECT_EQ(0.0, my_ray.get_closest_distance_to_point(Vec3::null));
-  EXPECT_EQ(1.0, my_ray.get_closest_distance_to_point(Vec3(1.0, 0.0, 0.0)));
-  EXPECT_EQ(1.0, my_ray.get_closest_distance_to_point(Vec3(0.0, 1.0, 0.0)));
-  EXPECT_EQ(0.0, my_ray.get_closest_distance_to_point(Vec3(0.0, 0.0, 1.0)));
+  Ray ray(Vec3::null, Vec3(0.0, 0.0, 1.0));
+  EXPECT_EQ(0.0, ray.get_closest_distance_to_point(Vec3::null));
+  EXPECT_EQ(1.0, ray.get_closest_distance_to_point(Vec3(1.0, 0.0, 0.0)));
+  EXPECT_EQ(1.0, ray.get_closest_distance_to_point(Vec3(0.0, 1.0, 0.0)));
+  EXPECT_EQ(0.0, ray.get_closest_distance_to_point(Vec3(0.0, 0.0, 1.0)));
 }
 //------------------------------------------------------------------------------
 TEST_F(RayTest, set_direction) {
-  Ray my_ray(support, direction*42.0);
-  my_ray.set_direction(Vec3(1.2, 3.4, 5.6));
-  EXPECT_EQ(1.0, my_ray.get_direction().norm());
+  
+  Ray ray(Vec3(1.2, 3.4, 5.6), Vec3(0, 0, 1));
+  ray.set_direction(Vec3(1.2, 3.4, 5.6));
+  EXPECT_EQ(1.0, ray.get_direction().norm());
 }
