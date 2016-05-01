ef608f7e1c57728276e768f3018ef1d9a7408607
sebastian <sebmuell@phys.ethz.ch>
2016-05-01 13:22:21 +0200

    kick obsolete stuff

--- Core/RayForPropagation.h
+++ Core/RayForPropagation.h
@@ -47,62 +47,40 @@ protected:
 	const Intersection* intersection = Intersection::void_intersection;
 
 public:
+	
 	RayForPropagation(const RayForPropagation* ray_to_be_carried_on);
-
 	RayForPropagation(const Vec3 support, const Vec3 direction);
-
 	virtual ~RayForPropagation(){};
-
 	void set_id(const uint nID);
-
 	uint get_id()const;
-
 	void push_back_intersection_and_type_to_propagation_history(
 		const Intersection* interact, 
 		const InteractionType type
 	);
-
 	string get_print()const;
-
 	double get_accumulative_distance()const;
-  	
 	virtual void propagate_in(PropagationEnvironment env);
-
 	uint get_number_of_interactions_so_far()const;
-
 	const Intersection* get_intersection_at(const uint index)const;
-
 	InteractionType get_final_interaction_type()const {
 		return interaction_type_history->back();
 	};
-	
 	const Intersection* get_final_intersection()const;
-
 	virtual double get_time_of_flight()const;
-	
 	virtual void delete_history();
-
 	void transform(const HomTra3 *T);
-	
 	Vec3 get_final_intersection_incident_vector_in_object_frame()const;
 protected:
+
 	string get_history_print()const;
-
 	RayForPropagation();
-
 	void init_propagation_history();
-
 	void carry_on_propagation_properties_of_ray(
 		const RayForPropagation* ray_to_be_carried_on
 	);
-
 	std::string get_type_print(const InteractionType type)const;
-
 private:
-	void push_back_production_of_ray();
 
-	static const Color* trajectory_col;
-	static const Color* absorption_in_void_col;
-	static const Color* interaction_col;
+	void push_back_production_of_ray();
 };
 #endif // __RayForPropagation_H_INCLUDED__ 
