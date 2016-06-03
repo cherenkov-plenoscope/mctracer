a48a28db4a2eaa02c226445ec3b8fb6a931498a0
sebastian <sebmuell@phys.ethz.ch>
2016-06-04 00:57:24 +0200

    show size of more mctracer classes

--- Benchmarks/PrintSizeOfTest.cpp
+++ Benchmarks/PrintSizeOfTest.cpp
@@ -7,8 +7,12 @@
 #include "Cameras/CameraRay.h"
 #include "Core/Color.h"
 #include "Core/HomTra3.h"
+#include "Core/SurfaceEntity.h"
+#include "Geometry/Geometry.h"
 #include "Core/PropagationEnvironment.h"
+#include "Tools/StringTools.h"
 using std::cout;
+using StringTools::fill_up_text_with_whitespaces_until_column;
 
 class SizeOfTest : public ::testing::Test {};
 //------------------------------------------------------------------------------
@@ -16,103 +20,75 @@ TEST_F(SizeOfTest, sizes_of_mct) {
 
     cout << "Size in bytes\n";
 
+    const uint c = 36;
+
     Vec3 v;
-    cout << "Vec3    " << sizeof(v) << "\n";
+    cout << fill_up_text_with_whitespaces_until_column("Vec3", c) << sizeof(v) << "\n";
 
     Rot3 r;
-    cout << "Rot3    " << sizeof(r) << "\n";
+    cout << fill_up_text_with_whitespaces_until_column("Rot3", c) << sizeof(r) << "\n";
 
     HomTra3 h;
-    cout << "HomTra3 " << sizeof(h) << "\n";
+    cout << fill_up_text_with_whitespaces_until_column("HomTra3", c) << sizeof(h) << "\n";
 
     Ray ray(Vec3::null, Vec3::unit_z);
-    cout << "Ray     " << sizeof(ray) << "\n";
+    cout << fill_up_text_with_whitespaces_until_column("Ray", c) << sizeof(ray) << "\n";
 
     CameraRay camray(Vec3::null, Vec3::unit_z);
-    cout << "CameraRay     " << sizeof(camray) << "\n";
+    cout << fill_up_text_with_whitespaces_until_column("CameraRay", c) << sizeof(camray) << "\n";
 
     RayForPropagation rayfp(Vec3::null, Vec3::unit_z);
-    cout << "RayForPropagation     " << sizeof(rayfp) << "\n";
+    cout << fill_up_text_with_whitespaces_until_column("RayForPropagation", c) << sizeof(rayfp) << "\n";
 
     Photon ph(Vec3::null, Vec3::unit_z, 433e-9);
-    cout << "Photon     " << sizeof(ph) << "\n";
+    cout << fill_up_text_with_whitespaces_until_column("Photon", c) << sizeof(ph) << "\n";
 
     Intersection is = Intersection::void_intersection;
-    cout << "Intersection     " << sizeof(is) << "\n";
+    cout << fill_up_text_with_whitespaces_until_column("Intersection", c) << sizeof(is) << "\n";
 
     Frame tree("0123456789", Vec3::null, Rot3::null);
-    cout << "Frame   " << sizeof(tree) << "\n";
+    cout << fill_up_text_with_whitespaces_until_column("Frame", c) << sizeof(tree) << "\n";
+
+    SurfaceEntity surf("0123456789", Vec3::null, Rot3::null);
+    cout << fill_up_text_with_whitespaces_until_column("SurfaceEntity", c) << sizeof(surf) << "\n";
+
+    Annulus annulus;
+    cout << fill_up_text_with_whitespaces_until_column("Annulus", c) << sizeof(annulus) << "\n";
+
+    BiConvexLens biConvexLens;
+    cout << fill_up_text_with_whitespaces_until_column("BiConvexLens", c) << sizeof(biConvexLens) << "\n";
+
+    BiConvexLensHexBound biConvexLensHexBound;
+    cout << fill_up_text_with_whitespaces_until_column("BiConvexLensHexBound", c) << sizeof(biConvexLensHexBound) << "\n";
+
+    Cylinder cylinder;
+    cout << fill_up_text_with_whitespaces_until_column("Cylinder", c) << sizeof(cylinder) << "\n";
+
+    Disc disc;
+    cout << fill_up_text_with_whitespaces_until_column("Disc", c) << sizeof(disc) << "\n";
+
+    EllipticalCapWithHexagonalBound ellipticalCapWithHexagonalBound;
+    cout << fill_up_text_with_whitespaces_until_column("EllipticalCapWithHexagonalBound   ", c) << sizeof(ellipticalCapWithHexagonalBound) << "\n";
+
+    HexPlane hexPlane;
+    cout << fill_up_text_with_whitespaces_until_column("HexPlane", c) << sizeof(hexPlane) << "\n";
+
+    Plane plane;
+    cout << fill_up_text_with_whitespaces_until_column("Plane", c) << sizeof(plane) << "\n";
+
+    Sphere sphere;
+    cout << fill_up_text_with_whitespaces_until_column("Sphere", c) << sizeof(sphere) << "\n";
+
+    SphereCapWithHexagonalBound sphereCapWithHexagonalBound;
+    cout << fill_up_text_with_whitespaces_until_column("SphereCapWithHexagonalBound", c) << sizeof(sphereCapWithHexagonalBound) << "\n";
+
+    Triangle triangle;
+    cout << fill_up_text_with_whitespaces_until_column("Triangle", c) << sizeof(triangle) << "\n";
 
     PropagationEnvironment environment = PropagationEnvironment::default_environment;
-    cout << "PropagationEnvironment   " << sizeof(environment) << "\n";
+    cout << fill_up_text_with_whitespaces_until_column("PropagationEnvironment", c) << sizeof(environment) << "\n";
 
-    Color col(123, 24, 12);
-    cout << "Color   " << sizeof(col) << "\n";     
-}
-//------------------------------------------------------------------------------
-#include "Core/Random/Random.h"
-#include <vector>
-#include <array>
-#include <math.h>
-#include <algorithm>
-TEST_F(SizeOfTest, speed_of_vector_ctor_and_tear_down) {
- 
-    std::array<double, 1000000> results; 
-    Random::Mt19937 prng(0);
-
-    for(uint i=0; i<results.size(); i++) {
-        const uint number_of_pushs = uint(ceil(20.0*prng.uniform()));
-        
-        std::vector<double> vec;
-
-        for(uint p=0; p<number_of_pushs; p++)
-            vec.push_back(prng.uniform());
-
-        results[i] = *std::min_element(vec.begin(), vec.end());
-    }
-
-    std::cout << *std::max_element(results.begin(), results.end()) << "\n";
-}
-//------------------------------------------------------------------------------
-TEST_F(SizeOfTest, speed_of_permanent_vector) {
- 
-    std::array<double, 1000000> results; 
-    Random::Mt19937 prng(0);
-    std::vector<double> vec;
-    //vec.reserve(32);
-
-    for(uint i=0; i<results.size(); i++) {
-        const uint number_of_pushs = uint(ceil(30.0*prng.uniform()));
-        
-        vec.clear();
-        for(uint p=0; p<number_of_pushs; p++)
-            vec.push_back(prng.uniform());
-
-        results[i] = *std::min_element(vec.begin(), vec.end());
-    }
-
-    std::cout << *std::max_element(results.begin(), results.end()) << "\n";
-}
-//------------------------------------------------------------------------------
-TEST_F(SizeOfTest, speed_of_permanent_array) {
- 
-    std::array<double, 1000000> results; 
-    Random::Mt19937 prng(0);
-    std::array<double, 32> arr;
-    uint x = 0;
-
-    for(uint i=0; i<results.size(); i++) {
-        const uint number_of_pushs = uint(ceil(30.0*prng.uniform()));
-        
-        x = 0;
-        for(uint p=0; p<number_of_pushs; p++) {
-            arr[x] = prng.uniform();
-            x++;
-        }
-
-        results[i] = *std::min_element(arr.begin(), arr.begin()+x);
-    }
-
-    std::cout << *std::max_element(results.begin(), results.end()) << "\n";
+    Color col(123, 65, 12);
+    cout << fill_up_text_with_whitespaces_until_column("Color", c) << sizeof(col) << "\n";     
 }
 //------------------------------------------------------------------------------
