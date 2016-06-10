8569daf605016e038ba00446318044857a907a01
sebastian <sebmuell@phys.ethz.ch>
2016-06-10 18:47:41 +0200

    print more Corsika infos

--- Corsika/Tools.cpp
+++ Corsika/Tools.cpp
@@ -34,6 +34,38 @@ namespace Corsika {
         out << " 11 " << "zenith angle Theta " << eh[11-1] << "rad\n";
         out << " 12 " << "azimuth angle Phi " << eh[12-1] << "rad\n";
         out << "------------------------------------------------------------\n";
+        out << " 13 " << "number of different random number sequences " << eh[13-1] << "\n";
+        out << " 11+3i " << "\n";
+        out << " 12+3i " << "\n";
+        out << " 13+3i " << "\n";
+        out << "------------------------------------------------------------\n";
+        out << " 44 " << "run number " << eh[44-1] << "\n";
+        out << " 45 " << "date of begin run (yymmdd) " << eh[45-1] << "\n";
+        out << " 46 " << "version of program " << eh[46-1] << "\n";
+        out << "------------------------------------------------------------\n";
+        out << " 47 " << "number of observation levels " << eh[47-1] << "\n";
+        for(uint t=1; t<= eh[47-1]; t++) {
+        out << " " << 47+t << " " << "height of level " << t << eh[47+t-1] << "cm\n";
+        }
+        out << "------------------------------------------------------------\n";
+        out << " 58 " << "slope of energy spectrum " << eh[58-1] << "\n";
+        out << " 59 " << "lower limit of energy range " << eh[59-1] << "GeV\n";
+        out << " 60 " << "upper limit of energy range " << eh[60-1] << "GeV\n";
+        out << " 61 " << "cutoff for hadrons kinetic energy " << eh[61-1] << "GeV\n";
+        out << " 62 " << "cutoff for muons kinetic energy " << eh[62-1] << "GeV\n";
+        out << " 63 " << "cutoff for electrons kinetic energy " << eh[63-1] << "GeV\n";
+        out << " 64 " << "cutoff for photons energy " << eh[64-1] << "GeV\n";
+        out << " 65 " << "NFLAIN " << eh[65-1] << "\n";
+        out << " 66 " << "NFLDIF " << eh[66-1] << "\n";
+        out << " 67 " << "NFLPI0 " << eh[67-1] << "\n";
+        out << " 68 " << "NFLPIF " << eh[68-1] << "\n";
+        out << " 69 " << "NFLCHE " << eh[69-1] << "\n";
+        out << " 70 " << "NFRAGM " << eh[70-1] << "\n";
+        out << " 71 " << "x component of Earth’s magnetic field " << eh[71-1] << "uT\n";
+        out << " 72 " << "z component of Earth’s magnetic field " << eh[72-1] << "uT\n";
+        out << " 73 " << "flag for activating EGS4 " << eh[73-1] << "\n";
+        out << " 74 " << "flag for activating NKG " << eh[74-1] << "\n";
+        out << "------------------------------------------------------------\n";
         return out.str();
     }
     
