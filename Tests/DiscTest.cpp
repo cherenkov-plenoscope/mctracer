faaf6746d80437a269d60c38dd0fa97824139e79
sebastian <sebmuell@phys.ethz.ch>
2016-06-30 12:27:28 +0200

    refactoring

--- Tests/DiscTest.cpp
+++ Tests/DiscTest.cpp
@@ -1,21 +1,21 @@
 #include "gtest/gtest.h"
 #include "Scenery/Primitive/Disc.h"
 
-using namespace std;
-
-// The fixture for testing class Foo.
 class DiscTest : public ::testing::Test {};
 //------------------------------------------------------------------------------
 TEST_F(DiscTest, set_Disc) {
 
-  bool error_detected = false;
-  try{
+	Disc D;
+	D.set_radius(42.0);
+	EXPECT_EQ(42.0, D.contour_radius());
+}
+//------------------------------------------------------------------------------
+TEST_F(DiscTest, set_Disc_negative_radius) {
 
-    Disc D;
-    D.set_radius(42.0);
-    EXPECT_EQ(42.0, D.contour_radius());
-  }catch(...){
-    error_detected = true;
-  }
-  EXPECT_FALSE(error_detected);
+	EXPECT_THROW(
+		Disc D;
+		D.set_radius(-42.0);
+		,
+		TracerException
+	);
 }
