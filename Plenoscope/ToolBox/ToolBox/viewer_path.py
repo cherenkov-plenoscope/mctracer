a718612269560f961d290124792d7edc6ba30208
sebastian <sebmuell@phys.ethz.ch>
2016-07-07 17:46:35 +0200

    remove obsolete files

--- Plenoscope/ToolBox/ToolBox/viewer_path.py
+++
@@ -1,8 +0,0 @@
-import os.path
-
-class Path():
-    def __init__(self, fullpath):
-        self.full = fullpath
-        self.path, self.name = os.path.split(fullpath)
-        self.name_wo_ext = os.path.splitext(self.name)[0]
-        self.path_wo_ext, self.ext = os.path.splitext(fullpath)
