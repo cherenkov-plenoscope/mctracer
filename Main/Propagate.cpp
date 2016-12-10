ee8de7100b2558df219e55611f5f05bed92f8177
sebastian <sebmuell@phys.ethz.ch>
2016-12-10 17:41:02 +0100

    enable binary output optional

--- Main/Propagate.cpp
+++ Main/Propagate.cpp
@@ -13,7 +13,7 @@ static const char USAGE[] =
 R"(Photon propagation
 
     Usage:
-      mctPropagate -s=SCENERY_PATH -c=CONFIG_PATH -i=INPUT_PATH -o=OUTPUT_PATH [-r=SEED]
+      mctPropagate -s=SCENERY_PATH -c=CONFIG_PATH -i=INPUT_PATH -o=OUTPUT_PATH [-r=SEED] [-b]
       mctPropagate (-h | --help)
       mctPropagate --version
 
@@ -23,13 +23,13 @@ R"(Photon propagation
       -i --input=INPUT_PATH     Photon file path (e.g. a CORSIKA run).
       -o --output=OUTPUT_PATH   Output path.
       -r --random_seed=SEED     Seed for pseudo random number generator.
+      -b --binary               Exports a binary float32 table for each sensor.
       -h --help                 Show this screen.
       --version                 Show version.
-      
 )";
 
 int main(int argc, char* argv[]) {
-	try{
+    try{
 
     std::map<std::string, docopt::value> args = docopt::docopt(
         USAGE,
@@ -42,75 +42,87 @@ int main(int argc, char* argv[]) {
     PathTools::Path scenery_path = PathTools::Path(args.find("--scenery")->second.asString());
     PathTools::Path photon_path = PathTools::Path(args.find("--input")->second.asString());
     PathTools::Path config_path = PathTools::Path(args.find("--config")->second.asString());
-
-	Xml::Document doc(config_path.path);
-	Xml::Node se = doc.node().child("settings");
+    const bool export_binary = args.find("--binary")->second.asBool();
+    std::cout << "exp bin: " << export_binary << "\n";
+    Xml::Document doc(config_path.path);
+    Xml::Node se = doc.node().child("settings");
    
     //--------------------------------------------------------------------------
     // BASIC SETTINGS
-	PropagationConfig settings = Xml::Configs::get_PropagationConfig_from_node(se);
-	
-	//--------------------------------------------------------------------------
-	// Random
+    PropagationConfig settings = Xml::Configs::get_PropagationConfig_from_node(se);
+    
+    //--------------------------------------------------------------------------
+    // Random
     Random::Mt19937 prng;
     if(args.find("--random_seed")->second)
         prng.set_seed(args.find("--random_seed")->second.asLong());
 
-	//--------------------------------------------------------------------------
-	// scenery
-	Xml::SceneryFactory fab(scenery_path.path);
-	Scenery scenery;
-	fab.append_to_frame_in_scenery(&scenery.root, &scenery);
-	scenery.root.init_tree_based_on_mother_child_relations();
-	//--------------------------------------------------------------------------
-	// sensors in scenery
-	PhotonSensor::Sensors sensors = fab.sensors();
-	
-	//--------------------------------------------------------------------------
-	// photon source
-	PhotonsReader photon_file(photon_path.path);
+    //--------------------------------------------------------------------------
+    // scenery
+    Xml::SceneryFactory fab(scenery_path.path);
+    Scenery scenery;
+    fab.append_to_frame_in_scenery(&scenery.root, &scenery);
+    scenery.root.init_tree_based_on_mother_child_relations();
+    //--------------------------------------------------------------------------
+    // sensors in scenery
+    PhotonSensor::Sensors sensors = fab.sensors();
+    
+    //--------------------------------------------------------------------------
+    // photon source
+    PhotonsReader photon_file(photon_path.path);
 
-	uint event_counter = 1;
-	while(photon_file.has_still_photons_left()) {
+    uint event_counter = 1;
+    while(photon_file.has_still_photons_left()) {
 
-		vector<Photon> photons;
-		photons = photon_file.next(&prng);
+        vector<Photon> photons;
+        photons = photon_file.next(&prng);
 
-		//----------------------------------------------------------------------
-		// photon propagation
-		Photons::propagate_photons_in_scenery_with_settings(
-			&photons, &scenery.root, &settings, &prng
-		);
+        //----------------------------------------------------------------------
+        // photon propagation
+        Photons::propagate_photons_in_scenery_with_settings(
+            &photons, &scenery.root, &settings, &prng
+        );
 
-		//----------------------------------------------------------------------
-		// detect photons in sensors
-		sensors.clear_history();
-		sensors.assign_photons(&photons);
-		//----------------------------------------------------------------------
-		// write each sensors to file
-		for(uint i=0; i<sensors.size(); i++) {
+        //----------------------------------------------------------------------
+        // detect photons in sensors
+        sensors.clear_history();
+        sensors.assign_photons(&photons);
+        //----------------------------------------------------------------------
+        // write each sensors to file
+        for(uint i=0; i<sensors.size(); i++) {
 
-			std::stringstream outname;
-			outname << out_path.path << event_counter << "_" << i;
+            std::stringstream outname;
+            outname << out_path.path << event_counter << "_" << i;
 
-			std::stringstream header;
-			header << "scenery: " << scenery_path.path << "\n";
-			header << "sensor:  " << sensors.at(i)->get_frame()->get_path_in_tree_of_frames() << ", ID: " << i << "\n";
-			header << "photons: " << photon_path.path << "\n";
-			header << "-------------\n";
-			header << sensors.at(i)->get_arrival_table_header();
+            if(export_binary) {
 
-			AsciiIo::write_table_to_file_with_header(
-				sensors.at(i)->get_arrival_table(),
-				outname.str(),
-				header.str()
-			);
-		}
-		event_counter++;
-	}
-	//--------------------------------------------------------------------------
-	}catch(std::exception &error) {
-		std::cerr << error.what(); 
-	}
-	return 0;
+                std::ofstream out;
+                out.open(outname.str(), std::ios::out | std::ios::binary);
+                PhotonSensor::write_arrival_information_to_file(
+                    &(sensors.at(i)->arrival_table),
+                    &out);
+                out.close();
+            }else{
+
+                std::stringstream header;
+                header << "scenery: " << scenery_path.path << "\n";
+                header << "sensor:  " << sensors.at(i)->get_frame()->get_path_in_tree_of_frames() << ", ID: " << i << "\n";
+                header << "photons: " << photon_path.path << "\n";
+                header << "-------------\n";
+                header << sensors.at(i)->get_arrival_table_header();
+
+                AsciiIo::write_table_to_file_with_header(
+                    sensors.at(i)->get_arrival_table(),
+                    outname.str(),
+                    header.str()
+                );      
+            }
+        }
+        event_counter++;
+    }
+    //--------------------------------------------------------------------------
+    }catch(std::exception &error) {
+        std::cerr << error.what(); 
+    }
+    return 0;
 }
