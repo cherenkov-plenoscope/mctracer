5f2776487a4dfefc9f737e3c52a247dc9c02d207
sebastian <sebmuell@phys.ethz.ch>
2017-01-11 07:44:57 +0100

    remove noise

--- Tests/ApertureCameraTest.cpp
+++ Tests/ApertureCameraTest.cpp
@@ -1,35 +1,7 @@
 #include "gtest/gtest.h"
 #include "Visual/ApertureCamera.h"
 
-// The fixture for testing class Foo.
-class ApertureCameraTest : public ::testing::Test {
- protected:
-  // You can remove any or all of the following functions if its body
-  // is empty.
-	
-  ApertureCameraTest() {
-    // You can do set-up work for each test here.
-  }
-
-  virtual ~ApertureCameraTest() {
-    // You can do clean-up work that doesn't throw exceptions here.
-  }
-
-  // If the constructor and destructor are not enough for setting up
-  // and cleaning up each test, you can define the following methods:
-
-  virtual void SetUp() {
-    // Code here will be called immediately after the constructor (right
-    // before each test).
-  }
-
-  virtual void TearDown() {
-    // Code here will be called immediately after each test (right
-    // before the destructor).
-  }
-
-  // Objects declared here can be used by all tests in the test case for Foo.
-};
+class ApertureCameraTest : public ::testing::Test {};
 //----------------------------------------------------------------------
 TEST_F(ApertureCameraTest, set_up) {
   ApertureCamera Mamiya645("Mamiya645", 640, 480);
