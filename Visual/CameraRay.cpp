a9881881a3d7b29d80d827bac62d470b06891cdd
sebastian <sebmuell@phys.ethz.ch>
2017-02-16 10:49:19 +0100

    remove obsolete

--- Visual/CameraRay.cpp
+++ Visual/CameraRay.cpp
@@ -13,49 +13,5 @@ std::string CameraRay::get_print()const{
 	out << Ray::get_print() << ", color: " << color;
 	return out.str();
 }
-/*
-//------------------------------------------------------------------------------
-Color CameraRay::trace(
-	const Frame* world,
-	uint refl_count,
-	const Config *visual_config
-) {
-	Color color;
-	const Intersection intersection = 
-		RayAndFrame::first_intersection(this, world);
-
-	if(intersection.does_intersect()) {
-
-		double refl_coeff = intersection.get_facing_reflection_propability(533e-9);
-		if(refl_coeff > 0.0) {
-			refl_count++;
-
-			CameraRay reflected_ray(
-				intersection.get_intersection_vector_in_world_system(),
-				intersection.get_reflection_direction_in_world_system(get_direction())
-			);
-
-			Color reflection_color = 
-				reflected_ray.trace(world, refl_count, visual_config);
-
-			color = intersection.get_facing_color();
-			color.reflection_mix(
-				reflection_color,
-				refl_coeff
-			);
-		}else{
-
-			if(visual_config->global_illumination.on)
-				color = shadow_of_sky_light(world, visual_config, intersection);
-			else
-				color = intersection.get_facing_color();
-		}
-	}else{
-		color = visual_config->sky_dome.get_color_for_direction(direction);
-	}
-	
-	return color;	
-}
-*/
 //------------------------------------------------------------------------------
 }//Visual
