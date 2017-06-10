49948106321a8ea0eeb7d6bbebf5b2f3a26b8010
sebastian <sebmuell@phys.ethz.ch>
2017-06-10 19:35:23 +0200

    Aloow larger misalignments to be calibrated easily at the cost of more computation time.

--- Plenoscope/NightSkyBackground/NightSkyBackground.h
+++ Plenoscope/NightSkyBackground/NightSkyBackground.h
@@ -15,7 +15,12 @@ namespace Plenoscope {
 	namespace NightSkyBackground {
 
 		const double APERTURE_RADIUS_OVERHEAD = 1.05;
-		const double FOV_RADIUS_OVERHEAD = 1.05;
+		const double FOV_RADIUS_OVERHEAD = 1.20; 
+                // For the target position and orientation of the
+                // light field sensor relative to the imaging reflector,
+                // a 1.05 overhead would be sufficient, but to explore
+                // also stronger misalignments, we spear the incoming
+                // photon directions more out.
 
 	}// NightSkyBackground
 }// Plenoscope
