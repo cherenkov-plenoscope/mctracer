0c5cd97c73e42434fe37daf78ac7ee673d440638
sebastian <sebmuell@phys.ethz.ch>
2017-12-07 19:30:11 +0100

    cpplint Rot3

--- Core/Rot3.cpp
+++ Core/Rot3.cpp
@@ -1,95 +1,91 @@
-#include <iostream>
+// Copyright 2014 Sebastian A. Mueller
 #include <math.h>
+#include <iostream>
 #include <sstream>
 #include <exception>
 #include "Rot3.h"
-//------------------------------------------------------------------------------
+
 const Rot3 Rot3::null = Rot3(0.0, 0.0, 0.0);
-//------------------------------------------------------------------------------
-Rot3::Rot3() {
-}
-//------------------------------------------------------------------------------
+
+Rot3::Rot3() {}
+
 Rot3::Rot3(double Phi, double The, double Psi) {
-	set(Phi,The,Psi);
+    set(Phi, The, Psi);
 }
-//------------------------------------------------------------------------------
+
 void Rot3::set(double Phi, double The, double Psi) {
-	flag_rot_angles_xyz = true;
-	
-	Rx = Phi;
-	Ry = The;
-	Rz = Psi;
+    flag_rot_angles_xyz = true;
+
+    Rx = Phi;
+    Ry = The;
+    Rz = Psi;
 }
-//------------------------------------------------------------------------------
+
 Rot3::Rot3(
-	const Vec3 new_rot_axis,
-	const double new_rot_angle_in_rad
-){
-	set(new_rot_axis,new_rot_angle_in_rad);
+    const Vec3 new_rot_axis,
+    const double new_rot_angle_in_rad
+) {
+    set(new_rot_axis, new_rot_angle_in_rad);
 }
-//------------------------------------------------------------------------------
+
 void Rot3::set(
-	const Vec3 new_rot_axis,
-	const double new_rot_angle_in_rad
-){
-	if(new_rot_angle_in_rad == 0.0) {
-
-		set(0.0, 0.0, 0.0);
-	}else{
-
-		flag_rot_angles_xyz = false;
-		rot_axis = new_rot_axis/new_rot_axis.norm();
-		rot_angle_in_rad = new_rot_angle_in_rad;	
-	}
+    const Vec3 new_rot_axis,
+    const double new_rot_angle_in_rad
+) {
+    if (new_rot_angle_in_rad == 0.0) {
+        set(0.0, 0.0, 0.0);
+    } else {
+        flag_rot_angles_xyz = false;
+        rot_axis = new_rot_axis/new_rot_axis.norm();
+        rot_angle_in_rad = new_rot_angle_in_rad;
+    }
 }
-//------------------------------------------------------------------------------
+
 Vec3 Rot3::get_rot_axis()const {
-	if(flag_rot_angles_xyz == true)
-		throw std::runtime_error(
-			"Rot3::get_rot_axis():\n"
-			"rot_axis was not set! Returning default ez.\n"
-		);
-	else
-		return rot_axis;
+    if (flag_rot_angles_xyz == true)
+        throw std::runtime_error(
+            "Rot3::get_rot_axis():\n"
+            "rot_axis was not set! Returning default ez.\n");
+    else
+        return rot_axis;
 }
-//------------------------------------------------------------------------------
+
 double Rot3::get_rot_angle_in_rad()const {
-	if(flag_rot_angles_xyz == true)
-		throw std::runtime_error(
-			"Rot3::get_rot_angle_in_rad():\n"
-			"rot_angle_in_rad was not set!"
-		);
-	else
-		return rot_angle_in_rad;
+    if (flag_rot_angles_xyz == true)
+        throw std::runtime_error(
+            "Rot3::get_rot_angle_in_rad():\n"
+            "rot_angle_in_rad was not set!");
+    else
+        return rot_angle_in_rad;
 }
-//------------------------------------------------------------------------------
+
 bool Rot3::uses_xyz_angels()const {
-	return flag_rot_angles_xyz;
+    return flag_rot_angles_xyz;
 }
-//------------------------------------------------------------------------------
+
 std::string Rot3::str()const {
-	std::stringstream out; 
+    std::stringstream out;
 
-	if(flag_rot_angles_xyz == true)
-		out << "(" << Rx << " " << Ry << " " << Rz << ")rad";
-	else
-		out << "rot-axis: " << rot_axis.str() << ", rot-angle: " 
-			<< rot_angle_in_rad << "rad";
+    if (flag_rot_angles_xyz == true)
+        out << "(" << Rx << " " << Ry << " " << Rz << ")rad";
+    else
+        out << "rot-axis: " << rot_axis.str() << ", rot-angle: "
+            << rot_angle_in_rad << "rad";
 
-	return out.str();
+    return out.str();
 }
-//------------------------------------------------------------------------------
+
 double Rot3::get_rot_x()const {return Rx;}
 double Rot3::get_rot_y()const {return Ry;}
 double Rot3::get_rot_z()const {return Rz;}
-//------------------------------------------------------------------------------
+
 double Rot3::cosRx() const {return cos(Rx);}
 double Rot3::cosRy() const {return cos(Ry);}
 double Rot3::cosRz() const {return cos(Rz);}
 double Rot3::sinRx() const {return sin(Rx);}
 double Rot3::sinRy() const {return sin(Ry);}
 double Rot3::sinRz() const {return sin(Rz);}
-//------------------------------------------------------------------------------
-bool Rot3::operator == (const Rot3& eqRot)const {	
-	return Rx == eqRot.Rx && Ry == eqRot.Ry && Rz == eqRot.Rz;
+
+bool Rot3::operator == (const Rot3& eqRot)const {
+    return Rx == eqRot.Rx && Ry == eqRot.Ry && Rz == eqRot.Rz;
 }
--- Core/Rot3.h
+++ Core/Rot3.h
@@ -1,46 +1,40 @@
-//=================================
-// include guard
-#ifndef __Rot3_H_INCLUDED__
-#define __Rot3_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef MCTRACER_CORE_ROT3_H_
+#define MCTRACER_CORE_ROT3_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
+#include <string>
 #include "Core/Vec3.h"
-//=================================
+
 // Tait–Bryan angles with an extrinsic coordinate frame fasion
+
 class Rot3 {
-	double Rx;
-	double Ry;
-	double Rz;
-	double rot_angle_in_rad;
-	Vec3 rot_axis;
-	bool flag_rot_angles_xyz;
-public:
-	static const Rot3 null;
-	
-	Rot3();
-	Rot3(double Phi, double The, double Psi);
-	Rot3(const Vec3 new_rot_axis, const double new_rot_angle_on_rad);
-	
-	void set(double Phi, double The, double Psi);
-	void set(const Vec3 new_rot_axis, const double new_rot_angle_on_rad);
-	
-	Vec3 get_rot_axis()const;
-	double get_rot_angle_in_rad()const;
-	bool uses_xyz_angels()const;
-	std::string str()const;
-	double get_rot_x()const;
-	double get_rot_y()const;
-	double get_rot_z()const;
-	double cosRx()const;
-	double cosRy()const;
-	double cosRz()const;
-	double sinRx()const;
-	double sinRy()const;
-	double sinRz()const;
-	bool operator == (const Rot3& eqRot)const;
+    double Rx;
+    double Ry;
+    double Rz;
+    double rot_angle_in_rad;
+    Vec3 rot_axis;
+    bool flag_rot_angles_xyz;
+
+ public:
+    static const Rot3 null;
+    Rot3();
+    Rot3(double Phi, double The, double Psi);
+    Rot3(const Vec3 new_rot_axis, const double new_rot_angle_on_rad);
+    void set(double Phi, double The, double Psi);
+    void set(const Vec3 new_rot_axis, const double new_rot_angle_on_rad);
+    Vec3 get_rot_axis()const;
+    double get_rot_angle_in_rad()const;
+    bool uses_xyz_angels()const;
+    std::string str()const;
+    double get_rot_x()const;
+    double get_rot_y()const;
+    double get_rot_z()const;
+    double cosRx()const;
+    double cosRy()const;
+    double cosRz()const;
+    double sinRx()const;
+    double sinRy()const;
+    double sinRz()const;
+    bool operator == (const Rot3& eqRot)const;
 };
-#endif // __Rot3_H_INCLUDED__ 
+#endif  // MCTRACER_CORE_ROT3_H_
