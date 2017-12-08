236f3593eecdaca0d6cee63ac161d296500153ff
sebastian <sebmuell@phys.ethz.ch>
2017-12-08 09:41:04 +0100

    cpplint Phys const

--- Core/PhysicalConstants.h
+++ Core/PhysicalConstants.h
@@ -1,11 +1,12 @@
-//=================================
-// include guard
-#ifndef __PhysicalConstants_H_INCLUDED__
-#define __PhysicalConstants_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef MCTRACER_CORE_PHYSICALCONSTANTS_H_
+#define MCTRACER_CORE_PHYSICALCONSTANTS_H_
 
 
-namespace PhysicalConstants{
-	static const double speed_of_light_in_vacuum = 299792458.0;
-}
+namespace PhysicalConstants {
 
-#endif // __PhysicalConstants_H_INCLUDED__
+static const double speed_of_light_in_vacuum = 299792458.0;
+
+}  // namespace PhysicalConstants
+
+#endif  // MCTRACER_CORE_PHYSICALCONSTANTS_H_
