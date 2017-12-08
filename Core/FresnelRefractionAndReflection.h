5815f3efe6d962c09a43a34a68bc2387b2fc0821
sebastian <sebmuell@phys.ethz.ch>
2017-12-08 10:13:18 +0100

    cpplint Fresnel

--- Core/FresnelRefractionAndReflection.h
+++ Core/FresnelRefractionAndReflection.h
@@ -1,41 +1,29 @@
-//=================================
-// include guard
-#ifndef __FresnelRefractionAndReflection_H_INCLUDED__
-#define __FresnelRefractionAndReflection_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef MCTRACER_CORE_FRESNELREFRACTIONANDREFLECTION_H_
+#define MCTRACER_CORE_FRESNELREFRACTIONANDREFLECTION_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
 #include "Core/Vec3.h"
 
-//=================================
 class FresnelRefractionAndReflection{
-protected:
+ protected:
+    const Vec3 incident_obj_sys;
+    const Vec3 normal_obj_sys;
+    const double n_from;
+    const double n_to;
 
-	const Vec3 incident_obj_sys;
-	const Vec3 normal_obj_sys;
-	const double n_from;
-	const double n_to;
+    double cosI;
+    double n_from_over_n_to;
+    double sinT2;
+    double cosT;
 
-	double cosI;
-	double n_from_over_n_to;
-	double sinT2;
-	double cosT;
-public:
-	
-	FresnelRefractionAndReflection(
-		const Vec3 &incident_obj_sys,
-		const Vec3 &normal_obj_sys,
-		double n_from,
-		double n_to
-	);
-	
-	Vec3 get_reflec_dir_in_object_system()const;
-	
-	Vec3 get_refrac_dir_in_object_system()const;
-	
-	double reflection_propability()const;
+ public:
+    FresnelRefractionAndReflection(
+        const Vec3 &incident_obj_sys,
+        const Vec3 &normal_obj_sys,
+        double n_from,
+        double n_to);
+    Vec3 get_reflec_dir_in_object_system()const;
+    Vec3 get_refrac_dir_in_object_system()const;
+    double reflection_propability()const;
 };
-#endif // __FresnelRefractionAndReflection_H_INCLUDED__ 
+#endif  // MCTRACER_CORE_FRESNELREFRACTIONANDREFLECTION_H_
