a3981ec7c93ad0aed26f1997c30d1571f0556e11
sebastian <sebmuell@phys.ethz.ch>
2017-12-08 10:09:53 +0100

    cpplint PhotonAndFrame

--- Core/PhotonAndFrame.cpp
+++ Core/PhotonAndFrame.cpp
@@ -1,140 +1,130 @@
-#include "PhotonAndFrame.h"
+// Copyright 2014 Sebastian A. Mueller
+#include "Core/PhotonAndFrame.h"
+
 namespace PhotonAndFrame {
-//------------------------------------------------------------------------------
+
 Propagator::Propagator(
-	Photon* photon,
-	PropagationEnvironment environment
-): 
-	env(environment), ph(photon) 
-{
-	propagate();
+    Photon* photon,
+    PropagationEnvironment environment
+): env(environment), ph(photon) {
+    propagate();
 }
-//------------------------------------------------------------------------------
+
 void Propagator::propagate() {
-	if(limit_of_interactions_is_not_reached_yet())
-		work_on_first_causal_intersection();		
+    if (limit_of_interactions_is_not_reached_yet())
+        work_on_first_causal_intersection();
 }
-//------------------------------------------------------------------------------
+
 bool Propagator::limit_of_interactions_is_not_reached_yet()const {
-	return ph->get_number_of_interactions_so_far() <= 
-		env.propagation_options->max_number_of_interactions_per_photon;			
+    return ph->get_number_of_interactions_so_far() <=
+        env.propagation_options->max_number_of_interactions_per_photon;
 }
-//------------------------------------------------------------------------------
+
 void Propagator::work_on_first_causal_intersection() {
+    isec = RayAndFrame::first_intersection(ph, env.world_geometry);
 
-	isec = RayAndFrame::first_intersection(ph, env.world_geometry);
-
-	if(isec.does_intersect() && !absorbed_in_medium_before_reaching_surface())
-		interact_with_object();
-	else
-		get_absorbed_in_void_space();
+    if (isec.does_intersect() && !absorbed_in_medium_before_reaching_surface())
+        interact_with_object();
+    else
+        get_absorbed_in_void_space();
 }
-//------------------------------------------------------------------------------
+
 bool Propagator::absorbed_in_medium_before_reaching_surface()const {
-	const double one_over_e_way = 
-		isec.get_half_way_depth_coming_from(ph->wavelength);
-	const double survival_prob = 
-		exp(-isec.get_intersection_distance()/one_over_e_way);
-	return env.random_engine->uniform() > survival_prob;
+    const double one_over_e_way =
+        isec.get_half_way_depth_coming_from(ph->wavelength);
+    const double survival_prob =
+        exp(-isec.get_intersection_distance()/one_over_e_way);
+    return env.random_engine->uniform() > survival_prob;
 }
-//------------------------------------------------------------------------------
+
 void Propagator::interact_with_object() {
-	if(
-		isec.get_facing_reflection_propability(ph->wavelength) >= 
-		env.random_engine->uniform()
-	)
-		reflect_on_surface_and_propagate_on(reflection_on_surface);
-	else
-		reach_boundary_layer(); 
+    if (
+        isec.get_facing_reflection_propability(ph->wavelength) >=
+        env.random_engine->uniform()
+    )
+        reflect_on_surface_and_propagate_on(reflection_on_surface);
+    else
+        reach_boundary_layer();
 }
-//------------------------------------------------------------------------------
+
 void Propagator::get_absorbed_in_void_space() {
-	ph->push_back_intersection_and_type_to_propagation_history(
-		isec, 
-		absorption_in_void
-	);
+    ph->push_back_intersection_and_type_to_propagation_history(
+        isec,
+        absorption_in_void);
 }
-//------------------------------------------------------------------------------
+
 void Propagator::reflect_on_surface_and_propagate_on(const Interaction type) {
-	ph->set_support_and_direction(
-		isec.get_intersection_vector_in_world_system(),
-		isec.get_reflection_direction_in_world_system(ph->get_direction())
-	);
+    ph->set_support_and_direction(
+        isec.get_intersection_vector_in_world_system(),
+        isec.get_reflection_direction_in_world_system(ph->get_direction()));
 
-	ph->push_back_intersection_and_type_to_propagation_history(
-		isec, 
-		type
-	);
+    ph->push_back_intersection_and_type_to_propagation_history(
+        isec,
+        type);
 
-	propagate();
+    propagate();
 }
-//------------------------------------------------------------------------------
+
 void Propagator::reach_boundary_layer() {
-	if(isec.boundary_layer_is_transparent())
-		fresnel_refraction_and_reflection();
-	else
-		get_absorbed_on_surface();
+    if (isec.boundary_layer_is_transparent())
+        fresnel_refraction_and_reflection();
+    else
+        get_absorbed_on_surface();
 }
-//------------------------------------------------------------------------------
+
 void Propagator::fresnel_refraction_and_reflection() {
-	FresnelRefractionAndReflection fresnel(
-		isec.object2world()->
-			get_transformed_orientation_inverse(ph->get_direction()),
-		isec.get_normal_in_faceing_surface_system(),
-		isec.get_refractive_index_coming_from(ph->wavelength),
-		isec.get_refractive_index_going_to(ph->wavelength)
-	);
+    FresnelRefractionAndReflection fresnel(
+        isec.object2world()->
+            get_transformed_orientation_inverse(ph->get_direction()),
+        isec.get_normal_in_faceing_surface_system(),
+        isec.get_refractive_index_coming_from(ph->wavelength),
+        isec.get_refractive_index_going_to(ph->wavelength));
 
-	if(fresnel.reflection_propability() > env.random_engine->uniform())
-		reflect_on_surface_and_propagate_on(fresnel_reflection_on_surface); 
-	else
-		pass_the_boundary_layer(fresnel);
+    if (fresnel.reflection_propability() > env.random_engine->uniform())
+        reflect_on_surface_and_propagate_on(fresnel_reflection_on_surface);
+    else
+        pass_the_boundary_layer(fresnel);
 }
-//------------------------------------------------------------------------------
+
 void Propagator::pass_the_boundary_layer(
-	const FresnelRefractionAndReflection &fresnel 
+    const FresnelRefractionAndReflection &fresnel
 ) {
-	if(isec.from_outside_to_inside())
-		ph->push_back_intersection_and_type_to_propagation_history(
-			isec, 
-			refraction_to_inside
-		);
-	else
-		ph->push_back_intersection_and_type_to_propagation_history(
-			isec, 
-			refraction_to_outside
-		);			
+    if (isec.from_outside_to_inside())
+        ph->push_back_intersection_and_type_to_propagation_history(
+            isec,
+            refraction_to_inside);
+    else
+        ph->push_back_intersection_and_type_to_propagation_history(
+            isec,
+            refraction_to_outside);
 
-	propagate_on_after_boundary_layer(fresnel);
+    propagate_on_after_boundary_layer(fresnel);
 }
-//------------------------------------------------------------------------------
+
 void Propagator::propagate_on_after_boundary_layer(
-	const FresnelRefractionAndReflection &fresnel
+    const FresnelRefractionAndReflection &fresnel
 ) {
-	if(	isec.get_object()->has_restrictions_on_frames_to_propagate_to() && 
-		!isec.going_to_default_refractive_index()
-	)
-		env.world_geometry = 
-			isec.get_object()->get_allowed_frame_to_propagate_to();
-	else
-		env.world_geometry = 
-			isec.get_object()->get_root();
+    if (isec.get_object()->has_restrictions_on_frames_to_propagate_to() &&
+        !isec.going_to_default_refractive_index()
+    )
+        env.world_geometry =
+            isec.get_object()->get_allowed_frame_to_propagate_to();
+    else
+        env.world_geometry =
+            isec.get_object()->get_root();
 
-	ph->set_support_and_direction(
-		isec.get_intersection_vector_in_world_system(),
-		isec.object2world()->get_transformed_orientation(
-			fresnel.get_refrac_dir_in_object_system()
-		)
-	);
+    ph->set_support_and_direction(
+        isec.get_intersection_vector_in_world_system(),
+        isec.object2world()->get_transformed_orientation(
+            fresnel.get_refrac_dir_in_object_system()));
 
-	propagate();	
+    propagate();
 }
-//------------------------------------------------------------------------------
+
 void Propagator::get_absorbed_on_surface() {
-	ph->push_back_intersection_and_type_to_propagation_history(
-		isec, 
-		absorption_on_surface
-	);	
+    ph->push_back_intersection_and_type_to_propagation_history(
+        isec,
+        absorption_on_surface);
 }
-//------------------------------------------------------------------------------
-}//PhotonAndFrame
+
+}  // namespace PhotonAndFrame
--- Core/PhotonAndFrame.h
+++ Core/PhotonAndFrame.h
@@ -1,46 +1,39 @@
-//=================================
-// include guard
-#ifndef __PHOTONANDFRAME_H_INCLUDED__
-#define __PHOTONANDFRAME_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef MCTRACER_CORE_PHOTONANDFRAME_H_
+#define MCTRACER_CORE_PHOTONANDFRAME_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
 #include "Core/Photon.h"
 #include "Core/Frame.h"
 #include "Core/RayAndFrame.h"
 #include "Core/PropagationEnvironment.h"
-//=================================
+
 namespace PhotonAndFrame {
 
-	class Propagator {
-	public:
-		Intersection isec;
-		PropagationEnvironment env;		
-		Photon* ph;
+class Propagator {
+ public:
+    Intersection isec;
+    PropagationEnvironment env;
+    Photon* ph;
 
-		Propagator(
-			Photon* photon,
-			PropagationEnvironment env
-		);
-		void propagate();
-		bool limit_of_interactions_is_not_reached_yet()const;
-		void work_on_first_causal_intersection();
-		bool absorbed_in_medium_before_reaching_surface()const ;
-		void interact_with_object();
-		void get_absorbed_in_void_space();
-		void reflect_on_surface_and_propagate_on(const Interaction type);
-		void reach_boundary_layer();
-		void fresnel_refraction_and_reflection();
-		void pass_the_boundary_layer(
-			const FresnelRefractionAndReflection &fresnel 
-		);
-		void propagate_on_after_boundary_layer(
-			const FresnelRefractionAndReflection &fresnel
-		);
-		void get_absorbed_on_surface() ;
-	};
-}//PhotonAndFrame
-#endif // __PHOTONANDFRAME_H_INCLUDED__ 
+    Propagator(
+        Photon* photon,
+        PropagationEnvironment env);
+    void propagate();
+    bool limit_of_interactions_is_not_reached_yet()const;
+    void work_on_first_causal_intersection();
+    bool absorbed_in_medium_before_reaching_surface()const;
+    void interact_with_object();
+    void get_absorbed_in_void_space();
+    void reflect_on_surface_and_propagate_on(const Interaction type);
+    void reach_boundary_layer();
+    void fresnel_refraction_and_reflection();
+    void pass_the_boundary_layer(
+        const FresnelRefractionAndReflection &fresnel);
+    void propagate_on_after_boundary_layer(
+        const FresnelRefractionAndReflection &fresnel);
+    void get_absorbed_on_surface();
+};
+
+}  // namespace PhotonAndFrame
+
+#endif  // MCTRACER_CORE_PHOTONANDFRAME_H_
