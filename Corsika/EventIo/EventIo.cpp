b74b2554f03b50f8b382203ba8263c93d65dd123
sebastian <sebmuell@phys.ethz.ch>
2017-12-08 15:48:04 +0100

    cpplint eventio, ignore casting yet

--- Corsika/EventIo/EventIo.cpp
+++ Corsika/EventIo/EventIo.cpp
@@ -1,22 +1,32 @@
-#include <sstream>
+// Copyright 2016 Sebastian A. Mueller, Dominik Neise
 #include <cmath>
-#include "EventIo.h"
+#include <sstream>
+#include "Corsika/EventIo/EventIo.h"
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::stringstream;
+using std::vector;
+using std::array;
+using std::istream;
+using std::ifstream;
 
 namespace EventIo {
-//------------------------------------------------------------------------------
+
 Header::Header(istream& f, bool top_level) {
     size_t _start_pos = f.tellg();
     FirstFour first_four;
     if (top_level) {
         f.read((char*)&first_four, sizeof(FirstFour) );
         is_sync = check_if_sync(first_four.sync);
-    }else { 
+    } else {
         // sub level headers do not have the 'sync' field.
         // sub level headers are therefore always considered, synced.
         f.read((char*)&(first_four.type), sizeof(FirstFour)-sizeof(int32_t));
-        is_sync = true; 
+        is_sync = true;
     }
-    
+
     if (!is_sync) {
         stringstream out;
         out << "In File/Function/Line:" << __FILE__;
@@ -27,11 +37,11 @@ Header::Header(istream& f, bool top_level) {
         f.seekg(_start_pos, f.beg);
         throw NoSyncFoundException(out.str());
     }
-    
+
     id = first_four.id;
     TypeInfo type_info(first_four.type);
     LengthInfo length_info(first_four.length);
-    
+
     type = type_info.type;
     version = type_info.version;
     user = type_info.user;
@@ -40,13 +50,13 @@ Header::Header(istream& f, bool top_level) {
 
     if (!type_info.extended) {
         length = length_info.length;
-    }else {
+    } else {
         int32_t extended;
         f.read((char*)&extended, sizeof(int32_t));
         length = extend_length(extended, length_info);
-    }        
+    }
 }
-//------------------------------------------------------------------------------
+
 int64_t Header::extend_length(int32_t extended, const LengthInfo length_info) {
     int64_t ext, len;
 
@@ -55,28 +65,28 @@ int64_t Header::extend_length(int32_t extended, const LengthInfo length_info) {
 
     len = length_info.length;
 
-    len &= (ext<<12);
+    len &= (ext << 12);
     return len;
 }
-//------------------------------------------------------------------------------
+
 bool Header::check_if_sync(int32_t _sync) {
     int32_t sync = -736130505;
     return (_sync == sync);
 }
-//------------------------------------------------------------------------------
+
 Header::TypeInfo::TypeInfo(int32_t _type) {
     type = _type & 0xffff;
     version = (_type & 0xfff00000) >> 20;
     user = bool(_type & (1<<16));
     extended = bool(_type & (1<<17));
 }
-//------------------------------------------------------------------------------
+
 Header::LengthInfo::LengthInfo(int32_t _length) {
-    only_sub_objects = bool(_length & 1<<30);    
+    only_sub_objects = bool(_length & 1<<30);
     // bit 31 of length is reserved
     length = _length &  0x3fffffff;
 }
-//------------------------------------------------------------------------------
+
 string Header::str() {
     stringstream out;
     out << "HEADER" << endl;
@@ -91,7 +101,7 @@ string Header::str() {
     out << "int32_t id " << id << endl;
     return out.str();
 }
-//------------------------------------------------------------------------------
+
 array<float, 273> make_corsika_273float_sub_block_form_stream(istream& f) {
     // read the first integer to get the size
     int32_t n;
@@ -108,12 +118,12 @@ array<float, 273> make_corsika_273float_sub_block_form_stream(istream& f) {
     f.read((char*)block.data(), block.size()*sizeof(float));
     return block;
 }
-//------------------------------------------------------------------------------
+
 array<float, 273> make_run_end_from_stream(istream& f) {
     // read the first integer to get the size
     int32_t n;
     f.read((char*)&n, sizeof(n));
-    if (n != 3){
+    if (n != 3) {
         stringstream out;
         out << "In File:" << __FILE__ << endl;
         out << "in function:" << __func__ << endl;
@@ -126,7 +136,7 @@ array<float, 273> make_run_end_from_stream(istream& f) {
     f.read((char*)block.data(), block.size()*sizeof(float));
     return block;
 }
-//------------------------------------------------------------------------------
+
 string make_input_card_from_stream(istream& f, const Header& head) {
     vector<char> input_card(head.length+1);
     f.read(input_card.data(), head.length);
@@ -134,37 +144,37 @@ string make_input_card_from_stream(istream& f, const Header& head) {
 
     string input_card_text;
 
-    for(size_t i=0; i<head.length; i++)
+    for (size_t i = 0; i < head.length; i++)
         input_card_text.push_back(input_card[i]);
 
     return input_card_text;
 }
-//------------------------------------------------------------------------------
+
 vector<TelPos> make_telescope_positions(istream& f, const Header& head) {
     int32_t ntel;
     f.read((char*)&ntel, sizeof(ntel));
 
     int number_of_following_arrays = int((head.length - 4) / ntel /4);
 
-    if (number_of_following_arrays != 4){
+    if (number_of_following_arrays != 4) {
         stringstream out;
         out << "In File:" << __FILE__ << endl;
         out << "in function:" << __func__ << endl;
         out << "Line:" << __LINE__ << endl;
-        out << " number_of_following_arrays is:" ;
+        out << " number_of_following_arrays is:";
         out <<  number_of_following_arrays << endl;
         throw std::runtime_error(out.str());
     }
     vector<TelPos> telescope_positions(ntel);
 
     f.read(
-        (char*)telescope_positions.data(), 
+        (char*)telescope_positions.data(),
         telescope_positions.size()*sizeof(TelPos));
 
     return telescope_positions;
 }
-//------------------------------------------------------------------------------
-string TelOffset::str() const{
+
+string TelOffset::str()const {
     stringstream out;
     out << "TelOffset\n";
     out << "---------\n";
@@ -174,9 +184,9 @@ string TelOffset::str() const{
     out << "weight " << weight << endl;
     return out.str();
 }
-//------------------------------------------------------------------------------
+
 vector<TelOffset> make_telescope_offsets_from_stream(
-    istream& f, 
+    istream& f,
     const Header& head
 ) {
     int length_first_two = 4 + 4;
@@ -188,15 +198,15 @@ vector<TelOffset> make_telescope_offsets_from_stream(
 
     vector<float> xoff(narray);
     vector<float> yoff(narray);
-    vector<float> weight(narray, 1.0); // we initialize
+    vector<float> weight(narray, 1.0);  // we initialize
     vector<TelOffset> telescope_offsets(narray);
 
     f.read((char*)xoff.data(), xoff.size()*sizeof(float));
     f.read((char*)yoff.data(), yoff.size()*sizeof(float));
 
-    int number_of_following_arrays = int((head.length - length_first_two) / narray /4);
-    switch (number_of_following_arrays)
-    {
+    int number_of_following_arrays = int(
+        (head.length - length_first_two) / narray /4);
+    switch (number_of_following_arrays) {
         case 2:
             // do nothing, we already read the 2 arrays xoff and yoff.
             break;
@@ -208,28 +218,31 @@ vector<TelOffset> make_telescope_offsets_from_stream(
             out << "In File:" << __FILE__ << endl;
             out << "in function:" << __func__ << endl;
             out << "Line:" << __LINE__ << endl;
-            out << "number_of_following_arrays is " << number_of_following_arrays << endl;
+            out << "number_of_following_arrays is ";
+            out << number_of_following_arrays << endl;
             out << "but only 2 or 3 are allowed." << endl;
             throw std::runtime_error(out.str());
     }
-    
-    int i=0;
-    for(auto tel_off=telescope_offsets.begin(); tel_off != telescope_offsets.end(); tel_off++, i++)
-    {
+
+    int i = 0;
+    for (auto tel_off = telescope_offsets.begin();
+        tel_off != telescope_offsets.end();
+        tel_off++, i++
+    ) {
         tel_off->toff = toff;
         tel_off->xoff = xoff[i];
         tel_off->yoff = yoff[i];
         tel_off->weight = weight[i];
-    }            
+    }
     return telescope_offsets;
 }
-//------------------------------------------------------------------------------
+
 vector<array<float, 8>> make_photons_from_stream(istream& f) {
     Header subhead(f, false);
     if (subhead.type != 1205) {
         int header_length = subhead.extended ? 4 : 3;
         f.seekg(header_length*-4, f.cur);
-        //TODO: put useful text.
+        // TODO(dneise): put useful text.
         stringstream info;
         info << __FILE__ << " " << __LINE__ <<"\n";
         throw WrongTypeException(info.str());
@@ -246,42 +259,41 @@ vector<array<float, 8>> make_photons_from_stream(istream& f) {
 
     vector<array<float, 8>> bunches(b_head.n_bunches);
 
-    if(is_compact)
-        for(size_t row=0; row<bunches.size(); row++)
-            for(size_t field=0; field<8; field++)
-                bunches.at(row).at(field) = 
-                    float(((int16_t*)buff.data())[row*8 + field]);             
-    else
-        for(size_t row=0; row<bunches.size(); row++)
-            for(size_t field=0; field<8; field++)
-                bunches.at(row).at(field) = 
-                    float(((float*)buff.data())[row*8 + field]); 
+    if (is_compact) {
+        for (size_t row = 0; row < bunches.size(); row++)
+            for (size_t field = 0; field < 8; field++)
+                bunches.at(row).at(field) =
+                    float(((int16_t*)buff.data())[row*8 + field]);
+    } else {
+        for (size_t row = 0; row < bunches.size(); row++)
+            for (size_t field = 0; field < 8; field++)
+                bunches.at(row).at(field) =
+                    float(((float*)buff.data())[row*8 + field]);
+    }
 
-    if(is_compact)
-        for(size_t row=0; row<bunches.size(); row++) {
-            bunches[row][0] *= 0.1;     
-            bunches[row][1] *= 0.1;     
-            bunches[row][2] /= 30000.;  
-            bunches[row][3] /= 30000.;  
+    if (is_compact) {
+        for (size_t row = 0; row < bunches.size(); row++) {
+            bunches[row][0] *= 0.1;
+            bunches[row][1] *= 0.1;
+            bunches[row][2] /= 30000.;
+            bunches[row][3] /= 30000.;
             bunches[row][4] *= 0.1;
-            bunches[row][5] = pow(10, bunches[row][5] * 0.001);  
-            bunches[row][6] *= 0.01;    
-            //bunches[row][7] *= 1.;
+            bunches[row][5] = pow(10, bunches[row][5] * 0.001);
+            bunches[row][6] *= 0.01;
+            // bunches[row][7] *= 1.;
         }
-
-    return bunches;    
+    }
+    return bunches;
 }
-//------------------------------------------------------------------------------
+
 bool Run::has_still_events_left()const {
     return !this->run_end_found;
 }
-//------------------------------------------------------------------------------
-Run::Run(string path):
-    run_end_found(false)
-{
+
+Run::Run(string path): run_end_found(false) {
     this->path = path;
     f.open(path);
-    if(!f.is_open()) {
+    if (!f.is_open()) {
         stringstream info;
         info << __FILE__ " " << __LINE__ << "\n";
         info << "Can not open file: " << path << "\n";
@@ -292,7 +304,7 @@ Run::Run(string path):
 
     this->_current_photon_data = this->_next();
 }
-//------------------------------------------------------------------------------
+
 void Run::__read_run_header() {
     this->__get_header(1200);
     this->header.raw = make_corsika_273float_sub_block_form_stream(this->f);
@@ -303,31 +315,34 @@ void Run::__read_run_header() {
     auto header_3 = this->__get_header(1201);
     this->header.tel_pos = make_telescope_positions(this->f, header_3);
 }
-//------------------------------------------------------------------------------
+
 void Run::__read_event_header() {
     this->__get_header(1202);
-    this->current_event_header.raw = make_corsika_273float_sub_block_form_stream(this->f);
+    this->current_event_header.raw =
+        make_corsika_273float_sub_block_form_stream(this->f);
 
     auto header_2 = this->__get_header(1203);
-    this->current_event_header.telescope_offsets = make_telescope_offsets_from_stream(this->f, header_2);
+    this->current_event_header.telescope_offsets =
+        make_telescope_offsets_from_stream(this->f, header_2);
 }
-//------------------------------------------------------------------------------
+
 void Run::__read_event_end() {
     this->__get_header(1209);
-    this->current_event_end = make_corsika_273float_sub_block_form_stream(this->f);
+    this->current_event_end =
+        make_corsika_273float_sub_block_form_stream(this->f);
 }
-//------------------------------------------------------------------------------
+
 void Run::__read_run_end() {
     this->__get_header(1210);
     this->end = make_run_end_from_stream(this->f);
 }
-//------------------------------------------------------------------------------
-Header Run::__get_header(int expect_type) {   
+
+Header Run::__get_header(int expect_type) {
     Header header(this->f);
-    if(header.type != expect_type) {
+    if (header.type != expect_type) {
         int header_length = header.extended ? 5 : 4;
         this->f.seekg(header_length*-4, this->f.cur);
-        //TODO: put useful text.
+        // TODO(dneise): put useful text.
         stringstream info;
         info << __FILE__ << " " << __LINE__ <<"\n";
         info << __func__ << "()\n";
@@ -338,31 +353,29 @@ Header Run::__get_header(int expect_type) {
     }
     return header;
 }
-//------------------------------------------------------------------------------
+
 Event Run::next_event() {
     Event event;
 
     event.header = this->current_event_header;
     event.photons = this->_current_photon_data;
-    
+
     this->_current_photon_data = this->_next();
 
     return event;
 }
-//------------------------------------------------------------------------------
+
 vector<array<float, 8>> Run::_next() {
     bool something_found = false;
-    while(!this->run_end_found) {
+    while (!this->run_end_found) {
         something_found = false;
-        try
-        {
+        try {
             return make_photons_from_stream(this->f);
             something_found = true;
         }
         catch (WrongTypeException& e) { /*nothing to do*/ }
 
-        try
-        {
+        try {
             this->__get_header(1204);
             something_found = true;
         }
@@ -370,24 +383,21 @@ vector<array<float, 8>> Run::_next() {
         catch (NoSyncFoundException& e) { /*nothing to do*/ }
 
 
-        try
-        {
+        try {
             this->__read_event_header();
             something_found = true;
         }
         catch (WrongTypeException& e) { /*nothing to do*/ }
         catch (NoSyncFoundException& e) { /*nothing to do*/ }
 
-        try
-        {
+        try {
             this->__read_event_end();
-            something_found = true;            
+            something_found = true;
         }
         catch (WrongTypeException& e) { /*nothing to do*/ }
         catch (NoSyncFoundException& e) { /*nothing to do*/ }
 
-        try
-        {
+        try {
             this->__read_run_end();
             something_found = true;
             this->run_end_found = true;
@@ -395,13 +405,14 @@ vector<array<float, 8>> Run::_next() {
         catch (WrongTypeException& e) { /*nothing to do*/ }
         catch (NoSyncFoundException& e) { /*nothing to do*/ }
 
-        if(!something_found) {
-            throw std::runtime_error("Not a single valid structure found in file.");
+        if (!something_found) {
+            throw std::runtime_error(
+                "Not a single valid structure found in file.");
         }
     }
 
     vector<array<float, 8>> dummy;
     return dummy;
 }
-//------------------------------------------------------------------------------
-} //namespace EventIo
+
+}  // namespace EventIo
--- Corsika/EventIo/EventIo.h
+++ Corsika/EventIo/EventIo.h
@@ -1,151 +1,148 @@
-//=================================
-// include guard
-#ifndef __CorsikaEventIo_H_INCLUDED__
-#define __CorsikaEventIo_H_INCLUDED__
+// Copyright 2016 Sebastian A. Mueller, Dominik Neise
+#ifndef MCTRACER_CORSIKA_EVENTIO_EVENTIO_H_
+#define MCTRACER_CORSIKA_EVENTIO_EVENTIO_H_
 
+#include <string>
 #include <vector>
 #include <array>
 #include <iostream>
 #include <fstream>
 
-using std::cout;
-using std::endl;
-using std::string;
-using std::stringstream;
-using std::vector;
-using std::array;
-using std::istream;
-using std::ifstream;
-
 namespace EventIo {
 
-    class WrongTypeException :public std::runtime_error {
-        using runtime_error::runtime_error;
+class WrongTypeException :public std::runtime_error {
+    using runtime_error::runtime_error;
+};
+
+class NoSyncFoundException :public std::runtime_error {
+    using runtime_error::runtime_error;
+};
+
+struct TelPos {
+    float x, y, z, r;
+    std::string str() const;
+};
+
+struct TelOffset {
+    float toff;
+    float xoff;
+    float yoff;
+    float weight;
+    std::string str() const;
+};
+
+struct EventIoRunHeader {
+    std::array<float, 273> raw;
+    std::string input_card;
+    std::vector<TelPos> tel_pos;
+};
+
+struct EventIoEventHeader {
+    std::array<float, 273> raw;
+    std::vector<TelOffset> telescope_offsets;
+};
+
+struct Event {
+    EventIoEventHeader header;
+    std::vector<std::array<float, 8>> photons;
+};
+
+class Header{
+ public:
+    bool is_sync;
+    int32_t type;
+    int32_t version;
+    bool user;
+    bool extended;
+    bool only_sub_objects;
+    uint64_t length;
+    int32_t id;
+    explicit Header(std::istream& f, bool top_level = true);
+    std::string str();
+
+ private:
+    struct FirstFour {
+        int32_t sync;
+        int32_t type;
+        int32_t id;
+        int32_t length;
     };
 
-    class NoSyncFoundException :public std::runtime_error {
-        using runtime_error::runtime_error;
-    };
-    
-    struct TelPos {
-        float x,y,z,r;
-        string str() const;
-    };
-
-    struct TelOffset {
-        float toff;
-        float xoff;
-        float yoff;
-        float weight;
-        string str() const;
-    };
-
-    struct EventIoRunHeader {
-        array<float, 273> raw;
-        string input_card;
-        vector<TelPos> tel_pos;
-    };
-
-    struct EventIoEventHeader {
-        array<float, 273> raw;
-        vector<TelOffset> telescope_offsets;
-    };
-
-    struct Event {
-        EventIoEventHeader header;
-        vector<array<float, 8>> photons;
-    };
-
-    class Header{
-    public:
-
-        bool is_sync;
+    struct TypeInfo {
         int32_t type;
         int32_t version;
         bool user;
         bool extended;
+        explicit TypeInfo(int32_t _type);
+    };
+
+    struct LengthInfo {
         bool only_sub_objects;
-        uint64_t length;
-        int32_t id;
-        Header(istream& f, bool top_level=true);
-        string str();
-    private:
-
-        struct FirstFour {
-            int32_t sync;
-            int32_t type;
-            int32_t id;
-            int32_t length;
-        };
-
-        struct TypeInfo {
-            int32_t type;
-            int32_t version;
-            bool user;
-            bool extended;
-            TypeInfo(int32_t _type);
-        };
-
-        struct LengthInfo {
-            bool only_sub_objects;
-            int32_t length;
-            LengthInfo(int32_t _length);
-        };
-
-        bool check_if_sync(int32_t _sync);
-        int64_t extend_length(int32_t extended, const LengthInfo length_info);
+        int32_t length;
+        explicit LengthInfo(int32_t _length);
     };
 
-    struct BunchHeader {
-        int16_t array;
-        int16_t tel;
-        float photons;
-        int32_t n_bunches;
-        string str() const;
-    };
+    bool check_if_sync(int32_t _sync);
+    int64_t extend_length(int32_t extended, const LengthInfo length_info);
+};
 
-    array<float, 273> make_corsika_273float_sub_block_form_stream(istream& f);
-    string make_input_card_from_stream(istream& f, const Header& head);
-    vector<TelPos> make_telescope_positions(istream& f, const Header& head);
-    vector<TelOffset> make_telescope_offsets_from_stream(istream& f, const Header& head);
+struct BunchHeader {
+    int16_t array;
+    int16_t tel;
+    float photons;
+    int32_t n_bunches;
+    std::string str() const;
+};
 
-    /* returns photon bunches as 2D float Matrix: with shape: (N x 8)
-     * 
-     *  the 8 floats per 'row' stand for:
-     *  * x in cm
-     *  * y in cm
-     *  * cx
-     *  * cy
-     *  * time in nanoseconds since first interaction.
-     *  * zem
-     *  * photons
-     *  * wavelength is in nanometer negative if scattered ?! (whatever this means)
-     */
-    vector<array<float, 8>> make_photons_from_stream(istream& f, const Header& head);
+std::array<float, 273> make_corsika_273float_sub_block_form_stream(
+    std::istream& f);
+std::string make_input_card_from_stream(std::istream& f, const Header& head);
+std::vector<TelPos> make_telescope_positions(
+    std::istream& f,
+    const Header& head);
+std::vector<TelOffset> make_telescope_offsets_from_stream(
+    std::istream& f,
+    const Header& head);
 
-    class Run {
+/* returns photon bunches as 2D float Matrix: with shape: (N x 8)
+ *
+ *  the 8 floats per 'row' stand for:
+ *  * x in cm
+ *  * y in cm
+ *  * cx
+ *  * cy
+ *  * time in nanoseconds since first interaction.
+ *  * zem
+ *  * photons
+ *  * wavelength is in nanometer negative if scattered ?! (whatever this means)
+ */
+std::vector<std::array<float, 8>> make_photons_from_stream(
+    std::istream& f,
+    const Header& head);
 
-        ifstream f;
-        string path;
-        bool run_end_found;
-        vector<array<float, 8>> _current_photon_data;
+class Run {
+    std::ifstream f;
+    std::string path;
+    bool run_end_found;
+    std::vector<std::array<float, 8>> _current_photon_data;
 
-        Header __get_header(int expect_type);
-        void __read_run_header();
-        void __read_event_header();
-        void __read_event_end();
-        void __read_run_end();
-        vector<array<float, 8>> _next();
-        array<float, 273> current_event_end;
-        EventIoEventHeader current_event_header;
-    public:
+    Header __get_header(int expect_type);
+    void __read_run_header();
+    void __read_event_header();
+    void __read_event_end();
+    void __read_run_end();
+    std::vector<std::array<float, 8>> _next();
+    std::array<float, 273> current_event_end;
+    EventIoEventHeader current_event_header;
 
-        bool has_still_events_left()const;
-        EventIoRunHeader header;
-        array<float, 273> end;
-        Run(const string path);
-        Event next_event();
-    };
-} //namespace EventIo
+ public:
+    bool has_still_events_left()const;
+    EventIoRunHeader header;
+    std::array<float, 273> end;
+    explicit Run(const std::string path);
+    Event next_event();
+};
 
-#endif // __CorsikaEventIo_H_INCLUDED__ 
+}  // namespace EventIo
+
+#endif  // MCTRACER_CORSIKA_EVENTIO_EVENTIO_H_
