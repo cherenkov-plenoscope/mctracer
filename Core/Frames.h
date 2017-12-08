fe2a26b725f6f928b0b63fdcceb89a11485870ab
sebastian <sebmuell@phys.ethz.ch>
2017-12-08 09:34:11 +0100

    cpplint Frames

--- Core/Frames.h
+++ Core/Frames.h
@@ -1,22 +1,21 @@
-//=================================
-// include guard
-#ifndef __FRAMES_H_INCLUDED__
-#define __FRAMES_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef MCTRACER_CORE_FRAMES_H_
+#define MCTRACER_CORE_FRAMES_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
+#include <vector>
 #include "Core/Frame.h"
 
 namespace Frames {
-	double spread_of_frame_position_in_mother(const std::vector<Frame*> &frames);
-	Vec3 bounding_sphere_center(const std::vector<Frame*> &frames);
-    Vec3 dumb_bounding_sphere_center(const std::vector<Frame*> &frames);
-    Vec3 mean_of_positions_in_mother(const std::vector<Frame*> &frames);
-    bool positions_in_mother_are_too_close_together(const std::vector<Frame*> &frames);
-    double bounding_sphere_radius(const std::vector<Frame*> &frames, const Vec3 center);
-}
 
-#endif // __FRAMES_H_INCLUDED__
+double spread_of_frame_position_in_mother(const std::vector<Frame*> &frames);
+Vec3 bounding_sphere_center(const std::vector<Frame*> &frames);
+Vec3 dumb_bounding_sphere_center(const std::vector<Frame*> &frames);
+Vec3 mean_of_positions_in_mother(const std::vector<Frame*> &frames);
+bool positions_in_mother_are_too_close_together(
+    const std::vector<Frame*> &frames);
+double bounding_sphere_radius(
+    const std::vector<Frame*> &frames, const Vec3 center);
+
+}  // namespace Frames
+
+#endif  // MCTRACER_CORE_FRAMES_H_
