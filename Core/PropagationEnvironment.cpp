fa6972cecdb546258e3b3894c68e79a415d79588
sebastian <sebmuell@phys.ethz.ch>
2017-12-08 09:36:57 +0100

    cpplint Prop Evn

--- Core/PropagationEnvironment.cpp
+++ Core/PropagationEnvironment.cpp
@@ -1,5 +1,5 @@
-#include "PropagationEnvironment.h"
-//------------------------------------------------------------------------------
-PropagationEnvironment PropagationEnvironment::default_environment = 
-	PropagationEnvironment();
-//------------------------------------------------------------------------------
+// Copyright 2014 Sebastian A. Mueller
+#include "Core/PropagationEnvironment.h"
+
+PropagationEnvironment PropagationEnvironment::default_environment =
+    PropagationEnvironment();
--- Core/PropagationEnvironment.h
+++ Core/PropagationEnvironment.h
@@ -1,26 +1,18 @@
-//=================================
-// include guard
-#ifndef __PropagationEnvironment_H_INCLUDED__
-#define __PropagationEnvironment_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef MCTRACER_CORE_PROPAGATIONENVIRONMENT_H_
+#define MCTRACER_CORE_PROPAGATIONENVIRONMENT_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
 #include "Core/Frame.h"
 #include "Core/PropagationConfig.h"
 #include "Core/Random/Random.h"
 
 class PropagationEnvironment {
-public:
-
-	const Frame* world_geometry = &Frame::void_frame;
-	const PropagationConfig* propagation_options = 
-		&PropagationConfig::default_config;
-	Random::Generator* random_engine =
-		&Random::void_generator;
-		
-	static PropagationEnvironment default_environment;
+ public:
+    const Frame* world_geometry = &Frame::void_frame;
+    const PropagationConfig* propagation_options =
+        &PropagationConfig::default_config;
+    Random::Generator* random_engine =
+        &Random::void_generator;
+    static PropagationEnvironment default_environment;
 };
-#endif // __PropagationEnvironment_H_INCLUDED__ 
+#endif  // MCTRACER_CORE_PROPAGATIONENVIRONMENT_H_
