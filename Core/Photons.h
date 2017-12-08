e0270e4c43be58c6cef110367e4c1c3e6e743f9d
sebastian <sebmuell@phys.ethz.ch>
2017-12-08 10:03:19 +0100

    cpplint Photons

--- Core/Photons.h
+++ Core/Photons.h
@@ -1,69 +1,61 @@
-//=================================
-// include guard
-#ifndef __PHOTONBUNCH_H_INCLUDED__
-#define __PHOTONBUNCH_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef MCTRACER_CORE_PHOTONS_H_
+#define MCTRACER_CORE_PHOTONS_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
-#include "Core/Photon.h"
-#include "Core/PropagationEnvironment.h"
 #include <vector>
 #include <string>
+#include "Core/Photon.h"
+#include "Core/PropagationEnvironment.h"
 
-//=================================
 namespace Photons {
 
-	std::string get_print(const std::vector<Photon> *photons);
-	// propagation
-	void propagate_photons_in_scenery_with_settings(
-		std::vector<Photon> *photons,
-		const Frame *world, 
-		const PropagationConfig *settings,
-		Random::Generator* prng
-	);	
+std::string get_print(const std::vector<Photon> *photons);
+// propagation
+void propagate_photons_in_scenery_with_settings(
+    std::vector<Photon> *photons,
+    const Frame *world,
+    const PropagationConfig *settings,
+    Random::Generator* prng);
 
-	void propagate_photons_using_single_thread(
-		std::vector<Photon> *photons,
-		const Frame* world, 
-		const PropagationConfig* settings,
-		Random::Generator* prng
-	);
+void propagate_photons_using_single_thread(
+    std::vector<Photon> *photons,
+    const Frame* world,
+    const PropagationConfig* settings,
+    Random::Generator* prng);
 
-	void propagate_photons_using_multi_thread(
-		std::vector<Photon> *photons,
-		const Frame* world, 
-		const PropagationConfig* settings
-	);
+void propagate_photons_using_multi_thread(
+    std::vector<Photon> *photons,
+    const Frame* world,
+    const PropagationConfig* settings);
 
-	// In Out to raw matrix vec<vec<d>>
-	// Can be given to AsciiIO to write/read to/from text file
-	std::vector<Photon> raw_matrix2photons(std::vector<std::vector<double> > raw_matrix);
-	std::vector<std::vector<double> > photons2raw_matrix(std::vector<Photon> *photons);
-	std::vector<double> photon2raw_row(Photon* ph);
-	Photon raw_row2photon(std::vector<double> &raw_row);
-	void assert_raw_row_size_matches_photon(std::vector<double> &raw_row);
+// In Out to raw matrix vec<vec<d>>
+// Can be given to AsciiIO to write/read to/from text file
+std::vector<Photon> raw_matrix2photons(
+    std::vector<std::vector<double> > raw_matrix);
+std::vector<std::vector<double> > photons2raw_matrix(
+    std::vector<Photon> *photons);
+std::vector<double> photon2raw_row(Photon* ph);
+Photon raw_row2photon(std::vector<double> &raw_row);
+void assert_raw_row_size_matches_photon(std::vector<double> &raw_row);
 
-	// creation of photons
-	namespace Source {
+namespace Source {
 
-		std::vector<Photon> parallel_towards_z_from_xy_disc(
-			const double disc_radius,
-			const unsigned int number_of_photons
-		);
+std::vector<Photon> parallel_towards_z_from_xy_disc(
+    const double disc_radius,
+    const unsigned int number_of_photons);
 
-		std::vector<Photon> point_like_towards_z_opening_angle_num_photons(
-			const double opening_angle,
-			const unsigned int number_of_photons 
-		);
-	}
+std::vector<Photon> point_like_towards_z_opening_angle_num_photons(
+    const double opening_angle,
+    const unsigned int number_of_photons);
 
-	void transform_all_photons(const HomTra3 Trafo, std::vector<Photon> *photons);
-	void transform_all_photons_multi_thread(
-		const HomTra3 Trafo, 
-		std::vector<Photon> *photons
-	);
-}
-#endif // __PHOTONBUNCH_H_INCLUDED__
+}  // namespace Source
+
+void transform_all_photons(const HomTra3 Trafo, std::vector<Photon> *photons);
+void transform_all_photons_multi_thread(
+    const HomTra3 Trafo,
+    std::vector<Photon> *photons
+);
+
+}  // namespace Photons
+
+#endif  // MCTRACER_CORE_PHOTONS_H_
