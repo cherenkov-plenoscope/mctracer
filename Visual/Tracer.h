d754c87222d26ef29ebd6f5fb68c14da7c82b464
sebastian <sebmuell@phys.ethz.ch>
2017-12-08 10:27:43 +0100

    cpplint Tracer

--- Visual/Tracer.h
+++ Visual/Tracer.h
@@ -1,13 +1,7 @@
-//=================================
-// include guard
-#ifndef __VisualTracer_H_INCLUDED__
-#define __VisualTracer_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef MCTRACER_VISUAL_TRACER_H_
+#define MCTRACER_VISUAL_TRACER_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
 #include "CameraRay.h"
 #include "Config.h"
 #include "Core/Random/Random.h"
@@ -16,44 +10,41 @@
 #include "Core/Color.h"
 #include "Core/RayAndFrame.h"
 #include "Core/PropagationEnvironment.h"
-//=================================
 
 namespace Visual {
 
 class Tracer {
-public:
-	Random::Mt19937 prng;
+ public:
+    Random::Mt19937 prng;
 
-	const Frame* scenery;
-	const Config* config;
+    const Frame* scenery;
+    const Config* config;
 
-	CameraRay* cray;
-	Intersection isec;
-	Color color;
+    CameraRay* cray;
+    Intersection isec;
+    Color color;
 
-	static const double wavelength;
+    static const double wavelength;
 
-	Tracer(
-		CameraRay* cray,
-		const Frame* scenery,
-		const Config* config);
-	void trace_back();
-	void trace_back_to_object_interaction();
-	void trace_back_after_reflection();
-	void trace_back_to_boundary_layer();
-	void trace_back_to_fresnel_interaction();
-	void trace_back_to_surface();
-	void trace_back_beyond_boundary_layer(
-		const FresnelRefractionAndReflection &fresnel
-	);
-	void trace_back_to_sky_dome();
-	bool surface_iluminated_by_global_light_source()const;
-	bool surface_normal_is_facing_observer(
-	    const Intersection &intersection
-	)const;
-	Color shadow_of_sky_light()const;
+    Tracer(
+        CameraRay* cray,
+        const Frame* scenery,
+        const Config* config);
+    void trace_back();
+    void trace_back_to_object_interaction();
+    void trace_back_after_reflection();
+    void trace_back_to_boundary_layer();
+    void trace_back_to_fresnel_interaction();
+    void trace_back_to_surface();
+    void trace_back_beyond_boundary_layer(
+        const FresnelRefractionAndReflection &fresnel);
+    void trace_back_to_sky_dome();
+    bool surface_iluminated_by_global_light_source()const;
+    bool surface_normal_is_facing_observer(
+        const Intersection &intersection)const;
+    Color shadow_of_sky_light()const;
 };
 
-}//Visual
+}  // namespace Visual
 
-#endif // __VisualTracer_H_INCLUDED__ 
+#endif  // MCTRACER_VISUAL_TRACER_H_
