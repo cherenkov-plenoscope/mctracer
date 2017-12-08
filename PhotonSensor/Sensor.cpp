0adc326761067e6cb4933901e0594704bbad6bec
sebastian <sebmuell@phys.ethz.ch>
2017-12-08 20:57:00 +0100

    cpplint Sensor

--- PhotonSensor/Sensor.cpp
+++ PhotonSensor/Sensor.cpp
@@ -1,4 +1,5 @@
-#include "PhotonSensor.h"
+// Copyright 2014 Sebastian A. Mueller
+#include "PhotonSensor/Sensor.h"
 #include <math.h>
 #include <sstream>
 using std::vector;
@@ -6,132 +7,114 @@ using std::string;
 
 namespace PhotonSensor {
 
-	Sensor::Sensor(unsigned int _id, const Frame* _sensor_frame) {
-		id = _id;
-		sensor_frame = _sensor_frame;
-	}
+Sensor::Sensor(unsigned int _id, const Frame* _sensor_frame) {
+    id = _id;
+    sensor_frame = _sensor_frame;
+}
 
-	unsigned int Sensor::get_id()const {
-		return id;
-	}
+unsigned int Sensor::get_id()const {
+    return id;
+}
 
-	void Sensor::assign_photon_to_this_sensor(const Photon* photon) {
-		
-		arrival_table.emplace_back(// ArrivalInformation
-			// id
-			photon->get_simulation_truth_id(),
-			// wavelength
-			photon->get_wavelength(),
-			// arrival_time
-			photon->get_time_of_flight(),
-			// x
-			photon->get_final_intersection().
-				get_intersection_vector_in_object_system().x(),
-			// y
-			photon->get_final_intersection().
-				get_intersection_vector_in_object_system().y(),
-			// tx
-			-1.0*photon->get_final_intersection_incident_vector_in_object_frame().x(),
-			// ty
-			-1.0*photon->get_final_intersection_incident_vector_in_object_frame().y()
-		);
-	}
+void Sensor::assign_photon_to_this_sensor(const Photon* photon) {
+    arrival_table.emplace_back(  // ArrivalInformation
+        // id
+        photon->get_simulation_truth_id(),
+        // wavelength
+        photon->get_wavelength(),
+        // arrival_time
+        photon->get_time_of_flight(),
+        // x
+        photon->get_final_intersection().
+            get_intersection_vector_in_object_system().x(),
+        // y
+        photon->get_final_intersection().
+            get_intersection_vector_in_object_system().y(),
+        // tx
+        -1.0*photon->
+            get_final_intersection_incident_vector_in_object_frame().x(),
+        // ty
+        -1.0*photon->
+            get_final_intersection_incident_vector_in_object_frame().y());
+}
 
-	void Sensor::clear_history() {
-		arrival_table.clear();
-	}
+void Sensor::clear_history() {
+    arrival_table.clear();
+}
 
-	const Frame* Sensor::get_frame()const {
-		return sensor_frame;
-	}
+const Frame* Sensor::get_frame()const {
+    return sensor_frame;
+}
 
-	double Sensor::arrival_time_mean()const {
+double Sensor::arrival_time_mean()const {
+    double t = 0.0;
+    for (ArrivalInformation ph : arrival_table)
+        t += ph.arrival_time;
+    return t/arrival_table.size();
+}
 
-		double t = 0.0;
+double Sensor::x_mean()const {
+    double xm = 0.0;
+    for (ArrivalInformation ph : arrival_table)
+        xm += ph.x_intersect;
+    return xm/arrival_table.size();
+}
 
-		for(ArrivalInformation ph: arrival_table)
-			t += ph.arrival_time;
+double Sensor::y_mean()const {
+    double ym = 0.0;
+    for (ArrivalInformation ph : arrival_table)
+        ym += ph.y_intersect;
+    return ym/arrival_table.size();
+}
 
-		return t/arrival_table.size();
-	}
+double Sensor::x_std_dev()const {
+    double xm = x_mean();
+    double sx = 0.0;
+    for (ArrivalInformation ph : arrival_table)
+        sx += (ph.x_intersect - xm)*(ph.x_intersect - xm);
+    return sqrt(sx/arrival_table.size());
+}
 
-	double Sensor::x_mean()const {
+double Sensor::y_std_dev()const {
+    double ym = y_mean();
+    double sy = 0.0;
+    for (ArrivalInformation ph : arrival_table)
+        sy += (ph.y_intersect - ym)*(ph.y_intersect - ym);
+    return sqrt(sy/arrival_table.size());
+}
 
-		double xm = 0.0;
+double Sensor::point_spread_std_dev()const {
+    return hypot(x_std_dev(), y_std_dev());
+}
 
-		for(ArrivalInformation ph: arrival_table)
-			xm += ph.x_intersect;
+vector<vector<double>> Sensor::get_arrival_table()const {
+    vector<vector<double>> output_table;
+    for (ArrivalInformation ph : arrival_table) {
+        vector<double> output_row;
+        output_row.push_back(ph.x_intersect);
+        output_row.push_back(ph.y_intersect);
+        output_row.push_back(ph.theta_x);
+        output_row.push_back(ph.theta_y);
+        output_row.push_back(ph.wavelength);
+        output_row.push_back(ph.arrival_time);
+        output_row.push_back(ph.simulation_truth_id);
+        output_table.push_back(output_row);
+    }
+    return output_table;
+}
 
-		return xm/arrival_table.size();
-	}
+string Sensor::get_arrival_table_header()const {
+    std::stringstream header;
+    header << "x intersection [m]\n";
+    header << "y intersection [m]\n";
+    header << "inverse_incident cos_x [1]\n";
+    header << "inverse_incident cos_y [1] inverse_incident = ";
+    header << "(cos_x, cos_y, sqrt(1 - cos_x^2 - cos_y^2))^T\n";
+    header << "wavelength [m]\n";
+    header << "relative arrival time t [s]\n";
+    header << "photon id [1]\n";
+    header << "x\ty\tcos_x\tcos_y\twavelength\tt\tid\n";
+    return header.str();
+}
 
-	double Sensor::y_mean()const {
-
-		double ym = 0.0;
-
-		for(ArrivalInformation ph: arrival_table)
-			ym += ph.y_intersect;
-
-		return ym/arrival_table.size();
-	}
-
-	double Sensor::x_std_dev()const {
-
-		double xm = x_mean();
-		double sx = 0.0;
-
-		for(ArrivalInformation ph: arrival_table)
-			sx += (ph.x_intersect - xm)*(ph.x_intersect - xm);
-
-		return sqrt(sx/arrival_table.size());
-	}
-
-	double Sensor::y_std_dev()const {
-
-		double ym = y_mean();
-		double sy = 0.0;
-
-		for(ArrivalInformation ph: arrival_table)
-			sy += (ph.y_intersect - ym)*(ph.y_intersect - ym);
-
-		return sqrt(sy/arrival_table.size());
-	}
-
-	double Sensor::point_spread_std_dev()const {
-
-		return hypot(x_std_dev(), y_std_dev());		
-	}
-
-	vector<vector<double>> Sensor::get_arrival_table()const {
-
-		vector<vector<double>> output_table;
-
-		for(ArrivalInformation ph: arrival_table) {
-
-			vector<double> output_row;
-			output_row.push_back(ph.x_intersect);
-			output_row.push_back(ph.y_intersect);
-			output_row.push_back(ph.theta_x);
-			output_row.push_back(ph.theta_y);
-			output_row.push_back(ph.wavelength);
-			output_row.push_back(ph.arrival_time);
-			output_row.push_back(ph.simulation_truth_id);
-			output_table.push_back(output_row);
-		}
-
-		return output_table;
-	}
-
-	string Sensor::get_arrival_table_header()const {
-		std::stringstream header;
-		header << "x intersection [m]\n";
-		header << "y intersection [m]\n";
-		header << "inverse_incident cos_x [1]\n";
-		header << "inverse_incident cos_y [1] inverse_incident = (cos_x, cos_y, sqrt(1 - cos_x^2 - cos_y^2))^T\n";           
-		header << "wavelength [m]\n";
-		header << "relative arrival time t [s]\n";
-		header << "photon id [1]\n";
-		header << "x\ty\tcos_x\tcos_y\twavelength\tt\tid\n";
-		return header.str();
-	}
-} // PhotonSensor
+}  // namespace PhotonSensor
--- PhotonSensor/Sensor.h
+++ PhotonSensor/Sensor.h
@@ -1,50 +1,41 @@
-//=================================
-// include guard
-#ifndef __PHOTONSENSOR_SENSOR_H_INCLUDED__
-#define __PHOTONSENSOR_SENSOR_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef MCTRACER_PHOTONSENSOR_SENSOR_H_
+#define MCTRACER_PHOTONSENSOR_SENSOR_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
+#include <vector>
+#include <string>
 #include "Core/Photon.h"
 #include "ArrivalInformation.h"
 
-
-//=================================
 namespace PhotonSensor {
 
-	class Sensor {
-	protected:
-		
-		const Frame* sensor_frame;
-	public:
-		std::vector<ArrivalInformation> arrival_table;
+class Sensor {
+ protected:
+    const Frame* sensor_frame;
 
-		unsigned int id;
-		Sensor(unsigned int _id, const Frame* _sensor_frame);
-		unsigned int get_id()const;
-		void assign_photon_to_this_sensor(const Photon* photon);
-		void clear_history();
-		const Frame* get_frame()const;
+ public:
+    std::vector<ArrivalInformation> arrival_table;
+    unsigned int id;
+    Sensor(unsigned int _id, const Frame* _sensor_frame);
+    unsigned int get_id()const;
+    void assign_photon_to_this_sensor(const Photon* photon);
+    void clear_history();
+    const Frame* get_frame()const;
+    double x_mean()const;
+    double y_mean()const;
+    double x_std_dev()const;
+    double y_std_dev()const;
+    double point_spread_std_dev()const;
+    double arrival_time_mean()const;
+    std::vector<std::vector<double>> get_arrival_table()const;
+    std::string get_arrival_table_header()const;
+    struct FrameSensorByFramePointerCompare {
+        bool operator()(const Frame* f, const Sensor* s) {
+            return f < s->sensor_frame;
+        }
+    };
+};
 
-		double x_mean()const;
-		double y_mean()const;
-		double x_std_dev()const;
-		double y_std_dev()const;
-		double point_spread_std_dev()const;
+}  // namespace PhotonSensor
 
-		double arrival_time_mean()const;
-
-		std::vector<std::vector<double>> get_arrival_table()const;
-		std::string get_arrival_table_header()const;
-
-	    struct FrameSensorByFramePointerCompare {
-			bool operator()(const Frame* f, const Sensor* s) {
-				return f < s->sensor_frame;
-			}
-	    };
-	};
-} // PhotonSensor
-#endif // __PHOTONSENSOR_SENSOR_H_INCLUDED__ 
+#endif  // MCTRACER_PHOTONSENSOR_SENSOR_H_
