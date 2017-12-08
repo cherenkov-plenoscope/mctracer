23a18224ce1a2dc6748504ff05a2a3b45340ab31
sebastian <sebmuell@phys.ethz.ch>
2017-12-08 09:07:15 +0100

    cpplint RayAndFrame

--- Core/RayAndFrame.cpp
+++ Core/RayAndFrame.cpp
@@ -1,45 +1,54 @@
+// Copyright 2014 Sebastian A. Mueller
 #include "Core/RayAndFrame.h"
 #include <algorithm>
 using std::vector;
 
 namespace RayAndFrame {
-//------------------------------------------------------------------------------
-bool ray_support_inside_frames_bounding_sphere(const Ray* ray, const Frame *frame) {
+
+bool ray_support_inside_frames_bounding_sphere(
+    const Ray* ray,
+    const Frame *frame
+) {
     return (frame->get_position_in_world() - ray->get_support()).
         norm_is_less_equal_than(frame->get_bounding_sphere_radius());
 }
-//------------------------------------------------------------------------------
-bool ray_has_intersection_with_bounding_sphere_of(const Ray* ray, const Frame *frame) {
 
-    const double alpha = ray->get_parameter_on_ray_for_closest_distance_to_point(
-        frame->get_position_in_world()
-    );
+bool ray_has_intersection_with_bounding_sphere_of(
+    const Ray* ray,
+    const Frame *frame
+) {
+    const double alpha =
+        ray->get_parameter_on_ray_for_closest_distance_to_point(
+            frame->get_position_in_world());
 
     const Vec3 q = ray->get_pos_at(alpha);
     const Vec3 shortest_connection = frame->get_position_in_world() - q;
     const double dist_square = shortest_connection*shortest_connection;
 
-    if(dist_square > frame->get_bounding_sphere_radius()*frame->get_bounding_sphere_radius()) {
+    if (
+        dist_square >
+        frame->get_bounding_sphere_radius()*frame->get_bounding_sphere_radius()
+    ) {
         //
         // -------------+-----\--------> ray
         //              |     |
         //            __|__    > dist
         //           /  |  \  |
         //          |   +   | /
-        //           \_____/ 
+        //           \_____/
         //              <--->
         //              radius of sphere enclosing all children
         //
         // The ray does not intersect the frame's boundary sphere.
         return false;
-    }else{
-        if(alpha < 0.0){
+    } else {
+        if (alpha < 0.0) {
             // The frame support is behind the source of this ray.
             // We test whether the source of this ray is still inside of the
             // frame, i.e. inside the boundary sphere, or completley behind it.
-            if(ray_support_inside_frames_bounding_sphere(ray, frame)) {
+            if (ray_support_inside_frames_bounding_sphere(ray, frame)) {
                 // The support of the ray is inside the frame's boundary sphere
-                // 
+                //
                 //               _________
                 //              /         \                                   //
                 //    ________/_____________\________ray_direction_________\  //
@@ -50,36 +59,36 @@ bool ray_has_intersection_with_bounding_sphere_of(const Ray* ray, const Frame *f
                 //            /   /         /
                 //           /  \/________/
                 //          /
-                //       support    
-                //             
-                // The ray intersects the boundary sphere when leaving the 
+                //       support
+                //
+                // The ray intersects the boundary sphere when leaving the
                 // sphere.
                 return true;
-            }else{
-                // The support of the ray is outside and behind the frame's   
-                // boundary sphere with respect to its support vector.        
-                //                                                            
-                //               _________                                    
-                //              /         \                                   //         
+            } else {
+                // The support of the ray is outside and behind the frame's
+                // boundary sphere with respect to its support vector.
+                //
+                //               _________
+                //              /         \                                   //
                 //    ________/_____________\________ray_direction_________\  //
-                //           /               \          /                  /  
+                //           /               \          /                  /
                 //          |        x frame  |        /
                 //          |       /         |       /
                 //           \     / radius  /       /
                 //            \   /         /       /
                 //              \/________/        /
                 //                                /
-                //                             support    
-                //             
+                //                             support
+                //
                 // The ray does not intersect the boundary sphere at all since
                 // it starts behind the frame's boundary sphere.
                 return false;
             }
-        }else{
+        } else {
             // the frame support is in front of the source of this ray
-            // Closest point on ray to center of frame = 
+            // Closest point on ray to center of frame =
             // ray.support + v * ray.direction
-            // 
+            //
             // here v > 0
             //               _________
             //              /         \                                       //
@@ -90,85 +99,81 @@ bool ray_has_intersection_with_bounding_sphere_of(const Ray* ray, const Frame *f
             // support   \     / radius  /
             //            \   /         /
             //              \/________/
-            //              
-            //             
+            //
             // The ray does not intersect the frame's boundary sphere.
             return true;
         }
     }
 }
-//------------------------------------------------------------------------------
+
 Ray get_ray_transformed_in_object_system_of_frame(
-    const Ray* ray, 
+    const Ray* ray,
     const Frame* frame
 ) {
     Ray ray_in_object_system_of_frame = *ray;
     ray_in_object_system_of_frame.transform_inverse(frame->frame2world());
     return ray_in_object_system_of_frame;
 }
-//------------------------------------------------------------------------------
+
 thread_local vector<const Frame*> CausalIntersection::candidate_objects;
 thread_local vector<Intersection> CausalIntersection::candidate_intersections;
-//------------------------------------------------------------------------------
+
 Intersection first_intersection(const Ray* ray, const Frame* frame) {
     CausalIntersection intersect_calculator(ray, frame);
     return intersect_calculator.closest_intersection;
 }
-//------------------------------------------------------------------------------
-CausalIntersection::CausalIntersection(const Ray* _ray, const Frame* frame): 
-    ray(_ray) 
-{
+
+CausalIntersection::CausalIntersection(
+    const Ray* _ray,
+    const Frame* frame
+): ray(_ray) {
     candidate_objects.clear();
     candidate_intersections.clear();
     find_intersection_candidates_in_tree_of_frames(frame);
     find_intersections_in_candidate_objects();
-    calculate_closest_intersection();   
+    calculate_closest_intersection();
 }
 
 void CausalIntersection::find_intersection_candidates_in_tree_of_frames(
     const Frame* frame
 ) {
-    if(ray_has_intersection_with_bounding_sphere_of(ray ,frame)) {
-        if(frame->has_children()) { 
-            for(unsigned int i=0; i<frame->get_children()->size(); i++)
+    if (ray_has_intersection_with_bounding_sphere_of(ray, frame)) {
+        if (frame->has_children()) {
+            for (unsigned int i = 0; i < frame->get_children()->size(); i++)
                 find_intersection_candidates_in_tree_of_frames(
-                    frame->get_children()->at(i)
-                );      
-        }else
+                    frame->get_children()->at(i));
+        } else {
             candidate_objects.push_back(frame);
+        }
     }
 }
 
 void CausalIntersection::find_intersections_in_candidate_objects() {
-    for(const Frame* object : candidate_objects) {
-        Ray ray_in_object_system = 
+    for (const Frame* object : candidate_objects) {
+        Ray ray_in_object_system =
             get_ray_transformed_in_object_system_of_frame(
                 ray,
-                object
-            );
+                object);
 
         object->calculate_intersection_with(
-            &ray_in_object_system, 
-            &candidate_intersections
-        );
+            &ray_in_object_system,
+            &candidate_intersections);
     }
 }
 
 void CausalIntersection::calculate_closest_intersection() {
-    if(candidate_intersections.size() == 0)
+    if (candidate_intersections.size() == 0)
         closest_intersection = Intersection(
             &SurfaceEntity::void_object,
             ray->get_pos_at(1e4),
             ray->get_direction(),
             1e4,
-            ray->get_direction()
-        );
+            ray->get_direction());
     else
-        closest_intersection = *min_element(    
+        closest_intersection = *min_element(
             candidate_intersections.begin(),
             candidate_intersections.end(),
-            Intersection::compare
-        );
+            Intersection::compare);
 }
-//------------------------------------------------------------------------------
-} //RayAndFrame
+
+}  // namespace RayAndFrame
--- Core/RayAndFrame.h
+++ Core/RayAndFrame.h
@@ -1,51 +1,43 @@
-//=================================
-// include guard
-#ifndef __RAYANDFRAME_H_INCLUDED__
-#define __RAYANDFRAME_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef MCTRACER_CORE_RAYANDFRAME_H_
+#define MCTRACER_CORE_RAYANDFRAME_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
+#include <vector>
 #include "Core/Frame.h"
 #include "Core/Ray.h"
 #include "Core/SurfaceEntity.h"
 #include "Core/Intersection.h"
-#include <vector>
 
 namespace RayAndFrame {
-    // Frame's bounding sphere
-    bool ray_support_inside_frames_bounding_sphere(
-        const Ray* ray, 
-        const Frame *frame
-    );
-    bool ray_has_intersection_with_bounding_sphere_of(
-        const Ray* ray, 
-        const Frame* frame
-    );
+// Frame's bounding sphere
 
-    Ray get_ray_transformed_in_object_system_of_frame(
-        const Ray* ray, 
-        const Frame* frame
-    );
+bool ray_support_inside_frames_bounding_sphere(
+    const Ray* ray,
+    const Frame *frame);
 
-    Intersection first_intersection(
-        const Ray* ray, 
-        const Frame* frame
-    );
+bool ray_has_intersection_with_bounding_sphere_of(
+    const Ray* ray,
+    const Frame* frame);
 
-    struct CausalIntersection {
-        const Ray* ray;
-        static thread_local std::vector<const Frame*> candidate_objects;
-        static thread_local std::vector<Intersection> candidate_intersections;
-        
-        Intersection closest_intersection;
-        CausalIntersection(const Ray* _ray, const Frame* frame);
-    private:
-        void find_intersection_candidates_in_tree_of_frames(const Frame* frame);
-        void find_intersections_in_candidate_objects();
-        void calculate_closest_intersection();
-    };
-}
-#endif // __RAYANDFRAME_H_INCLUDED__ 
+Ray get_ray_transformed_in_object_system_of_frame(
+    const Ray* ray,
+    const Frame* frame);
+
+Intersection first_intersection(
+    const Ray* ray,
+    const Frame* frame);
+
+struct CausalIntersection {
+    const Ray* ray;
+    static thread_local std::vector<const Frame*> candidate_objects;
+    static thread_local std::vector<Intersection> candidate_intersections;
+    Intersection closest_intersection;
+    CausalIntersection(const Ray* _ray, const Frame* frame);
+ private:
+    void find_intersection_candidates_in_tree_of_frames(const Frame* frame);
+    void find_intersections_in_candidate_objects();
+    void calculate_closest_intersection();
+};
+
+}  // namespace RayAndFrame
+#endif  // MCTRACER_CORE_RAYANDFRAME_H_
