9db00846bf3683f88613b54ba8f75cd01f4a314d
sebastian <sebmuell@phys.ethz.ch>
2017-12-12 12:39:49 +0100

    cpplint raytest

--- Tests/RayTest.cpp
+++ Tests/RayTest.cpp
@@ -1,21 +1,20 @@
+// Copyright 2014 Sebastian A. Mueller
 #include "gtest/gtest.h"
 #include "Core/Ray.h"
 #include "Scenery/Primitive/Sphere.h"
 
 class RayTest : public ::testing::Test {};
-//------------------------------------------------------------------------------
-TEST_F(RayTest, Constructor_using_vectors) {
 
+TEST_F(RayTest, Constructor_using_vectors) {
   const Vec3 sup(1.2, 3.4, 5.6);
   const Vec3 dir(7.8, 9.0, 4.2);
   const Ray ray(sup, dir);
   EXPECT_EQ(sup, ray.get_support());
-  EXPECT_EQ(dir/dir.norm(), ray.get_direction()); 
+  EXPECT_EQ(dir/dir.norm(), ray.get_direction());
   EXPECT_NEAR(1.0, ray.get_direction().norm(), 1e-6);
 }
-//------------------------------------------------------------------------------
-TEST_F(RayTest, position_on_ray) {
 
+TEST_F(RayTest, position_on_ray) {
   const Vec3 sup(1.2, 3.4, 5.6);
   const Vec3 dir(7.0, 9.0, 4.0);
   const Ray ray(sup, dir);
@@ -23,18 +22,16 @@ TEST_F(RayTest, position_on_ray) {
   const Vec3 pos_on_ray = sup+dir/dir.norm()*alpha;
   EXPECT_NEAR(0.0, (pos_on_ray - ray.position_at(alpha) ).norm(), 1e-6);
 }
-//------------------------------------------------------------------------------
-TEST_F(RayTest, closest_distance_to_point) {
 
+TEST_F(RayTest, closest_distance_to_point) {
   Ray ray(Vec3::ORIGIN, Vec3(0.0, 0.0, 1.0));
   EXPECT_EQ(0.0, ray.closest_distance_to_point(Vec3::ORIGIN));
   EXPECT_EQ(1.0, ray.closest_distance_to_point(Vec3(1.0, 0.0, 0.0)));
   EXPECT_EQ(1.0, ray.closest_distance_to_point(Vec3(0.0, 1.0, 0.0)));
   EXPECT_EQ(0.0, ray.closest_distance_to_point(Vec3(0.0, 0.0, 1.0)));
 }
-//------------------------------------------------------------------------------
+
 TEST_F(RayTest, set_support_and_direction) {
-  
   Ray ray(Vec3(1.2, 3.4, 5.6), Vec3(0, 0, 1));
   ray.set_support_and_direction(Vec3(1.2, 3.4, 5.6), Vec3(1.2, 3.4, 5.6));
   EXPECT_EQ(1.0, ray.get_direction().norm());
