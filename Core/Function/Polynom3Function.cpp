544e4fd834c4a4c1039055313ba7831a1efa3435
sebastian <sebmuell@phys.ethz.ch>
2017-12-15 17:50:22 +0100

    cpplint Poly3

--- Core/Function/Polynom3Function.cpp
+++ Core/Function/Polynom3Function.cpp
@@ -1,38 +1,39 @@
-#include "Polynom3Function.h"
+// Copyright 2014 Sebastian A. Mueller
+#include "Core/Function/Polynom3Function.h"
+
 namespace Function {
-	//--------------------------------------------------------------------------
-	Polynom3::Polynom3(): a3(0.0), a2(0.0), a1(0.0), a0(0.0) {
-	}
-	//--------------------------------------------------------------------------
-	Polynom3::Polynom3(
-		const double a3, 
-		const double a2, 
-		const double a1, 
-		const double a0,
-		const Limits &limits 
-	) {
-		init(a3, a2, a1, a0, limits);
-	}
-	//--------------------------------------------------------------------------
-	void Polynom3::init(
-		const double a3, 
-		const double a2, 
-		const double a1, 
-		const double a0,
-		const Limits &limits
-	) {
-		this->a3 = a3;
-		this->a2 = a2;
-		this->a1 = a1;
-		this->a0 = a0;
-		this->limits = limits;
-	}
-	//--------------------------------------------------------------------------
-	double Polynom3::evaluate(const double x)const {
-		limits.assert_contains(x);
-		const double x2 = x*x;
-		const double x3 = x2*x;
-		return a3*x3 + a2*x2 + a1*x + a0;
-	}
-	//--------------------------------------------------------------------------
-} // namespace Function
+
+Polynom3::Polynom3(): a3(0.0), a2(0.0), a1(0.0), a0(0.0) {}
+
+Polynom3::Polynom3(
+    const double a3,
+    const double a2,
+    const double a1,
+    const double a0,
+    const Limits &limits
+) {
+    init(a3, a2, a1, a0, limits);
+}
+
+void Polynom3::init(
+    const double a3,
+    const double a2,
+    const double a1,
+    const double a0,
+    const Limits &limits
+) {
+    this->a3 = a3;
+    this->a2 = a2;
+    this->a1 = a1;
+    this->a0 = a0;
+    this->limits = limits;
+}
+
+double Polynom3::evaluate(const double x)const {
+    limits.assert_contains(x);
+    const double x2 = x*x;
+    const double x3 = x2*x;
+    return a3*x3 + a2*x2 + a1*x + a0;
+}
+
+}  // namespace Function
--- Core/Function/Polynom3Function.h
+++ Core/Function/Polynom3Function.h
@@ -1,36 +1,30 @@
-//==============================================================================
-// include guard
-#ifndef __FUNCTIONPOLYNOM3_H_INCLUDED__
-#define __FUNCTIONPOLYNOM3_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef CORE_FUNCTION_POLYNOM3FUNCTION_H_
+#define CORE_FUNCTION_POLYNOM3FUNCTION_H_
 
-//==============================================================================
-// forward declared dependencies
-
-//==============================================================================
-// included dependencies
-#include "Func1DFunction.h"
+#include "Core/Function/Func1DFunction.h"
 
 namespace Function {
-	class Polynom3 : public Func1D {
+class Polynom3 : public Func1D {
+    double a3, a2, a1, a0;
 
-		double a3, a2, a1, a0;
-	public:
-		Polynom3();
-		Polynom3(
-			const double a3, 
-			const double a2, 
-			const double a1, 
-			const double a0,
-			const Limits &limits
-		);
-		void init(
-			const double a3, 
-			const double a2, 
-			const double a1, 
-			const double a0,
-			const Limits &limits
-		);	
-		double evaluate(const double x)const;
-	};
-} // namespace Function
-#endif // __FUNCTIONPOLYNOM3_H_INCLUDED__
+ public:
+    Polynom3();
+    Polynom3(
+        const double a3,
+        const double a2,
+        const double a1,
+        const double a0,
+        const Limits &limits);
+    void init(
+        const double a3,
+        const double a2,
+        const double a1,
+        const double a0,
+        const Limits &limits);
+    double evaluate(const double x)const;
+};
+
+}  // namespace Function
+
+#endif  // CORE_FUNCTION_POLYNOM3FUNCTION_H_
