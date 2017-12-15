40ffbb8805a86b2045c41ea84e0dc501a251670e
sebastian <sebmuell@phys.ethz.ch>
2017-12-15 18:23:01 +0100

    cpplint Random.h

--- Core/Random/Random.h
+++ Core/Random/Random.h
@@ -1,23 +1,17 @@
-//==============================================================================
-// include guard
-#ifndef __RANDOM_H_INCLUDED__
-#define __RANDOM_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef CORE_RANDOM_RANDOM_H_
+#define CORE_RANDOM_RANDOM_H_
 
-//==============================================================================
-// forward declared dependencies
-
-//==============================================================================
-// included dependencies
 #include "Generator.h"
 #include "Mt19937.h"
 #include "FakeConstant.h"
 #include "SamplesFromDistribution.h"
-//==============================================================================
 
-// the Pseudo random generators
 namespace Random {
 
-	static const unsigned int ZERO_SEED = 0;
-	static Mt19937 void_generator = Mt19937(ZERO_SEED);
-}
-#endif // __RANDOM_H_INCLUDED__
+static const unsigned int ZERO_SEED = 0;
+static Mt19937 void_generator = Mt19937(ZERO_SEED);
+
+}  // namespace Random
+
+#endif  // CORE_RANDOM_RANDOM_H_
