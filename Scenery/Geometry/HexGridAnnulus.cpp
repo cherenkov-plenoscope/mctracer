eadc46a5d076a839f08aae69b9ea4e62bf4289f7
sebastian <sebmuell@phys.ethz.ch>
2017-12-15 20:34:40 +0100

    cpplint

--- Scenery/Geometry/HexGridAnnulus.cpp
+++ Scenery/Geometry/HexGridAnnulus.cpp
@@ -1,52 +1,55 @@
-#include "HexGridAnnulus.h"
+// Copyright 2014 Sebastian A. Mueller
+#include "Scenery/Geometry/HexGridAnnulus.h"
 #include <math.h>
 #include <sstream>
-//------------------------------------------------------------------------------
-HexGridAnnulus::HexGridAnnulus(const double outer_radius, const double spacing) {
-	init_grid(outer_radius, 0.0, spacing);
-}
-//------------------------------------------------------------------------------
-HexGridAnnulus::HexGridAnnulus(const double outer_radius, const double inner_radius ,const double spacing) {
-	init_grid(outer_radius, inner_radius, spacing);
-}
-//------------------------------------------------------------------------------
-void HexGridAnnulus::init_grid(
-	const double outer_radius, 
-	const double inner_radius,
-	const double spacing
+
+HexGridAnnulus::HexGridAnnulus(
+    const double outer_radius,
+    const double spacing
 ) {
-	init_unit_vectors_hex_grid_with_length(spacing);
-
-	int sample_radius = 2.0*floor(outer_radius/spacing);
-
-	for(int a=-sample_radius; a<=sample_radius; a++) {
-		for(int b=-sample_radius; b<=sample_radius; b++) {
-
-			Vec3 cell_ab = unit_hex_a*a + unit_hex_b*b;
-
-			const double cell_norm = cell_ab.norm();
-
-			if(cell_norm <= outer_radius && cell_norm >= inner_radius)
-				grid.push_back(cell_ab);
-		}
-	}
+    init_grid(outer_radius, 0.0, spacing);
 }
-//------------------------------------------------------------------------------
+
+HexGridAnnulus::HexGridAnnulus(
+    const double outer_radius,
+    const double inner_radius,
+    const double spacing
+) {
+    init_grid(outer_radius, inner_radius, spacing);
+}
+
+void HexGridAnnulus::init_grid(
+    const double outer_radius,
+    const double inner_radius,
+    const double spacing
+) {
+    init_unit_vectors_hex_grid_with_length(spacing);
+    int sample_radius = 2.0*floor(outer_radius/spacing);
+    for (int a = -sample_radius; a <= sample_radius; a++) {
+        for (int b = -sample_radius; b <= sample_radius; b++) {
+            Vec3 cell_ab = unit_hex_a*a + unit_hex_b*b;
+            const double cell_norm = cell_ab.norm();
+            if (cell_norm <= outer_radius && cell_norm >= inner_radius)
+                grid.push_back(cell_ab);
+        }
+    }
+}
+
 std::vector<Vec3> HexGridAnnulus::get_grid()const {
-	return grid;
+    return grid;
 }
-//------------------------------------------------------------------------------
-void HexGridAnnulus::init_unit_vectors_hex_grid_with_length(const double spacing) { 
-	unit_hex_b = Vec3::UNIT_Y*spacing;
-	unit_hex_a = (Vec3::UNIT_Y*0.5+Vec3::UNIT_X*sqrt(3.0)/2.0)*spacing;
+
+void HexGridAnnulus::init_unit_vectors_hex_grid_with_length(
+    const double spacing
+) {
+    unit_hex_b = Vec3::UNIT_Y*spacing;
+    unit_hex_a = (Vec3::UNIT_Y*0.5+Vec3::UNIT_X*sqrt(3.0)/2.0)*spacing;
 }
-//------------------------------------------------------------------------------
+
 std::string HexGridAnnulus::str()const {
-	std::stringstream out;
-
-	unsigned int number = 1;
-	for(Vec3 vec : grid)
-		out << number++ << " " << vec.str() << "\n";
-
-	return out.str();
+    std::stringstream out;
+    unsigned int number = 1;
+    for (Vec3 vec : grid)
+        out << number++ << " " << vec.str() << "\n";
+    return out.str();
 }
--- Scenery/Geometry/HexGridAnnulus.h
+++ Scenery/Geometry/HexGridAnnulus.h
@@ -1,35 +1,32 @@
-//=================================
-// include guard
-#ifndef __HexGridAnnulus_H_INCLUDED__
-#define __HexGridAnnulus_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef SCENERY_GEOMETRY_HEXGRIDANNULUS_H_
+#define SCENERY_GEOMETRY_HEXGRIDANNULUS_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
-#include "Core/Vec3.h"
 #include <string>
 #include <vector>
+#include "Core/Vec3.h"
 
-//=================================
 class HexGridAnnulus{
+    Vec3 unit_hex_a;
+    Vec3 unit_hex_b;
+    std::vector<Vec3> grid;
 
-	Vec3 unit_hex_a;
-	Vec3 unit_hex_b;
+ public:
+    HexGridAnnulus(
+        const double outer_radius,
+        const double spacing);
+    HexGridAnnulus(
+        const double outer_radius,
+        const double inner_radius,
+        const double spacing);
+    std::vector<Vec3> get_grid()const;
+    std::string str()const;
 
-	std::vector<Vec3> grid;
-public:
-	HexGridAnnulus(const double outer_radius, const double spacing);
-	HexGridAnnulus(const double outer_radius, const double inner_radius ,const double spacing);
-	std::vector<Vec3> get_grid()const; 
-	std::string str()const;
-private:
-	void init_unit_vectors_hex_grid_with_length(const double spacing);
-	void init_grid(
-		const double outer_radius, 
-		const double inner_radius,
-		const double spacing
-	);
+ private:
+    void init_unit_vectors_hex_grid_with_length(const double spacing);
+    void init_grid(
+        const double outer_radius,
+        const double inner_radius,
+        const double spacing);
 };
-#endif // __HexGridAnnulus_H_INCLUDED__ 
+#endif  // SCENERY_GEOMETRY_HEXGRIDANNULUS_H_
