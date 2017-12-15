c89cb6a6cf9b33456db2863759728d7b57c97bfc
sebastian <sebmuell@phys.ethz.ch>
2017-12-15 18:37:26 +0100

    cpplint SampleFromDistribution

--- Core/Random/SamplesFromDistribution.cpp
+++ Core/Random/SamplesFromDistribution.cpp
@@ -1,34 +1,32 @@
-#include "SamplesFromDistribution.h"
-//------------------------------------------------------------------------------
+// Copyright 2014 Sebastian A. Mueller
+#include "Core/Random/SamplesFromDistribution.h"
+
 namespace Random {
-//------------------------------------------------------------------------------
+
 SamplesFromDistribution::SamplesFromDistribution(
-	const Function::Func1D* _distribution
+    const Function::Func1D* _distribution
 ): distribution(_distribution) {
+    Function::LinInterpol integral_of_distribution =
+        Function::get_integral(*distribution);
 
-	Function::LinInterpol integral_of_distribution = 
-		Function::get_integral(*distribution);
+    total_integral_of_distribution =
+        integral_of_distribution.get_max_value();
 
-	total_integral_of_distribution = 
-		integral_of_distribution.get_max_value();
-
-	cumulative_distribution_function = new Function::LinInterpol(
-		Function::get_inverse(integral_of_distribution)
-	);
+    cumulative_distribution_function = new Function::LinInterpol(
+        Function::get_inverse(integral_of_distribution));
 }
-//------------------------------------------------------------------------------
+
 SamplesFromDistribution::~SamplesFromDistribution() {
-	delete cumulative_distribution_function;
+    delete cumulative_distribution_function;
 }
-//------------------------------------------------------------------------------
-double SamplesFromDistribution::draw(const double uniform_0_to_1)const {
 
-	double y = uniform_0_to_1*total_integral_of_distribution;
-	return cumulative_distribution_function->evaluate(y);
+double SamplesFromDistribution::draw(const double uniform_0_to_1)const {
+    double y = uniform_0_to_1*total_integral_of_distribution;
+    return cumulative_distribution_function->evaluate(y);
 }
-//------------------------------------------------------------------------------
+
 double SamplesFromDistribution::get_total_integral_of_distribution()const {
-	return total_integral_of_distribution;
+    return total_integral_of_distribution;
 }
-//------------------------------------------------------------------------------
-} // Random
+
+}  // namespace Random
--- Core/Random/SamplesFromDistribution.h
+++ Core/Random/SamplesFromDistribution.h
@@ -1,35 +1,24 @@
-//==============================================================================
-// include guard
-#ifndef __SamplesFromDistribution_H_INCLUDED__
-#define __SamplesFromDistribution_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef CORE_RANDOM_SAMPLESFROMDISTRIBUTION_H_
+#define CORE_RANDOM_SAMPLESFROMDISTRIBUTION_H_
 
-//==============================================================================
-// forward declared dependencies
-
-//==============================================================================
-// included dependencies
 #include "Random.h"
 #include "Core/Function/Function.h"
-//==============================================================================
 
-// the Pseudo random generators
 namespace Random {
 
-	class SamplesFromDistribution {
+class SamplesFromDistribution {
+    const Function::Func1D* distribution;
+    double total_integral_of_distribution;
+    Function::LinInterpol* cumulative_distribution_function;
+ public:
+    explicit SamplesFromDistribution(
+        const Function::Func1D* _distribution);
+    ~SamplesFromDistribution();
+    double draw(const double uniform_0_to_1)const;
+    double get_total_integral_of_distribution()const;
+};
 
-		const Function::Func1D* distribution;
+}  // namespace Random
 
-		double total_integral_of_distribution;
-		Function::LinInterpol* cumulative_distribution_function;
-	public:
-
-		SamplesFromDistribution(
-			const Function::Func1D* _distribution
-		);
-		~SamplesFromDistribution();
-		double draw(const double uniform_0_to_1)const;
-		double get_total_integral_of_distribution()const;
-	};
-
-} // Random
-#endif // __SamplesFromDistribution_H_INCLUDED__
+#endif  // CORE_RANDOM_SAMPLESFROMDISTRIBUTION_H_
