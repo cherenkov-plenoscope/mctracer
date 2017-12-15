d3b430e3f61cb679050f93e390e4f3fbbab148ad
sebastian <sebmuell@phys.ethz.ch>
2017-12-15 20:26:56 +0100

    cpplint

--- Scenery/Geometry/DualSphericalPrismZ.cpp
+++ Scenery/Geometry/DualSphericalPrismZ.cpp
@@ -1,47 +1,46 @@
-#include "DualSphericalPrismZ.h"
+// Copyright 2014 Sebastian A. Mueller
+#include "Scenery/Geometry/DualSphericalPrismZ.h"
 #include <math.h>
-//------------------------------------------------------------------------------
+
 void DualSphericalPrismZ::set_x_hight_and_y_width(
-	const double hight,
-	const double width
+    const double hight,
+    const double width
 ) {
-	this->hight = hight;
-	this->width = width;
-	circle_distance = (width*width - hight*hight)/(2.0*hight);
-	circle_radius = circle_distance/2.0 + hight/2.0;
+    this->hight = hight;
+    this->width = width;
+    circle_distance = (width*width - hight*hight)/(2.0*hight);
+    circle_radius = circle_distance/2.0 + hight/2.0;
 
-	circ1 = Vec2(+circle_distance/2.0, 0.0);
-	circ2 = Vec2(-circle_distance/2.0, 0.0);
+    circ1 = Vec2(+circle_distance/2.0, 0.0);
+    circ2 = Vec2(-circle_distance/2.0, 0.0);
 }
-//------------------------------------------------------------------------------
+
 bool DualSphericalPrismZ::is_inside(const Vec3* vec)const {
-	Vec2 v2c1 = circ1 - Vec2(vec->x, vec->y);
-	Vec2 v2c2 = circ2 - Vec2(vec->x, vec->y);
+    Vec2 v2c1 = circ1 - Vec2(vec->x, vec->y);
+    Vec2 v2c2 = circ2 - Vec2(vec->x, vec->y);
 
-	return 	v2c1.norm_is_less_equal_than(circle_radius) &&
-			v2c2.norm_is_less_equal_than(circle_radius);
+    return  v2c1.norm_is_less_equal_than(circle_radius) &&
+            v2c2.norm_is_less_equal_than(circle_radius);
 }
-//------------------------------------------------------------------------------
+
 double DualSphericalPrismZ::get_hight()const {
-	return hight;
+    return hight;
 }
-//------------------------------------------------------------------------------
-double DualSphericalPrismZ::get_width()const {
-	return width;
-}
-//------------------------------------------------------------------------------
-double DualSphericalPrismZ::get_enclosing_radius()const {
-	return width*0.5;
-}
-//------------------------------------------------------------------------------
-double DualSphericalPrismZ::get_circle_radius()const {
-	return circle_radius;
-}
-//------------------------------------------------------------------------------
-double DualSphericalPrismZ::get_area()const {
 
- 	double opening_angle_of_circle = 2.0*asin(width/(2.0*circle_radius));
- 	
- 	return 2.0*(circle_radius*circle_radius/2.0)*
- 		(opening_angle_of_circle - sin(opening_angle_of_circle));
+double DualSphericalPrismZ::get_width()const {
+    return width;
+}
+
+double DualSphericalPrismZ::get_enclosing_radius()const {
+    return width*0.5;
+}
+
+double DualSphericalPrismZ::get_circle_radius()const {
+    return circle_radius;
+}
+
+double DualSphericalPrismZ::get_area()const {
+    double opening_angle_of_circle = 2.0*asin(width/(2.0*circle_radius));
+    return 2.0*(circle_radius*circle_radius/2.0)*
+        (opening_angle_of_circle - sin(opening_angle_of_circle));
 }
