6e5782724e9f182ea98d39fe95d71449731724ad
sebastian <sebmuell@phys.ethz.ch>
2017-12-15 18:11:16 +0100

    cpplint discrete sampling

--- Core/Function/DiscretSamplingLookUpTable.h
+++ Core/Function/DiscretSamplingLookUpTable.h
@@ -1,47 +1,37 @@
-//=================================
-// include guard
-#ifndef __FunctionDiscretSamplingLookUpTable_H_INCLUDED__
-#define __FunctionDiscretSamplingLookUpTable_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef CORE_FUNCTION_DISCRETSAMPLINGLOOKUPTABLE_H_
+#define CORE_FUNCTION_DISCRETSAMPLINGLOOKUPTABLE_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
 #include "LimitsFunction.h"
 #include "Func1DFunction.h"
 #include <vector>
 
-//------------------------------------------------------------------------------
 namespace Function {
-	namespace DiscretSampling {
+namespace DiscretSampling {
 
 struct Config {
-
-	double slice_width;
-	unsigned int samples_per_slice;
-	Config();
+    double slice_width;
+    unsigned int samples_per_slice;
+    Config();
 };
 
 class LookUpTable {
+    Config config;
+    const Func1D* func;
+    std::vector<std::vector<double>> table;
+    unsigned int number_of_slices;
+    double step_width_in_slice;
 
-	Config config;
-	const Func1D* func;
+ public:
+    LookUpTable(const Func1D* func, const Config config);
+    unsigned int size()const;
+    const std::vector<double>* at(const double slice_offset)const;
 
-	std::vector<std::vector<double>> table;
-
-	unsigned int number_of_slices;
-	double step_width_in_slice;
-public: 
-
-	LookUpTable(const Func1D* func, const Config config);
-	unsigned int size()const;
-	const std::vector<double>* at(const double slice_offset)const;
-private:
-
-	void fill_table();
+ private:
+    void fill_table();
 };
 
-	}// DiscretSampling
-}// Function
-#endif // __FunctionDiscretSamplingLookUpTable_H_INCLUDED__
+}  // namespace DiscretSampling
+}  // namespace Function
+
+#endif  // CORE_FUNCTION_DISCRETSAMPLINGLOOKUPTABLE_H_
