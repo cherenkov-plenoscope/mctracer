c4527c96583313177986b6d48d89cdab252efa76
sebastian <sebmuell@phys.ethz.ch>
2017-12-15 20:40:17 +0100

    cpplint

--- Scenery/Geometry/GridNeighborhoodTopoligy.h
+++ Scenery/Geometry/GridNeighborhoodTopoligy.h
@@ -1,40 +1,27 @@
-//=================================
-// include guard
-#ifndef __GridNeighborhoodTopoligy_H_INCLUDED__
-#define __GridNeighborhoodTopoligy_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef SCENERY_GEOMETRY_GRIDNEIGHBORHOODTOPOLIGY_H_
+#define SCENERY_GEOMETRY_GRIDNEIGHBORHOODTOPOLIGY_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
-#include "Core/Vec3.h"
-#include "HexagonalPrismZ.h"
 #include <string>
 #include <vector>
+#include "Core/Vec3.h"
+#include "HexagonalPrismZ.h"
 
-//=================================
 class GridNeighborhoodTopoligy{
-
-	const std::vector<Vec3> *grid;
-	double neighborhood_radius;
-
-	std::vector<unsigned int> idx_x;
-
-	std::vector<std::vector<unsigned int>> neigbors;
-public:
-
-	GridNeighborhoodTopoligy(
-		const std::vector<Vec3> *grid,
-		const double neighborhood_radius
-	);
-
-	std::vector<std::vector<unsigned int>> get_neighbor_relations()const;
-private:
-
-	void init_idx();
-	void sort_idx();
-	unsigned int get_candidate_low_x(const double x);	
-	unsigned int get_candidate_hig_x(const double x);	
+    const std::vector<Vec3> *grid;
+    double neighborhood_radius;
+    std::vector<unsigned int> idx_x;
+    std::vector<std::vector<unsigned int>> neigbors;
+ public:
+    GridNeighborhoodTopoligy(
+        const std::vector<Vec3> *grid,
+        const double neighborhood_radius);
+    std::vector<std::vector<unsigned int>> get_neighbor_relations()const;
+ private:
+    void init_idx();
+    void sort_idx();
+    unsigned int get_candidate_low_x(const double x);
+    unsigned int get_candidate_hig_x(const double x);
 };
-#endif // __GridNeighborhoodTopoligy_H_INCLUDED__ 
+
+#endif  // SCENERY_GEOMETRY_GRIDNEIGHBORHOODTOPOLIGY_H_
