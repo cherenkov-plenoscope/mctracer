3c6fd2727492d1f651a9f3a2dc1d89edbc860709
sebastian <sebmuell@phys.ethz.ch>
2017-12-15 17:53:18 +0100

    cpplint limits

--- Core/Function/LimitsFunction.cpp
+++ Core/Function/LimitsFunction.cpp
@@ -1,60 +1,62 @@
-#include "LimitsFunction.h"
+// Copyright 2014 Sebastian A. Mueller
+#include "Core/Function/LimitsFunction.h"
 #include <sstream>
 #include <exception>
 
 namespace Function {
-	const Limits Limits::VOID_LIMITS = Limits(0.0, 0.0);
-	//--------------------------------------------------------------------------
-	Limits::Limits(const double _lower, const double _upper) {
-		lower = _lower;
-		upper = _upper;
-		assert_limits_are_causal();
-	}
-	//--------------------------------------------------------------------------
-	Limits::Limits() {
-		lower = 0.0;
-		upper = 0.0;
-		assert_limits_are_causal();
-	}
-	//--------------------------------------------------------------------------
-	void Limits::assert_limits_are_causal()const {
-		if(upper < lower) {
-			std::stringstream info;
-			info << "Numerical Function Limit in file " << __FILE__ << "\n";
-			info << "Expected upper limit u to be ";
-			info << "equal or larger the lower limit l, but actual ";
-			info << "u=" << upper << " , l=" << lower << "\n";
-			throw std::logic_error(info.str());
-		}
-	}
-	//--------------------------------------------------------------------------
-	double Limits::get_lower()const {
-		return lower;
-	}
-	//--------------------------------------------------------------------------
-	double Limits::get_upper()const {
-		return upper;
-	}
-	//--------------------------------------------------------------------------
-	double Limits::get_range()const {
-		return upper - lower;
-	}
-	//--------------------------------------------------------------------------
-	void Limits::assert_contains(const double x)const {
-		if(x < lower || x >= upper) {
-			std::stringstream info;
-			info << "Numerical Function 1D in file " << __FILE__ << "\n";
-			info << "Expected function argument x ";
-			info << "to be in limits " << lower << "<= x <";
-			info << upper << " but actual x=" << x << "\n";
-			throw std::out_of_range(info.str());
-		}	
-	}
-	//--------------------------------------------------------------------------
-	std::string Limits::str()const {
-		std::stringstream out;
-		out << "[" << lower << ", " << upper << ")";
-		return out.str();
-	}
-	//--------------------------------------------------------------------------
+
+const Limits Limits::VOID_LIMITS = Limits(0.0, 0.0);
+
+Limits::Limits(const double _lower, const double _upper) {
+    lower = _lower;
+    upper = _upper;
+    assert_limits_are_causal();
 }
+
+Limits::Limits() {
+    lower = 0.0;
+    upper = 0.0;
+    assert_limits_are_causal();
+}
+
+void Limits::assert_limits_are_causal()const {
+    if (upper < lower) {
+        std::stringstream info;
+        info << "Numerical Function Limit in file " << __FILE__ << "\n";
+        info << "Expected upper limit u to be ";
+        info << "equal or larger the lower limit l, but actual ";
+        info << "u=" << upper << " , l=" << lower << "\n";
+        throw std::logic_error(info.str());
+    }
+}
+
+double Limits::get_lower()const {
+    return lower;
+}
+
+double Limits::get_upper()const {
+    return upper;
+}
+
+double Limits::get_range()const {
+    return upper - lower;
+}
+
+void Limits::assert_contains(const double x)const {
+    if (x < lower || x >= upper) {
+        std::stringstream info;
+        info << "Numerical Function 1D in file " << __FILE__ << "\n";
+        info << "Expected function argument x ";
+        info << "to be in limits " << lower << "<= x <";
+        info << upper << " but actual x=" << x << "\n";
+        throw std::out_of_range(info.str());
+    }
+}
+
+std::string Limits::str()const {
+    std::stringstream out;
+    out << "[" << lower << ", " << upper << ")";
+    return out.str();
+}
+
+}  // namespace Function
--- Core/Function/LimitsFunction.h
+++ Core/Function/LimitsFunction.h
@@ -1,32 +1,27 @@
-//==============================================================================
-// include guard
-#ifndef __FUNCTIONLIMITS_H_INCLUDED__
-#define __FUNCTIONLIMITS_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef CORE_FUNCTION_LIMITSFUNCTION_H_
+#define CORE_FUNCTION_LIMITSFUNCTION_H_
 
-//==============================================================================
-// forward declared dependencies
-
-//==============================================================================
-// included dependencies
 #include <string>
 
 namespace Function {
 
-	class Limits{
-	
-		double lower;
-		double upper;
-	public:
-		
-		static const Limits VOID_LIMITS;
-		Limits();
-		Limits(const double _lower, const double _upper);
-		void assert_limits_are_causal()const;
-		double get_lower()const;
-		double get_upper()const;
-		double get_range()const;
-		void assert_contains(const double x)const;
-		std::string str()const;
-	};
-} // namespace Function
-#endif // __FUNCTIONLIMITS_H_INCLUDED__
+class Limits{
+    double lower;
+    double upper;
+
+ public:
+    static const Limits VOID_LIMITS;
+    Limits();
+    Limits(const double _lower, const double _upper);
+    void assert_limits_are_causal()const;
+    double get_lower()const;
+    double get_upper()const;
+    double get_range()const;
+    void assert_contains(const double x)const;
+    std::string str()const;
+};
+
+}  // namespace Function
+
+#endif  // CORE_FUNCTION_LIMITSFUNCTION_H_
