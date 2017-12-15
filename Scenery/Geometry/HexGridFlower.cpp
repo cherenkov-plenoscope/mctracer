2544bd7c3fcd7d95f0dbec469e58ea7f5864e373
sebastian <sebmuell@phys.ethz.ch>
2017-12-15 20:47:06 +0100

    cpplint

--- Scenery/Geometry/HexGridFlower.cpp
+++ Scenery/Geometry/HexGridFlower.cpp
@@ -1,62 +1,50 @@
-#include "HexGridFlower.h"
+// Copyright 2014 Sebastian A. Mueller
+#include "Scenery/Geometry/HexGridFlower.h"
+#include <math.h>
 #include "Tools/Tools.h"
-#include "math.h"
-//------------------------------------------------------------------------------
+
 HexGridFlower::HexGridFlower(
-	const double outer_hex_grid_radius,
-	unsigned int facet_count_on_outer_diameter
+    const double outer_hex_grid_radius,
+    unsigned int facet_count_on_outer_diameter
 ) {
-	// ensure diagonal count is odd
-	if(is_even(facet_count_on_outer_diameter))
-		facet_count_on_outer_diameter++;
-
-	const unsigned int facet_row_count_on_flat2flat_diameter = 
-		facet_count_on_outer_diameter;
-
-	const double inner_hex_grid_radius = outer_hex_grid_radius*(sqrt(3.0)/2.0);
-
-	const double inner_hex_grid_diameter = 2.0*inner_hex_grid_radius;
-
-	const double outer_hex_facet_radius = inner_hex_grid_diameter/(
-			0.5*(3.0*double(facet_row_count_on_flat2flat_diameter) + 1.0)
-		);
-
-	const double inner_hex_facet_radius = outer_hex_facet_radius*(sqrt(3.0)/2.0);
-
-	facet_spacing = 2.0*inner_hex_facet_radius;
-
-	hex_prism.set_outer_radius(outer_hex_grid_radius);
-	init_grid();
+    // ensure diagonal count is odd
+    if (is_even(facet_count_on_outer_diameter))
+        facet_count_on_outer_diameter++;
+    const unsigned int facet_row_count_on_flat2flat_diameter =
+        facet_count_on_outer_diameter;
+    const double inner_hex_grid_radius = outer_hex_grid_radius*(sqrt(3.0)/2.0);
+    const double inner_hex_grid_diameter = 2.0*inner_hex_grid_radius;
+    const double outer_hex_facet_radius = inner_hex_grid_diameter/(
+            0.5*(3.0*double(facet_row_count_on_flat2flat_diameter) + 1.0));
+    const double inner_hex_facet_radius =
+        outer_hex_facet_radius*(sqrt(3.0)/2.0);
+    facet_spacing = 2.0*inner_hex_facet_radius;
+    hex_prism.set_outer_radius(outer_hex_grid_radius);
+    init_grid();
 }
-//------------------------------------------------------------------------------
+
 void HexGridFlower::init_grid() {
-	init_unit_vectors_hex_grid_with_length();
-
-	int sample_radius = 2.0*floor(hex_prism.get_outer_radius()/facet_spacing);
-
-	for(int a=-sample_radius; a<=sample_radius; a++) {
-		for(int b=-sample_radius; b<=sample_radius; b++) {
-
-			Vec3 cell_ab = unit_hex_a*a + unit_hex_b*b;
-
-			if(hex_prism.is_inside(&cell_ab))
-				grid.push_back(cell_ab);
-		}
-	}
+    init_unit_vectors_hex_grid_with_length();
+    int sample_radius = 2.0*floor(hex_prism.get_outer_radius()/facet_spacing);
+    for (int a = -sample_radius; a <= sample_radius; a++) {
+        for (int b = -sample_radius; b <= sample_radius; b++) {
+            Vec3 cell_ab = unit_hex_a*a + unit_hex_b*b;
+            if (hex_prism.is_inside(&cell_ab))
+                grid.push_back(cell_ab);
+        }
+    }
 }
-//------------------------------------------------------------------------------
+
 std::vector<Vec3> HexGridFlower::get_grid()const {
-	return grid;
+    return grid;
 }
-//------------------------------------------------------------------------------
-void HexGridFlower::init_unit_vectors_hex_grid_with_length() {
 
-	unit_hex_b = Vec3::UNIT_X*facet_spacing;
-	unit_hex_a = (
-		Vec3::UNIT_X*0.5+Vec3::UNIT_Y*sqrt(3.0)/2.0
-	)*facet_spacing;
+void HexGridFlower::init_unit_vectors_hex_grid_with_length() {
+    unit_hex_b = Vec3::UNIT_X*facet_spacing;
+    unit_hex_a = (
+        Vec3::UNIT_X*0.5+Vec3::UNIT_Y*sqrt(3.0)/2.0)*facet_spacing;
 }
-//------------------------------------------------------------------------------
+
 double HexGridFlower::get_facet_spacing()const {
-	return facet_spacing;
+    return facet_spacing;
 }
--- Scenery/Geometry/HexGridFlower.h
+++ Scenery/Geometry/HexGridFlower.h
@@ -1,37 +1,29 @@
-//=================================
-// include guard
-#ifndef __HexGridFlower_H_INCLUDED__
-#define __HexGridFlower_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef SCENERY_GEOMETRY_HEXGRIDFLOWER_H_
+#define SCENERY_GEOMETRY_HEXGRIDFLOWER_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
-#include "Core/Vec3.h"
-#include "HexagonalPrismZ.h"
 #include <string>
 #include <vector>
+#include "Core/Vec3.h"
+#include "HexagonalPrismZ.h"
 
-//=================================
 class HexGridFlower{
+    Vec3 unit_hex_a;
+    Vec3 unit_hex_b;
+    std::vector<Vec3> grid;
+    HexagonalPrismZ hex_prism;
+    double facet_spacing;
 
-	Vec3 unit_hex_a;
-	Vec3 unit_hex_b;
-	std::vector<Vec3> grid;
-	HexagonalPrismZ hex_prism;
-	double facet_spacing;
-public:
+ public:
+    HexGridFlower(
+        const double outer_hex_grid_radius,
+        unsigned int facet_count_on_outer_diameter);
+    double get_facet_spacing()const;
+    std::vector<Vec3> get_grid()const;
 
-	HexGridFlower(
-		const double outer_hex_grid_radius,
-		unsigned int facet_count_on_outer_diameter
-	);
-	double get_facet_spacing()const;
-	std::vector<Vec3> get_grid()const; 
-private:
-
-	void init_unit_vectors_hex_grid_with_length();
-	void init_grid();
+ private:
+    void init_unit_vectors_hex_grid_with_length();
+    void init_grid();
 };
-#endif // __HexGridFlower_H_INCLUDED__ 
+
+#endif  // SCENERY_GEOMETRY_HEXGRIDFLOWER_H_
