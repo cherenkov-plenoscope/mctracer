dcc9c46934584da6775cf6f93ab63a9b85191ded
sebastian <sebmuell@phys.ethz.ch>
2017-12-15 20:18:21 +0100

    cpplint

--- Scenery/Geometry/DualSphericalPrismZ.h
+++ Scenery/Geometry/DualSphericalPrismZ.h
@@ -1,37 +1,29 @@
-//=================================
-// include guard
-#ifndef __DualSphericalPrismZ_H_INCLUDE__
-#define __DualSphericalPrismZ_H_INCLUDE__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef SCENERY_GEOMETRY_DUALSPHERICALPRISMZ_H_
+#define SCENERY_GEOMETRY_DUALSPHERICALPRISMZ_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
 #include "PrismZ.h"
 #include "Core/Vec2.h"
-//=================================
+
 class DualSphericalPrismZ :public PrismZ {
-private:
+ private:
+    double circle_distance;
+    double circle_radius;
+    double hight;
+    double width;
+    Vec2 circ1;
+    Vec2 circ2;
 
-	double circle_distance;
-	double circle_radius;
-	double hight;
-	double width;
-
-	Vec2 circ1;
-	Vec2 circ2;
-public:
-
-	void set_x_hight_and_y_width(
-		const double hight,
-		const double width
-	);
-	double get_hight()const;
-	double get_width()const;
-	double get_enclosing_radius()const;
-	double get_circle_radius()const;
-	double get_area()const;
-	bool is_inside(const Vec3* vec)const;
+ public:
+    void set_x_hight_and_y_width(
+        const double hight,
+        const double width);
+    double get_hight()const;
+    double get_width()const;
+    double get_enclosing_radius()const;
+    double get_circle_radius()const;
+    double get_area()const;
+    bool is_inside(const Vec3* vec)const;
 };
-#endif // __DualSphericalPrismZ_H_INCLUDE__
+
+#endif  // SCENERY_GEOMETRY_DUALSPHERICALPRISMZ_H_
