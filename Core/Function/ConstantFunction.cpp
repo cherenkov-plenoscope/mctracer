ac40d7705f24b60c6a31fd585850830ce8cc76ea
sebastian <sebmuell@phys.ethz.ch>
2017-12-15 18:03:59 +0100

    cpplint Constant

--- Core/Function/ConstantFunction.cpp
+++ Core/Function/ConstantFunction.cpp
@@ -1,24 +1,25 @@
-#include "ConstantFunction.h"
+// Copyright 2014 Sebastian A. Mueller
+#include "Core/Function/ConstantFunction.h"
 #include <sstream>
 
 namespace Function {
-	const Constant Constant::VOID_FUNCTION = Constant(0.0, Limits::VOID_LIMITS);
-	//--------------------------------------------------------------------------
-	Constant::Constant(): value(0.0) {}
-	//--------------------------------------------------------------------------
-	Constant::Constant(const double value, const Limits &limits)
-	{
-		init(value, limits);
-	}
-	//--------------------------------------------------------------------------
-	void Constant::init(const double value, const Limits &limits) {
-		this->value = value;
-		this->limits = limits;
-	}
-	//--------------------------------------------------------------------------
-	double Constant::evaluate(const double x)const {
-		limits.assert_contains(x);
-		return value;
-	}
-	//--------------------------------------------------------------------------
-} // namespace Function
+
+const Constant Constant::VOID_FUNCTION = Constant(0.0, Limits::VOID_LIMITS);
+
+Constant::Constant(): value(0.0) {}
+
+Constant::Constant(const double value, const Limits &limits) {
+    init(value, limits);
+}
+
+void Constant::init(const double value, const Limits &limits) {
+    this->value = value;
+    this->limits = limits;
+}
+
+double Constant::evaluate(const double x)const {
+    limits.assert_contains(x);
+    return value;
+}
+
+}  // namespace Function
--- Core/Function/ConstantFunction.h
+++ Core/Function/ConstantFunction.h
@@ -1,27 +1,21 @@
-//==============================================================================
-// include guard
-#ifndef __FUNCTIONCONSTANT_H_INCLUDED__
-#define __FUNCTIONCONSTANT_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef CORE_FUNCTION_CONSTANTFUNCTION_H_
+#define CORE_FUNCTION_CONSTANTFUNCTION_H_
 
-//==============================================================================
-// forward declared dependencies
-
-//==============================================================================
-// included dependencies
 #include "Func1DFunction.h"
 
 namespace Function {
 
-	class Constant :public Func1D {
+class Constant :public Func1D {
+    double value;
 
-		double value;
-	public:
-		Constant();
-		Constant(const double value, const Limits &limits);
-		void init(const double value, const Limits &limits);
-		double evaluate(const double x)const;
-		static const Constant VOID_FUNCTION;
-	};
+ public:
+    Constant();
+    Constant(const double value, const Limits &limits);
+    void init(const double value, const Limits &limits);
+    double evaluate(const double x)const;
+    static const Constant VOID_FUNCTION;
+};
 
-} // namespace Function
-#endif // __FUNCTIONCONSTANT_H_INCLUDED__
+}  // namespace Function
+#endif  // CORE_FUNCTION_CONSTANTFUNCTION_H_
