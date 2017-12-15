89c79120f06fb15c92059f345d13af3322fe6f99
sebastian <sebmuell@phys.ethz.ch>
2017-12-15 17:46:56 +0100

    cpplint

--- Core/Function/ConcatFunction.cpp
+++ Core/Function/ConcatFunction.cpp
@@ -1,4 +1,5 @@
-#include "ConcatFunction.h"
+// Copyright 2014 Sebastian A. Mueller
+#include "Core/Function/ConcatFunction.h"
 #include <sstream>
 #include <algorithm>
 #include <exception>
@@ -7,67 +8,63 @@ using std::vector;
 
 namespace Function {
 
-	Concat::Concat() {
-	}
-	//--------------------------------------------------------------------------
-	Concat::Concat(const vector<Func1D*> conc) {
-		init(conc);
-	}
-	//--------------------------------------------------------------------------
-	void Concat::init(const vector<Func1D*> conc) {
-		this->conc = conc;
-		assert_limits_do_fit();
-		adopt_new_limits();
-	}
-	//--------------------------------------------------------------------------
-	void Concat::assert_limits_do_fit()const {
+Concat::Concat() {}
 
-		for(unsigned int i=0; i<conc.size()-1; i++) {
-			if(func_does_not_match_limit_of_next_func(i)) {
-				std::stringstream info;
-				info << "Function::Concat::" << __func__ << "()\n";
-				info << __FILE__ << ", " << __LINE__ << "\n";
-				info << "Expected limits of functions to be concatenated to fit. ";
-				info << "function[" << i << "].upper_limit == ";
-				info << "function[" << i+1 << "].lower_limit, but actual\n";
-				info << "f[" << i << "].upper_limit = ";
-				info << conc.at(i)->get_limits().get_upper() << "\n";
-				info << "f[" << i+1 << "].lower_limit = ";
-				info << conc.at(i+1)->get_limits().get_lower() << "\n";
-				throw std::logic_error(info.str());			
-			}
-		}
-	}
-	//--------------------------------------------------------------------------
-	bool Concat::func_does_not_match_limit_of_next_func(const unsigned int i)const {
-		return conc.at(i)->get_limits().get_upper() != 
-			conc.at(i+1)->get_limits().get_lower();
-	}
-	//--------------------------------------------------------------------------
-	void Concat::adopt_new_limits() {
-		limits = Limits(
-			conc.front()->get_limits().get_lower(),
-			conc.back()->get_limits().get_upper()
-		);
-	}
-	//--------------------------------------------------------------------------
-	double Concat::evaluate(const double x)const {
-		limits.assert_contains(x);
-		const Func1D* sub_f = get_sub_function_responsible_for(x);
-		return sub_f->evaluate(x);
-	}
-	//--------------------------------------------------------------------------
-	const Func1D* Concat::get_sub_function_responsible_for(const double x)const {
-		return	(*	std::upper_bound(
-						conc.begin(),
-						conc.end(), 
-						x, 
-						compare_upper_limit
-					)
-				);	
-	}
-	//--------------------------------------------------------------------------
-	bool Concat::compare_upper_limit(const double x, const Func1D *f) {
-		return f->get_limits().get_upper() > x;
-	}
-} // namespace Function
+Concat::Concat(const vector<Func1D*> conc) {
+    init(conc);
+}
+
+void Concat::init(const vector<Func1D*> conc) {
+    this->conc = conc;
+    assert_limits_do_fit();
+    adopt_new_limits();
+}
+
+void Concat::assert_limits_do_fit()const {
+    for (unsigned int i = 0; i < conc.size()-1; i++) {
+        if (func_does_not_match_limit_of_next_func(i)) {
+            std::stringstream info;
+            info << "Function::Concat::" << __func__ << "()\n";
+            info << __FILE__ << ", " << __LINE__ << "\n";
+            info << "Expected limits of functions to be concatenated to fit. ";
+            info << "function[" << i << "].upper_limit == ";
+            info << "function[" << i+1 << "].lower_limit, but actual\n";
+            info << "f[" << i << "].upper_limit = ";
+            info << conc.at(i)->get_limits().get_upper() << "\n";
+            info << "f[" << i+1 << "].lower_limit = ";
+            info << conc.at(i+1)->get_limits().get_lower() << "\n";
+            throw std::logic_error(info.str());
+        }
+    }
+}
+
+bool Concat::func_does_not_match_limit_of_next_func(const unsigned int i)const {
+    return conc.at(i)->get_limits().get_upper() !=
+        conc.at(i+1)->get_limits().get_lower();
+}
+
+void Concat::adopt_new_limits() {
+    limits = Limits(
+        conc.front()->get_limits().get_lower(),
+        conc.back()->get_limits().get_upper());
+}
+
+double Concat::evaluate(const double x)const {
+    limits.assert_contains(x);
+    const Func1D* sub_f = get_sub_function_responsible_for(x);
+    return sub_f->evaluate(x);
+}
+
+const Func1D* Concat::get_sub_function_responsible_for(const double x)const {
+    return  (*  std::upper_bound(
+                    conc.begin(),
+                    conc.end(),
+                    x,
+                    compare_upper_limit));
+}
+
+bool Concat::compare_upper_limit(const double x, const Func1D *f) {
+    return f->get_limits().get_upper() > x;
+}
+
+}  // namespace Function
--- Core/Function/ConcatFunction.h
+++ Core/Function/ConcatFunction.h
@@ -1,34 +1,29 @@
-//==============================================================================
-// include guard
-#ifndef __FUNCTIONCONCATENATE_H_INCLUDED__
-#define __FUNCTIONCONCATENATE_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef CORE_FUNCTION_CONCATFUNCTION_H_
+#define CORE_FUNCTION_CONCATFUNCTION_H_
 
-//==============================================================================
-// forward declared dependencies
-
-//==============================================================================
-// included dependencies
 #include <vector>
 #include "Func1DFunction.h"
 
 namespace Function {
 
-	class Concat : public Func1D {
+class Concat : public Func1D {
+    std::vector<Func1D*> conc;
 
-		std::vector<Func1D*> conc;
-	public:
+ public:
+    Concat();
+    explicit Concat(const std::vector<Func1D*> conc);
+    void init(const std::vector<Func1D*> conc);
+    double evaluate(const double x)const;
 
-		Concat();
-		Concat(const std::vector<Func1D*> conc);
-		void init(const std::vector<Func1D*> conc);
-		double evaluate(const double x)const;
-	private:
+ private:
+    void assert_limits_do_fit()const;
+    void adopt_new_limits();
+    const Func1D* get_sub_function_responsible_for(const double x)const;
+    static bool compare_upper_limit(const double x, const Func1D *f);
+    bool func_does_not_match_limit_of_next_func(const unsigned int i)const;
+};
 
-		void assert_limits_do_fit()const;
-		void adopt_new_limits();
-		const Func1D* get_sub_function_responsible_for(const double x)const;
-		static bool compare_upper_limit(const double x, const Func1D *f);
-		bool func_does_not_match_limit_of_next_func(const unsigned int i)const;
-	};
-} // namespace Function
-#endif // __FUNCTIONCONCATENATE_H_INCLUDED__
+}  // namespace Function
+
+#endif  // CORE_FUNCTION_CONCATFUNCTION_H_
