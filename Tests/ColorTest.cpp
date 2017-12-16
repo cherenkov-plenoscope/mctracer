675bffc0cfbb29ad1f44308e00528184729bae9e
sebastian <sebmuell@phys.ethz.ch>
2017-12-16 10:35:15 +0100

    ccplint ColorTest

--- Tests/ColorTest.cpp
+++ Tests/ColorTest.cpp
@@ -1,50 +1,32 @@
+// Copyright 2014 Sebastian A. Mueller
 #include "gtest/gtest.h"
 #include "Core/Color.h"
 
 class ColorTest : public ::testing::Test {};
-//------------------------------------------------------------------------------
+
 TEST_F(ColorTest, 8bit_range) {
-
     EXPECT_NO_THROW(
-        Color col(0,0,0);
-    );
-
+        Color col(0, 0, 0));
     EXPECT_NO_THROW(
-        Color col(128,128,128);
-    );
-
+        Color col(128, 128, 128));
     EXPECT_NO_THROW(
-        Color col(255,255,255);
-    );
-
+        Color col(255, 255, 255));
     EXPECT_THROW(
-        Color col(256,0,0);,
-        std::out_of_range
-    );
-
+        Color col(256, 0, 0),
+        std::out_of_range);
     EXPECT_THROW(
-        Color col(0,256,0);,
-        std::out_of_range
-    );
-
+        Color col(0, 256, 0),
+        std::out_of_range);
     EXPECT_THROW(
-        Color col(0,0,256);,
-        std::out_of_range
-    );
-
+        Color col(0, 0, 256),
+        std::out_of_range);
     EXPECT_THROW(
-        Color col(-1,0,0);,
-        std::out_of_range
-    );
-
+        Color col(-1, 0, 0),
+        std::out_of_range);
     EXPECT_THROW(
-        Color col(0,-1,0);,
-        std::out_of_range
-    );
-
+        Color col(0, -1, 0),
+        std::out_of_range);
     EXPECT_THROW(
-        Color col(0,0,-1);,
-        std::out_of_range
-    );
+        Color col(0, 0, -1),
+        std::out_of_range);
 }
-//------------------------------------------------------------------------------
