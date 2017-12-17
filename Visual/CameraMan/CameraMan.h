482577d09ac6f4b4abc543b04b152302a1989a58
sebastian <sebmuell@phys.ethz.ch>
2017-12-17 20:57:08 +0100

    cpplint

--- Visual/CameraMan/CameraMan.h
+++ Visual/CameraMan/CameraMan.h
@@ -1,4 +1,10 @@
+// Copyright 2014 Sebastian A. Mueller
+#ifndef VISUAL_CAMERAMAN_CAMERAMAN_H_
+#define VISUAL_CAMERAMAN_CAMERAMAN_H_
+
 #include "Translation.h"
 #include "Rotation.h"
 #include "Stereo3D.h"
 #include "FieldOfView.h"
+
+#endif  // VISUAL_CAMERAMAN_CAMERAMAN_H_
