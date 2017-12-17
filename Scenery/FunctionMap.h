f9d1405741714018fb504501bc234718826c56e6
sebastian <sebmuell@phys.ethz.ch>
2017-12-17 15:13:55 +0100

    cpplint FunctionMap

--- Scenery/FunctionMap.h
+++ Scenery/FunctionMap.h
@@ -1,37 +1,26 @@
-//=================================
-// include guard
-#ifndef __FUNCTIONMAP_H_INCLUDED__
-#define __FUNCTIONMAP_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef SCENERY_FUNCTIONMAP_H_
+#define SCENERY_FUNCTIONMAP_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
 #include <string>
+#include <utility>
 #include <map>
 #include "ResourceMap.h"
 #include "Core/Function/Function.h"
 
-class FunctionMap: public ResourceMap {
-public:
+struct FunctionMap: public ResourceMap {
     std::map<std::string, Function::Func1D*> functions;
-
     bool has(const std::string key)const;
     Function::Func1D* get(const std::string key)const;
-    
     template<class ProtoFunction>
     ProtoFunction* add(const std::string key) {
         assert_not_in_use_yet(key);
         ProtoFunction* child = new ProtoFunction;
-        functions.insert(std::pair<std::string, Function::Func1D*>(key, child)); 
-        return child;        
+        functions.insert(std::pair<std::string, Function::Func1D*>(key, child));
+        return child;
     }
-
     ~FunctionMap();
-private:
-
     void assert_has(const std::string key)const;
     void assert_not_in_use_yet(const std::string key);
 };
-#endif // __FUNCTIONMAP_H_INCLUDED__
+#endif  // SCENERY_FUNCTIONMAP_H_
