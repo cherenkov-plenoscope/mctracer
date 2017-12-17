eb0f23069201a337d0cbd380087619d922cbab02
sebastian <sebmuell@phys.ethz.ch>
2017-12-17 15:11:12 +0100

    cpplint and make class->struct, has no private fields anyhow

--- Scenery/ColorMap.h
+++ Scenery/ColorMap.h
@@ -1,28 +1,19 @@
-//=================================
-// include guard
-#ifndef __COLORMAP_H_INCLUDED__
-#define __COLORMAP_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef SCENERY_COLORMAP_H_
+#define SCENERY_COLORMAP_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
 #include <string>
 #include <map>
 #include "ResourceMap.h"
 #include "Core/Color.h"
 
-class ColorMap: public ResourceMap {
-public:
+struct ColorMap: public ResourceMap {
     std::map<std::string, Color> colors;
-
     bool has(const std::string key)const;
     Color* get(const std::string key);
     void add(const std::string key, const Color color);
-private:
-
     void assert_has(const std::string key)const;
     void assert_not_in_use_yet(const std::string key);
 };
-#endif // __COLORMAP_H_INCLUDED__
+
+#endif  // SCENERY_COLORMAP_H_
