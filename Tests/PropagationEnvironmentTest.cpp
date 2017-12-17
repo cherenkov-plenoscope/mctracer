11055aeb8aadfd5b92b555f569d6057e98087634
sebastian <sebmuell@phys.ethz.ch>
2017-12-17 10:57:30 +0100

    cpplint Prop Env test

--- Tests/PropagationEnvironmentTest.cpp
+++ Tests/PropagationEnvironmentTest.cpp
@@ -1,12 +1,11 @@
+// Copyright 2014 Sebastian A. Mueller
 #include "gtest/gtest.h"
 #include "Core/PropagationEnvironment.h"
 
 class PropagationEnvironmentTest : public ::testing::Test {};
-//------------------------------------------------------------------------------
+
 TEST_F(PropagationEnvironmentTest, default_instance) {
-
     PropagationEnvironment env;
-
     EXPECT_EQ(&Frame::VOID_FRAME, env.world_geometry);
     EXPECT_EQ(&PropagationConfig::DEFAULT_CONFIG, env.propagation_options);
     EXPECT_EQ(&Random::VOID_PRNG, env.random_engine);
