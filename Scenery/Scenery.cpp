b282703b7f496176c8029ebe76f5b536b2b83de7
sebastian <sebmuell@phys.ethz.ch>
2017-12-17 15:06:07 +0100

    cpplint scenery

--- Scenery/Scenery.cpp
+++ Scenery/Scenery.cpp
@@ -1,8 +1,8 @@
-#include "Scenery.h"
-
+// Copyright 2014 Sebastian A. Mueller
+#include "Scenery/Scenery.h"
 
 Scenery Scenery::void_scenery = Scenery();
 
 Scenery::Scenery() {
-    root.set_name_pos_rot("root", Vec3(0.0,0.0,0.0), Rot3(0.0,0.0,0.0));
+    root.set_name_pos_rot("root", Vec3(0.0, 0.0, 0.0), Rot3(0.0, 0.0, 0.0));
 }
--- Scenery/Scenery.h
+++ Scenery/Scenery.h
@@ -1,26 +1,18 @@
-//=================================
-// include guard
-#ifndef __SCENERY_H_INCLUDED__
-#define __SCENERY_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef SCENERY_SCENERY_H_
+#define SCENERY_SCENERY_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
 #include "ColorMap.h"
 #include "FunctionMap.h"
 #include "Core/Frame.h"
 
 struct Scenery {
-
     Frame root;
-
     ColorMap colors;
     FunctionMap functions;
 
     Scenery();
-
     static Scenery void_scenery;
 };
-#endif // __SCENERY_H_INCLUDED__
+
+#endif  // SCENERY_SCENERY_H_
