04198c6cf2e4cd5d94e291b1b63469f7b5c174ec
sebastian <sebmuell@phys.ethz.ch>
2017-12-17 15:15:55 +0100

    cpplint ColorMap.cpp

--- Scenery/ColorMap.cpp
+++ Scenery/ColorMap.cpp
@@ -1,38 +1,39 @@
-#include "ColorMap.h"
+// Copyright 2014 Sebastian A. Mueller
+#include "Scenery/ColorMap.h"
+#include <utility>
 #include <sstream>
 using std::stringstream;
 using std::string;
 using std::map;
-//------------------------------------------------------------------------------
+
 bool ColorMap::has(const string key)const {
     return colors.find(key) != colors.end();
 }
-//------------------------------------------------------------------------------
+
 Color* ColorMap::get(const string key) {
     assert_has(key);
     return &colors.find(key)->second;
 }
-//------------------------------------------------------------------------------
+
 void ColorMap::add(const string key, const Color color) {
     assert_not_in_use_yet(key);
-    colors.insert(std::pair<string, Color>(key, color));    
+    colors.insert(std::pair<string, Color>(key, color));
 }
-//------------------------------------------------------------------------------
+
 void ColorMap::assert_has(const string key)const {
-    if(!has(key)) {
+    if (!has(key)) {
         stringstream info;
         info << "ColorMap " << __FILE__ << ", " << __LINE__ << "\n";
         info << "There is no color called '" << key << "'. \n";
         throw NoSuchKey(info.str());
-    }   
+    }
 }
-//------------------------------------------------------------------------------
+
 void ColorMap::assert_not_in_use_yet(const string key) {
-    if(has(key)) {
+    if (has(key)) {
         stringstream info;
         info << "ColorMap " << __FILE__ << ", " << __LINE__ << "\n";
         info << "The key '" << key << "' is already in use.\n";
         throw KeyAlreadyInUse(info.str());
     }
 }
-//------------------------------------------------------------------------------
