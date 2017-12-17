fd0ef7660be120042992e5cf2020177d3c5faac6
sebastian <sebmuell@phys.ethz.ch>
2017-12-17 15:18:08 +0100

    omitt horizontal barrs

--- Tests/ColorMapTest.cpp
+++ Tests/ColorMapTest.cpp
@@ -3,23 +3,23 @@
 #include "Scenery/ColorMap.h"
 
 class ColorMapTest : public ::testing::Test {};
-//------------------------------------------------------------------------------
+
 TEST_F(ColorMapTest, init_empty) {
     ColorMap my_colors;
     EXPECT_FALSE(my_colors.has("red"));
 }
-//------------------------------------------------------------------------------
+
 TEST_F(ColorMapTest, get_not_existing_color) {
     ColorMap my_colors;
     EXPECT_THROW(my_colors.get("red"), ColorMap::NoSuchKey);
 }
-//------------------------------------------------------------------------------
+
 TEST_F(ColorMapTest, add_color_and_check_it_is_in) {
     ColorMap my_colors;
     my_colors.add("red", Color(255, 0, 0));
     EXPECT_TRUE(my_colors.has("red"));
 }
-//------------------------------------------------------------------------------
+
 TEST_F(ColorMapTest, add_color_and_add_it_again) {
     ColorMap my_colors;
     my_colors.add("red", Color(255, 0, 0));
@@ -27,7 +27,7 @@ TEST_F(ColorMapTest, add_color_and_add_it_again) {
         my_colors.add("red", Color(128, 0, 0)),
         ColorMap::KeyAlreadyInUse);
 }
-//------------------------------------------------------------------------------
+
 TEST_F(ColorMapTest, add_color_and_get_it_again) {
     ColorMap my_colors;
     Color novel_red =  Color(255, 0, 0);
@@ -37,4 +37,4 @@ TEST_F(ColorMapTest, add_color_and_get_it_again) {
     EXPECT_EQ(my_red->g, novel_red.g);
     EXPECT_EQ(my_red->b, novel_red.b);
 }
-//------------------------------------------------------------------------------
+
