92782ba0d5eb2239efa4f4de4234b8020b7a94bd
sebastian <sebmuell@phys.ethz.ch>
2017-12-17 14:12:58 +0100

    cpplint online stats

--- Tools/OnlineStatistics.h
+++ Tools/OnlineStatistics.h
@@ -1,30 +1,23 @@
-//=================================
-// include guard
-#ifndef __OnlineVariance_H_INCLUDED__
-#define __OnlineVariance_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef TOOLS_ONLINESTATISTICS_H_
+#define TOOLS_ONLINESTATISTICS_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
-
-// https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
-// Online algorithm
-//
 class OnlineStatistics {
-	double n_samples;
-	double _sum;
-	double smean;
-	double m2;
-public:
+    // https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
+    // Online algorithm
+    double n_samples;
+    double _sum;
+    double smean;
+    double m2;
 
-	OnlineStatistics();
-	void add(const double x);
-	double variance()const;
-	double stddev()const;
-	double mean()const;
-	double sum()const;
-	double number_of_samples()const;
+ public:
+    OnlineStatistics();
+    void add(const double x);
+    double variance()const;
+    double stddev()const;
+    double mean()const;
+    double sum()const;
+    double number_of_samples()const;
 };
-#endif // __OnlineVariance_H_INCLUDED__
+
+#endif  // TOOLS_ONLINESTATISTICS_H_
