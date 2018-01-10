a1dbe572e8a7feb4490ba2de69f990a0bbdf2344
sebastian <sebmuell@phys.ethz.ch>
2018-01-10 21:57:00 +0100

    cpplint DiscTest

--- Tests/DiscTest.cpp
+++ Tests/DiscTest.cpp
@@ -1,20 +1,18 @@
+// Copyright 2014 Sebastian A. Mueller
 #include "gtest/gtest.h"
 #include "Scenery/Primitive/Disc.h"
 
 class DiscTest : public ::testing::Test {};
-//------------------------------------------------------------------------------
+
 TEST_F(DiscTest, set_Disc) {
-
-	Disc D;
-	D.set_radius(42.0);
-	EXPECT_EQ(42.0, D.get_bounding_sphere_radius());
+    Disc D;
+    D.set_radius(42.0);
+    EXPECT_EQ(42.0, D.get_bounding_sphere_radius());
 }
-//------------------------------------------------------------------------------
-TEST_F(DiscTest, set_Disc_negative_radius) {
 
-	EXPECT_THROW(
-		Disc D;
-		D.set_radius(-42.0);,
-		std::invalid_argument
-	);
+TEST_F(DiscTest, set_Disc_negative_radius) {
+    EXPECT_THROW(
+        Disc D;
+        D.set_radius(-42.0),
+        std::invalid_argument);
 }
