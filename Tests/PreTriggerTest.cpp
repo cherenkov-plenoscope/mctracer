136f93456170ba835f7c7cefa993c38c869dcad2
sebastian <sebmuell@phys.ethz.ch>
2018-01-10 23:26:53 +0100

    cpplint PreTriggerTest

--- Tests/PreTriggerTest.cpp
+++ Tests/PreTriggerTest.cpp
@@ -7,9 +7,8 @@ using std::cout;
 namespace SP = SignalProcessing;
 
 class PreTriggerTest : public ::testing::Test {};
-//----------------------------------------------------------------------
-TEST_F(PreTriggerTest, empty_time_line) {
 
+TEST_F(PreTriggerTest, empty_time_line) {
     std::vector<double> arrival_moments;
 
     EXPECT_EQ(0u, arrival_moments.size());
@@ -22,9 +21,8 @@ TEST_F(PreTriggerTest, empty_time_line) {
     conf.threshold = 0;
     EXPECT_TRUE(SP::PreTrigger::might_trigger(&arrival_moments, conf));
 }
-//----------------------------------------------------------------------
-TEST_F(PreTriggerTest, single_in_time_line) {
 
+TEST_F(PreTriggerTest, single_in_time_line) {
     std::vector<double> arrival_moments = {0.0};
 
     SP::PreTrigger::Config conf;
@@ -35,31 +33,29 @@ TEST_F(PreTriggerTest, single_in_time_line) {
     conf.threshold = 2;
     EXPECT_FALSE(SP::PreTrigger::might_trigger(&arrival_moments, conf));
 }
-//----------------------------------------------------------------------
-TEST_F(PreTriggerTest, even_distributed) {
 
+TEST_F(PreTriggerTest, even_distributed) {
     std::vector<double> arrival_moments = {0.0, 1.0, 2.0, 3.0, 4.0, 5.0};
 
     SP::PreTrigger::Config conf;
     conf.time_window = 0.9;
     conf.threshold = 2;
     EXPECT_FALSE(SP::PreTrigger::might_trigger(&arrival_moments, conf));
-    
     conf.time_window = 1.01;
     EXPECT_TRUE(SP::PreTrigger::might_trigger(&arrival_moments, conf));
 }
-//----------------------------------------------------------------------
-TEST_F(PreTriggerTest, dense_center_distributed) {
 
+TEST_F(PreTriggerTest, dense_center_distributed) {
     std::vector<double> arrival_moments = {
-        0.0, 1.0, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 4.0, 5.0
+        0.0, 1.0, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 4.0,
+        5.0
     };
 
     SP::PreTrigger::Config conf;
     conf.time_window = 1.0;
     conf.threshold = 12;
     EXPECT_FALSE(SP::PreTrigger::might_trigger(&arrival_moments, conf));
-    
+
     conf.time_window = 0.5;
     conf.threshold = 4;
     EXPECT_TRUE(SP::PreTrigger::might_trigger(&arrival_moments, conf));
