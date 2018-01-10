3bdfe91e4d8e5bdf3de39b810836d371ad76722b
sebastian <sebmuell@phys.ethz.ch>
2018-01-10 22:52:09 +0100

    cpplint AsciiIoTest.cpp

--- Tests/AsciiIoTest.cpp
+++ Tests/AsciiIoTest.cpp
@@ -1,3 +1,4 @@
+// Copyright 2014 Sebastian A. Mueller
 #include "gtest/gtest.h"
 #include "Tools/AsciiIo.h"
 using std::stringstream;
@@ -5,33 +6,29 @@ using std::string;
 using std::vector;
 
 class AsciiIoTest : public ::testing::Test {};
-//------------------------------------------------------------------------------
+
 TEST_F(AsciiIoTest, read_non_existing_file) {
-
-EXPECT_THROW(
-    std::vector<std::vector<double>> table = 
-        AsciiIo::gen_table_from_file("non_existing_path/non_existing_file.csv"),
-        std::runtime_error
-    );
+    EXPECT_THROW(
+        std::vector<std::vector<double>> table =
+            AsciiIo::gen_table_from_file(
+                "non_existing_path/non_existing_file.csv"),
+            std::runtime_error);
 }
-//------------------------------------------------------------------------------
+
 TEST_F(AsciiIoTest, read_empty_file) {
-
-    std::vector<std::vector<double>> table = 
+    std::vector<std::vector<double>> table =
         AsciiIo::gen_table_from_file("numeric_table_IO/empty.csv");
-
     EXPECT_EQ(0u, table.size());
 }
-//------------------------------------------------------------------------------
-TEST_F(AsciiIoTest, read_valid_file_table_only) {
 
-    std::vector<std::vector<double>> table = 
+TEST_F(AsciiIoTest, read_valid_file_table_only) {
+    std::vector<std::vector<double>> table =
         AsciiIo::gen_table_from_file("numeric_table_IO/matrix_4col_3row.csv");
 
-    //wavelength, mean_free_path, absorbtion_coeff, wuff
-    //123.4, 100.0, 0.99, 133.7
-    //234.5, 101.0, 0.04, 13.37
-    //666.6, 88.8, 0.056, 1.337
+    // wavelength, mean_free_path, absorbtion_coeff, wuff
+    // 123.4, 100.0, 0.99, 133.7
+    // 234.5, 101.0, 0.04, 13.37
+    // 666.6, 88.8, 0.056, 1.337
 
     ASSERT_EQ(3u, table.size());
     ASSERT_EQ(4u, table[0].size());
@@ -47,7 +44,7 @@ TEST_F(AsciiIoTest, read_valid_file_table_only) {
     // 2nd row
     EXPECT_EQ(234.5, table[1][0]);
     EXPECT_EQ(101.0, table[1][1]);
-    EXPECT_EQ( 0.04, table[1][2]);
+    EXPECT_EQ(0.04, table[1][2]);
     EXPECT_EQ(13.37, table[1][3]);
 
     // 3rd row
@@ -56,9 +53,8 @@ TEST_F(AsciiIoTest, read_valid_file_table_only) {
     EXPECT_EQ(0.056, table[2][2]);
     EXPECT_EQ(1.337, table[2][3]);
 }
-//------------------------------------------------------------------------------
-TEST_F(AsciiIoTest, write_table) {
 
+TEST_F(AsciiIoTest, write_table) {
     const unsigned int rows = 100;
     const unsigned int cols = 10;
     std::string path = "numeric_table_IO/written_table.csv";
@@ -66,10 +62,11 @@ TEST_F(AsciiIoTest, write_table) {
     std::vector<std::vector<double>> table(rows);
 
     double value;
-    for(unsigned int row=0; row<rows; row++) {
+    for (unsigned int row = 0; row < rows; row++) {
         std::vector<double> single_row(cols);
-        for(unsigned int col=0; col<cols; col++) {
-            value = double(col)*double(row)/(double(rows)*double(cols));
+        for (unsigned int col = 0; col < cols; col++) {
+            value = static_cast<double>(col)*static_cast<double>(row)/
+                (static_cast<double>(rows)*static_cast<double>(cols));
             single_row[col] = value;
         }
         table[row] = single_row;
@@ -81,22 +78,21 @@ TEST_F(AsciiIoTest, write_table) {
     // read in again
     //----------------
 
-    std::vector<std::vector<double>> table_in = 
+    std::vector<std::vector<double>> table_in =
         AsciiIo::gen_table_from_file(path);
 
     // check column names
     ASSERT_EQ(rows, table_in.size());
-    
-    for(unsigned int row=0; row<rows; row++) {
-        ASSERT_EQ(cols, table_in.at(row).size());       
-        for(unsigned int col=0; col<cols; col++) {
-            EXPECT_NEAR(table[row][col], table_in[row][col], 1e-9);            
+
+    for (unsigned int row = 0; row < rows; row++) {
+        ASSERT_EQ(cols, table_in.at(row).size());
+        for (unsigned int col = 0; col < cols; col++) {
+            EXPECT_NEAR(table[row][col], table_in[row][col], 1e-9);
         }
     }
 }
-//------------------------------------------------------------------------------
-TEST_F(AsciiIoTest, table_from_string_extra_new_line_in_the_end) {
 
+TEST_F(AsciiIoTest, table_from_string_extra_new_line_in_the_end) {
     string text =   "266e-9\t0.03\n"
                     "277e-9\t0.1\n"
                     "283e-9\t0.2\n"
@@ -122,9 +118,8 @@ TEST_F(AsciiIoTest, table_from_string_extra_new_line_in_the_end) {
     EXPECT_EQ(table.at(3).at(0), 300e-9);
     EXPECT_EQ(table.at(3).at(1), 0.254);
 }
-//------------------------------------------------------------------------------
-TEST_F(AsciiIoTest, table_from_string_) {
 
+TEST_F(AsciiIoTest, table_from_string_) {
     string text =   "266e-9\t0.03\n"
                     " 277e-9 0.1\n"
                     "  283e-9  0.2\n"
