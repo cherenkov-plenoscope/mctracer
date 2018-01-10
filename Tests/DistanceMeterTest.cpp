29c11e75cb2c6c9f444d13bb3b1f3600d9b6ac5c
sebastian <sebmuell@phys.ethz.ch>
2018-01-10 21:58:39 +0100

    cpplint DistanceMeterTest

--- Tests/DistanceMeterTest.cpp
+++ Tests/DistanceMeterTest.cpp
@@ -1,3 +1,4 @@
+// Copyright 2014 Sebastian A. Mueller
 #include "gtest/gtest.h"
 #include "Core/DistanceMeter.h"
 #include "Core/Ray.h"
@@ -6,9 +7,9 @@
 class DistanceMeterTest : public ::testing::Test {};
 
 TEST_F(DistanceMeterTest, is_facing) {
-    Sphere sphere = Sphere("sphere_1", Vec3(0,0,0), Rot3(0,0,0));
+    Sphere sphere = Sphere("sphere_1", Vec3(0, 0, 0), Rot3(0, 0, 0));
     sphere.set_radius(0.5);
-    Ray ray = Ray(Vec3(0,0,-1), Vec3(0,0,1));
+    Ray ray = Ray(Vec3(0, 0, -1), Vec3(0, 0, 1));
     DistanceMeter dist_meter = DistanceMeter(&ray, &sphere);
 
     EXPECT_TRUE(dist_meter.faces_an_object);
@@ -16,9 +17,9 @@ TEST_F(DistanceMeterTest, is_facing) {
 }
 
 TEST_F(DistanceMeterTest, is_not_facing) {
-    Sphere sphere = Sphere("sphere_1", Vec3(0,0,0), Rot3(0,0,0));
+    Sphere sphere = Sphere("sphere_1", Vec3(0, 0, 0), Rot3(0, 0, 0));
     sphere.set_radius(0.5);
-    Ray ray = Ray(Vec3(0,0,+1), Vec3(0,0,1));
+    Ray ray = Ray(Vec3(0, 0, +1), Vec3(0, 0, 1));
     DistanceMeter dist_meter = DistanceMeter(&ray, &sphere);
 
     EXPECT_FALSE(dist_meter.faces_an_object);
