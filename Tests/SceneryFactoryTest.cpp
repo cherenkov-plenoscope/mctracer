c3943d5a8a271f48efcc3c2088116385a64a4ad2
sebastian <sebmuell@phys.ethz.ch>
2018-01-10 23:32:32 +0100

    cpplint SceneryFactoryTest.cpp

--- Tests/SceneryFactoryTest.cpp
+++ Tests/SceneryFactoryTest.cpp
@@ -2,34 +2,29 @@
 #include "gtest/gtest.h"
 #include "Xml/Factory/SceneryFactory.h"
 #include "Scenery/Scenery.h"
-//#include "Cameras/FlyingCamera.h"
 using std::cout;
 using std::string;
 
 class SceneryFactoryTest : public ::testing::Test {};
-//------------------------------------------------------------------------------
-TEST_F(SceneryFactoryTest, call_SceneryFactory) {
 
+TEST_F(SceneryFactoryTest, call_SceneryFactory) {
     Scenery scenery;
     Xml::SceneryFactory xml2frame("test_scenery/tree_epoch_20160320.xml");
     xml2frame.append_to_frame_in_scenery(&scenery.root, &scenery);
     EXPECT_EQ(scenery.root.get_name(), "root");
-
-    //PropagationConfig settings;
-    //FlyingCamera free(xml2frame.scenery, &settings);
+    // PropagationConfig settings;
+    // FlyingCamera free(xml2frame.scenery, &settings);
 }
-//------------------------------------------------------------------------------
-TEST_F(SceneryFactoryTest, author_and_comment) {
 
+TEST_F(SceneryFactoryTest, author_and_comment) {
     Xml::SceneryFactory xml2frame("test_scenery/tree_epoch_20160320.xml");
     EXPECT_EQ(xml2frame.author, "Sebastian");
     EXPECT_EQ(xml2frame.comment, "First novel xml format.");
 }
-//------------------------------------------------------------------------------
-TEST_F(SceneryFactoryTest, no_author_and_no_comment) {
 
-    Xml::SceneryFactory xml2frame("test_scenery/tree_epoch_20160320_no_author_no_comment.xml");
+TEST_F(SceneryFactoryTest, no_author_and_no_comment) {
+    Xml::SceneryFactory xml2frame(
+        "test_scenery/tree_epoch_20160320_no_author_no_comment.xml");
     EXPECT_TRUE(xml2frame.author.empty());
     EXPECT_TRUE(xml2frame.comment.empty());
 }
-//------------------------------------------------------------------------------
