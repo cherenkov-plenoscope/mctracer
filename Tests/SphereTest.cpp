f0b0713ce4d4e34ccf494ec60fcc29f8c494eb21
sebastian <sebmuell@phys.ethz.ch>
2018-01-10 23:50:14 +0100

    cpplint SphereTest

--- Tests/SphereTest.cpp
+++ Tests/SphereTest.cpp
@@ -2,26 +2,22 @@
 #include "gtest/gtest.h"
 #include "Scenery/Primitive/Sphere.h"
 
-class SphereTest : public ::testing::Test {
-};
-//------------------------------------------------------------------------------
+class SphereTest : public ::testing::Test {};
+
 TEST_F(SphereTest, set_positiv_radius) {
-  
   Sphere N;
   EXPECT_NO_THROW(N.set_radius(1.0));
 }
-//------------------------------------------------------------------------------
-TEST_F(SphereTest, set_negativ_radius) {
 
+TEST_F(SphereTest, set_negativ_radius) {
   Sphere N;
-  EXPECT_THROW(N.set_radius(-1.0), Sphere::BadRadius) << 
+  EXPECT_THROW(N.set_radius(-1.0), Sphere::BadRadius) <<
   "Spheres must not have negative radius";
 }
-//------------------------------------------------------------------------------
-TEST_F(SphereTest, set_zero_radius) {
 
+TEST_F(SphereTest, set_zero_radius) {
   Sphere N;
-  EXPECT_THROW(N.set_radius(0.0), Sphere::BadRadius) << 
+  EXPECT_THROW(N.set_radius(0.0), Sphere::BadRadius) <<
   "Spheres with zero radius are not allowed! "
   "Imagine setting up such a sphere and wondering why there are never "
   "intersections althoug no other errors occured.";
