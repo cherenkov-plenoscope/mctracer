e771dc3bc85e9764fd98752823ea7dd207000daf
sebastian <sebmuell@phys.ethz.ch>
2018-01-10 23:24:06 +0100

    cpplint Lixel stats Test

--- Tests/PlenoscopeLixelStatisticsTest.cpp
+++ Tests/PlenoscopeLixelStatisticsTest.cpp
@@ -4,7 +4,7 @@
 using std::vector;
 
 class PlenoscopeLixelStatisticsTest : public ::testing::Test {};
-//----------------------------------------------------------------------
+
 TEST_F(PlenoscopeLixelStatisticsTest, default_ctor) {
     Plenoscope::Calibration::LixelStatistic stat;
 
@@ -21,13 +21,12 @@ TEST_F(PlenoscopeLixelStatisticsTest, default_ctor) {
     EXPECT_EQ(stat.time_delay_mean, 0.0);
     EXPECT_EQ(stat.time_delay_std, 0.0);
 }
-//----------------------------------------------------------------------
-TEST_F(PlenoscopeLixelStatisticsTest, write_and_read_binary) {
 
+TEST_F(PlenoscopeLixelStatisticsTest, write_and_read_binary) {
     const unsigned int number_of_lixels = 1337;
     vector<Plenoscope::Calibration::LixelStatistic> lixel_stats;
 
-    for(unsigned int i=0; i<number_of_lixels; i++) {
+    for (unsigned int i = 0; i < number_of_lixels; i++) {
         Plenoscope::Calibration::LixelStatistic stat;
         stat.efficiency =       i*1.000;
         stat.efficiency_std =   i*1.001;
@@ -37,22 +36,22 @@ TEST_F(PlenoscopeLixelStatisticsTest, write_and_read_binary) {
         stat.cy_std =           i*1.005;
         stat.x_mean =           i*1.006;
         stat.x_std =            i*1.007;
-        stat.y_mean =           i*1.008; 
+        stat.y_mean =           i*1.008;
         stat.y_std =            i*1.009;
         stat.time_delay_mean =  i*1.010;
-        stat.time_delay_std =   i*1.011;    
+        stat.time_delay_std =   i*1.011;
         lixel_stats.push_back(stat);
     }
-    
+
     const string path = "InOut/my_stats.LixelStatistics.bin";
     Plenoscope::Calibration::write(lixel_stats, path);
 
-    vector<Plenoscope::Calibration::LixelStatistic> lixel_stats_in = 
+    vector<Plenoscope::Calibration::LixelStatistic> lixel_stats_in =
         Plenoscope::Calibration::read(path);
 
     EXPECT_EQ(lixel_stats_in.size(), number_of_lixels);
 
-    for(unsigned int i=0; i<number_of_lixels; i++) {
+    for (unsigned int i = 0; i < number_of_lixels; i++) {
         EXPECT_NEAR(lixel_stats_in.at(i).efficiency,        i*1.000, 1e-4);
         EXPECT_NEAR(lixel_stats_in.at(i).efficiency_std,    i*1.001, 1e-4);
         EXPECT_NEAR(lixel_stats_in.at(i).cx_mean,           i*1.002, 1e-4);
@@ -67,16 +66,13 @@ TEST_F(PlenoscopeLixelStatisticsTest, write_and_read_binary) {
         EXPECT_NEAR(lixel_stats_in.at(i).time_delay_std,    i*1.011, 1e-4);
     }
 }
-//----------------------------------------------------------------------
+
 TEST_F(PlenoscopeLixelStatisticsTest, read_non_existing_binary_file) {
-
     EXPECT_THROW(
-        Plenoscope::Calibration::read("InOut/non_existing_file.bin");,
-        std::runtime_error
-    );
+        Plenoscope::Calibration::read("InOut/non_existing_file.bin"),
+        std::runtime_error);
 }
-//----------------------------------------------------------------------
-TEST_F(PlenoscopeLixelStatisticsTest, size_is_just_a_plain_struct) {
 
+TEST_F(PlenoscopeLixelStatisticsTest, size_is_just_a_plain_struct) {
     EXPECT_EQ(sizeof(Plenoscope::Calibration::LixelStatistic), 12u*4u);
 }
