73493019837b099202b79e13907ec4538d824d5f
sebastian <sebmuell@phys.ethz.ch>
2018-01-11 12:01:30 +0100

    cpplint Geometry

--- Scenery/Geometry/SurfaceWithOuterPrismBound.h
+++ Scenery/Geometry/SurfaceWithOuterPrismBound.h
@@ -1,28 +1,19 @@
-//=================================
-// include guard
-#ifndef __SurfaceWithOuterPrismBound_H_INCLUDED__
-#define __SurfaceWithOuterPrismBound_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef SCENERY_GEOMETRY_SURFACEWITHOUTERPRISMBOUND_H_
+#define SCENERY_GEOMETRY_SURFACEWITHOUTERPRISMBOUND_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
+#include <vector>
 #include "Core/SurfaceEntity.h"
 #include "Core/Intersection.h"
 #include "TwoSolutionSurfaceRayEquation.h"
 #include "PrismZ.h"
-#include <vector>
 
-//=================================
 class SurfaceWithOuterPrismBound :public SurfaceEntity{
-protected:
-
-	void add_causeal_intersection(
-		const TwoSolutionSurfaceRayEquation* eq,
-		const PrismZ* outer_bound,
-		const Ray *ray,
-		std::vector<Intersection> *intersections
-	)const;	
+ protected:
+    void add_causeal_intersection(
+        const TwoSolutionSurfaceRayEquation* eq,
+        const PrismZ* outer_bound,
+        const Ray *ray,
+        std::vector<Intersection> *intersections)const;
 };
-#endif // __SurfaceWithOuterPrismBound_H_INCLUDED__
+#endif  // SCENERY_GEOMETRY_SURFACEWITHOUTERPRISMBOUND_H_
