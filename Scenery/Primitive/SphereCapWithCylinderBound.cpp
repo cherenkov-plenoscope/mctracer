0ac22637084f5539db6c277f7c7a8537a14ed15d
sebastian <sebmuell@phys.ethz.ch>
2018-01-12 11:22:57 +0100

    cpplint sphere cap cylinder

--- Scenery/Primitive/SphereCapWithCylinderBound.cpp
+++ Scenery/Primitive/SphereCapWithCylinderBound.cpp
@@ -1,68 +1,60 @@
-#include "SphereCapWithCylinderBound.h"
-#include "math.h"
+// Copyright 2014 Sebastian A. Mueller
+#include "Scenery/Primitive/SphereCapWithCylinderBound.h"
+#include <math.h>
 #include <sstream>
 using std::string;
 using std::vector;
 using std::stringstream;
 
-//------------------------------------------------------------------------------
 void SphereCapWithCylinderBound::set_curvature_radius_and_outer_radius(
-	const double _curvature_radius,
-	const double cap_radius
+    const double _curvature_radius,
+    const double cap_radius
 ) {
-	curvature_radius =_curvature_radius;
-	this->cap_radius = cap_radius;
-	restrict_cap_radius_to_curvature_radius();
-	CylBounds.set_radius(cap_radius);
-	post_initialize_radius_of_enclosing_sphere();	
+    curvature_radius = _curvature_radius;
+    this->cap_radius = cap_radius;
+    restrict_cap_radius_to_curvature_radius();
+    CylBounds.set_radius(cap_radius);
+    post_initialize_radius_of_enclosing_sphere();
 }
-//------------------------------------------------------------------------------
+
 double SphereCapWithCylinderBound::get_focal_length()const {
-	return curvature_radius*0.5;
+    return curvature_radius*0.5;
 }
-//------------------------------------------------------------------------------
+
 void SphereCapWithCylinderBound::post_initialize_radius_of_enclosing_sphere() {
-	
-	double sphere_center_to_edge_in_z = 
-		sqrt(curvature_radius*curvature_radius - cap_radius*cap_radius);
-
-	bounding_sphere_radius = hypot(
-		cap_radius,
-		curvature_radius - sphere_center_to_edge_in_z
-	);	
+    double sphere_center_to_edge_in_z =
+        sqrt(curvature_radius*curvature_radius - cap_radius*cap_radius);
+    bounding_sphere_radius = hypot(
+        cap_radius,
+        curvature_radius - sphere_center_to_edge_in_z);
 }
-//------------------------------------------------------------------------------
+
 void SphereCapWithCylinderBound::restrict_cap_radius_to_curvature_radius() {
-	if(cap_radius > curvature_radius)
-		cap_radius = curvature_radius;
+    if (cap_radius > curvature_radius)
+        cap_radius = curvature_radius;
 }
-//------------------------------------------------------------------------------
-std::string SphereCapWithCylinderBound::str()const {
-	std::stringstream out;
-	out << SurfaceEntity::str();
 
-	out << "SphereCapWithCylinderBound:\n";
-	out << "| focal length: " << get_focal_length() << "m\n";
-	out << "| curvature radius: " << curvature_radius << "m\n";
-	out << "| cap radius: " << cap_radius << "m\n";
-	out << "| f/D: " << get_focal_length()/(2.0*cap_radius) << "\n";
-	return out.str();
+std::string SphereCapWithCylinderBound::str()const {
+    std::stringstream out;
+    out << SurfaceEntity::str();
+    out << "SphereCapWithCylinderBound:\n";
+    out << "| focal length: " << get_focal_length() << "m\n";
+    out << "| curvature radius: " << curvature_radius << "m\n";
+    out << "| cap radius: " << cap_radius << "m\n";
+    out << "| f/D: " << get_focal_length()/(2.0*cap_radius) << "\n";
+    return out.str();
 }
-//------------------------------------------------------------------------------
+
 void SphereCapWithCylinderBound::calculate_intersection_with(
-    const Ray* ray, 
+    const Ray* ray,
     vector<Intersection> *intersections
 )const {
-
-	SphericalCapRayIntersectionEquation sphereCapRayEq(curvature_radius, ray);
-
-	if(sphereCapRayEq.has_solutions()) {
-		
-		add_causeal_intersection(
-			&sphereCapRayEq,
-			&CylBounds,
-			ray,
-			intersections
-		);
-	}
+    SphericalCapRayIntersectionEquation sphereCapRayEq(curvature_radius, ray);
+    if (sphereCapRayEq.has_solutions()) {
+        add_causeal_intersection(
+            &sphereCapRayEq,
+            &CylBounds,
+            ray,
+            intersections);
+    }
 }
--- Scenery/Primitive/SphereCapWithCylinderBound.h
+++ Scenery/Primitive/SphereCapWithCylinderBound.h
@@ -1,41 +1,32 @@
-//=================================
-// include guard
-#ifndef __SPHERECAPWITHCYLINDERBOUND_H_INCLUDED__
-#define __SPHERECAPWITHCYLINDERBOUND_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef SCENERY_PRIMITIVE_SPHERECAPWITHCYLINDERBOUND_H_
+#define SCENERY_PRIMITIVE_SPHERECAPWITHCYLINDERBOUND_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
+#include <vector>
+#include <string>
 #include "Scenery/Geometry/SphericalCapRayIntersectionEquation.h"
 #include "Scenery/Geometry/CylinderPrismZ.h"
 #include "Scenery/Geometry/SurfaceWithOuterPrismBound.h"
-#include <vector>
-#include <string>
 
-//=================================
 class SphereCapWithCylinderBound :public SurfaceWithOuterPrismBound {
-protected:	
-	CylinderPrismZ CylBounds;
+ protected:
+    CylinderPrismZ CylBounds;
+    double curvature_radius;
+    double cap_radius;
 
-	double curvature_radius;
-	double cap_radius;
-public:
-
-	void set_curvature_radius_and_outer_radius(
-		const double curvature_radius,
-		const double cap_radius
-	);
-	std::string str()const;
-	void calculate_intersection_with(
-        const Ray* ray, 
-        std::vector<Intersection> *intersections
-    )const;
+ public:
+    void set_curvature_radius_and_outer_radius(
+        const double curvature_radius,
+        const double cap_radius);
+    std::string str()const;
+    void calculate_intersection_with(
+        const Ray* ray,
+        std::vector<Intersection> *intersections)const;
     double get_focal_length()const;
-private:
 
-	void restrict_cap_radius_to_curvature_radius();
-	void post_initialize_radius_of_enclosing_sphere();
+ private:
+    void restrict_cap_radius_to_curvature_radius();
+    void post_initialize_radius_of_enclosing_sphere();
 };
-#endif // __SPHERECAPWITHCYLINDERBOUND_H_INCLUDED__
+
+#endif  // SCENERY_PRIMITIVE_SPHERECAPWITHCYLINDERBOUND_H_
