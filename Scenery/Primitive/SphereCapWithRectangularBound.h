dca9db700d01ef06f86230a8e186ad9dc14f862f
sebastian <sebmuell@phys.ethz.ch>
2018-01-12 11:30:23 +0100

    cpplint sphere cap rectangular

--- Scenery/Primitive/SphereCapWithRectangularBound.h
+++ Scenery/Primitive/SphereCapWithRectangularBound.h
@@ -1,41 +1,32 @@
-//=================================
-// include guard
-#ifndef __SphereCapWithRectangularBound_H_INCLUDED__
-#define __SphereCapWithRectangularBound_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef SCENERY_PRIMITIVE_SPHERECAPWITHRECTANGULARBOUND_H_
+#define SCENERY_PRIMITIVE_SPHERECAPWITHRECTANGULARBOUND_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
+#include <vector>
+#include <string>
 #include "Scenery/Geometry/SphericalCapRayIntersectionEquation.h"
 #include "Scenery/Geometry/RectangularPrismZ.h"
 #include "Scenery/Geometry/SurfaceWithOuterPrismBound.h"
-#include <vector>
-#include <string>
 
-//=================================
 class SphereCapWithRectangularBound :public SurfaceWithOuterPrismBound {
-protected:	
-	
-	RectangularPrismZ rect_bounds;
-	double curvature_radius;
-public:
+ protected:
+    RectangularPrismZ rect_bounds;
+    double curvature_radius;
 
-	void set_curvature_radius_and_x_y_width(
-		const double _curvature_radius,
-		const double x_width,
-		const double y_width
-	);
-	std::string str()const;
-	void calculate_intersection_with(
-        const Ray* ray, 
-        std::vector<Intersection> *intersections
-    )const;
+ public:
+    void set_curvature_radius_and_x_y_width(
+        const double _curvature_radius,
+        const double x_width,
+        const double y_width);
+    std::string str()const;
+    void calculate_intersection_with(
+        const Ray* ray,
+        std::vector<Intersection> *intersections)const;
     double get_focal_length()const;
-private:
 
-	void restrict_cap_radius_to_curvature_radius();
-	void post_initialize_radius_of_enclosing_sphere();
+ private:
+    void restrict_cap_radius_to_curvature_radius();
+    void post_initialize_radius_of_enclosing_sphere();
 };
-#endif // __SphereCapWithRectangularBound_H_INCLUDED__
+
+#endif  // SCENERY_PRIMITIVE_SPHERECAPWITHRECTANGULARBOUND_H_
