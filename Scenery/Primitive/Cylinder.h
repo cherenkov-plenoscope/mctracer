7cb52b8b2ef598350f1f1a242f83224eec6366e8
sebastian <sebmuell@phys.ethz.ch>
2018-01-12 10:26:45 +0100

    cpplint Cylinder

--- Scenery/Primitive/Cylinder.h
+++ Scenery/Primitive/Cylinder.h
@@ -1,58 +1,46 @@
-//=================================
-// include guard
-#ifndef __CYLINDER_H_INCLUDED__
-#define __CYLINDER_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef SCENERY_PRIMITIVE_CYLINDER_H_
+#define SCENERY_PRIMITIVE_CYLINDER_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
+#include <vector>
+#include <string>
 #include "Core/SurfaceEntity.h"
 #include "Core/Intersection.h"
 #include "Scenery/Geometry/ZaxisCylinderRayIntersectionEquation.h"
-#include <vector>
-#include <string>
 
-//=================================
-class Cylinder :public SurfaceEntity{
-protected:
+class Cylinder :public SurfaceEntity {
+ protected:
+    double Radius;
+    double Length;
 
-	double Radius;
-	double Length;
-public:
+ public:
+    Cylinder();
+    Cylinder(const std::string name, const Vec3 pos, const Rot3 rot);
+    void set_cylinder(
+        const double radius,
+        const Vec3 start_pos,
+        const Vec3 end_pos);
+    void set_radius_and_length(const double radius, const double length);
+    std::string str()const;
+    void calculate_intersection_with(
+        const Ray* ray,
+        std::vector<Intersection> *intersections)const;
 
-	Cylinder();
-	Cylinder(const std::string name, const Vec3 pos, const Rot3 rot);
-	void set_cylinder(
-		const double radius, 
-		const Vec3 start_pos, 
-		const Vec3 end_pos
-	);
-	void set_radius_and_length(const double radius, const double length);
-	std::string str()const;
-	void calculate_intersection_with(
-        const Ray* ray, 
-        std::vector<Intersection> *intersections
-    )const;
-private:
+ private:
+    void set_cylinder_length(const double Length);
+    void set_cylinder_length(const Vec3 start_pos, const Vec3 end_pos);
+    void set_cylinder_radius(const double new_cylinder_radius);
+    void post_initialize_radius_of_enclosing_sphere();
+    void assert_start_and_end_point_are_distinct(
+        const Vec3 start_pos, const Vec3 end_pos)const;
+    void set_position_and_orientation(
+        const Vec3 start_pos,
+        const Vec3 end_pos);
+    bool is_in_cylinders_z_bounds(const Vec3* vec)const;
+    Vec3 get_surface_normal_for_intersection_vec(const Vec3* vec)const;
+    Rot3 calculate_new_rotation_in_mother(const Vec3 rotsym_axis)const;
 
-	void set_cylinder_length(const double Length);
-	void set_cylinder_length(const Vec3 start_pos, const Vec3 end_pos);
-	void set_cylinder_radius(const double new_cylinder_radius);
-	void post_initialize_radius_of_enclosing_sphere();
-	void assert_start_and_end_point_are_distinct(
-		const Vec3 start_pos, const Vec3 end_pos
-	)const;
-	void set_position_and_orientation(
-		const Vec3 start_pos,
-		const Vec3 end_pos
-	);
-	bool is_in_cylinders_z_bounds(const Vec3* vec)const;
-	Vec3 get_surface_normal_for_intersection_vec(const Vec3* vec)const;
-	Rot3 calculate_new_rotation_in_mother(const Vec3 rotsym_axis)const;
-public:
-
+ public:
     class BadRadius :public std::invalid_argument{
         using invalid_argument::invalid_argument;
     };
@@ -65,4 +53,5 @@ public:
         using invalid_argument::invalid_argument;
     };
 };
-#endif // __CYLINDER_H_INCLUDED__
+
+#endif  // SCENERY_PRIMITIVE_CYLINDER_H_
