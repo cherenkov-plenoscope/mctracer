3a0cdbf6aac6d08cca2f9f0eec9519d80780c865
sebastian <sebmuell@phys.ethz.ch>
2018-01-12 10:59:07 +0100

    cpplint Elliptical Cap

--- Scenery/Primitive/EllipticalCapWithHexagonalBound.h
+++ Scenery/Primitive/EllipticalCapWithHexagonalBound.h
@@ -1,55 +1,41 @@
-//=================================
-// include guard
-#ifndef __EllipticalCapWithHexagonalBound_H_INCLUDED__
-#define __EllipticalCapWithHexagonalBound_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef SCENERY_PRIMITIVE_ELLIPTICALCAPWITHHEXAGONALBOUND_H_
+#define SCENERY_PRIMITIVE_ELLIPTICALCAPWITHHEXAGONALBOUND_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
+#include <vector>
+#include <string>
 #include "Scenery/Geometry/EllipticalCapRayIntersectionEquation.h"
 #include "Scenery/Geometry/HexagonalPrismZ.h"
 #include "Scenery/Geometry/SurfaceWithOuterPrismBound.h"
-#include <vector>
-#include <string>
 
-//=================================
 class EllipticalCapWithHexagonalBound :public SurfaceWithOuterPrismBound {
-protected:	
-	HexagonalPrismZ hexBounds;
+ protected:
+    HexagonalPrismZ hexBounds;
+    double focal_length;
+    double ratio_x_to_y_curvature_radius;
+    double X_curvature_radius;
+    double Y_curvature_radius;
+    double Z_curvature_radius;
+    double iXX;
+    double iYY;
+    double iZZ;
+    double hex_bound_rotation;
+    double outer_hex_radius;
 
-	double focal_length;
-	double ratio_x_to_y_curvature_radius;
+ public:
+    void set_focalLength_radiiRatio_hexAngel_hexRadius(
+        const double focal_length,
+        const double ratio_x_to_y_curvature_radius,
+        const double hex_bound_rotation,
+        const double outer_hex_radius);
+    std::string str()const;
+    void calculate_intersection_with(
+        const Ray* ray,
+        std::vector<Intersection> *intersections)const;
 
-	double X_curvature_radius;
-	double Y_curvature_radius;
-	double Z_curvature_radius;
-
-	double iXX;
-	double iYY;
-	double iZZ;
-
-	double hex_bound_rotation;
-	double outer_hex_radius;
-public:
-
-	void set_focalLength_radiiRatio_hexAngel_hexRadius(
-		const double focal_length,
-		const double ratio_x_to_y_curvature_radius,
-		const double hex_bound_rotation,
-		const double outer_hex_radius
-	);
-
-	std::string str()const;
-
-	void calculate_intersection_with(
-        const Ray* ray, 
-        std::vector<Intersection> *intersections
-    )const;
-private:
-	void restrict_outer_hex_radius_to_curvature_radius();
-
-	void post_initialize_radius_of_enclosing_sphere();
+ private:
+    void restrict_outer_hex_radius_to_curvature_radius();
+    void post_initialize_radius_of_enclosing_sphere();
 };
-#endif // __EllipticalCapWithHexagonalBound_H_INCLUDED__
+
+#endif  // SCENERY_PRIMITIVE_ELLIPTICALCAPWITHHEXAGONALBOUND_H_
