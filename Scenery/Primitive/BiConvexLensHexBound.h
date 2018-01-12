2022230133b9267c6c79e53ca9c8f0fdcee731e0
sebastian <sebmuell@phys.ethz.ch>
2018-01-12 11:04:02 +0100

    cpplint hex lens

--- Scenery/Primitive/BiConvexLensHexBound.h
+++ Scenery/Primitive/BiConvexLensHexBound.h
@@ -1,26 +1,19 @@
-//=================================
-// include guard
-#ifndef __BiConvexLensHexBound_H_INCLUDED__
-#define __BiConvexLensHexBound_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef SCENERY_PRIMITIVE_BICONVEXLENSHEXBOUND_H_
+#define SCENERY_PRIMITIVE_BICONVEXLENSHEXBOUND_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
 #include "Scenery/Primitive/SphereCapWithHexagonalBound.h"
 #include "Scenery/Primitive/PlaneDualSphericalBound.h"
-//=================================
-class BiConvexLensHexBound :public SurfaceEntity{
-public:
 
-	void set_curvature_radius_and_outer_hex_radius(
-		const double curvature_radius,
-		const double outer_aperture_radius
-	);
-protected:
-	double height_of_a_cap_given_curv_radius_and_outer_radius(
-		const double curvature_radius, const double outer_radius
-	)const;
+class BiConvexLensHexBound :public SurfaceEntity{
+ public:
+    void set_curvature_radius_and_outer_hex_radius(
+        const double curvature_radius,
+        const double outer_aperture_radius);
+
+ protected:
+    double height_of_a_cap_given_curv_radius_and_outer_radius(
+        const double curvature_radius, const double outer_radius)const;
 };
-#endif // __BiConvexLensHexBound_H_INCLUDED__ 
+
+#endif  // SCENERY_PRIMITIVE_BICONVEXLENSHEXBOUND_H_
