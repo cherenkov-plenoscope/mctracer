4e70bd01d71e0168ae89a00f4b43cc12cbb9d0f1
sebastian <sebmuell@phys.ethz.ch>
2018-01-12 10:54:10 +0100

    cpplint

--- Scenery/Primitive/BiConvexLens.h
+++ Scenery/Primitive/BiConvexLens.h
@@ -1,27 +1,18 @@
-//=================================
-// include guard
-#ifndef __BiConvexLens_H_INCLUDED__
-#define __BiConvexLens_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef SCENERY_PRIMITIVE_BICONVEXLENS_H_
+#define SCENERY_PRIMITIVE_BICONVEXLENS_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
 #include "SphereCapWithCylinderBound.h"
 
-//=================================
-class BiConvexLens :public SurfaceEntity{
-public:
+class BiConvexLens :public SurfaceEntity {
+ public:
+    void set_curvature_radius_and_aperture_radius(
+        const double curvature_radius,
+        const double aperture_radius);
 
-	void set_curvature_radius_and_aperture_radius(
-		const double curvature_radius,
-		const double aperture_radius
-	);
-protected:
-
-	double height_of_a_cap_given_curv_radius_and_aperture_radius(
-		const double curvature_radius, const double aperture_radius
-	)const;
+ protected:
+    double height_of_a_cap_given_curv_radius_and_aperture_radius(
+        const double curvature_radius, const double aperture_radius)const;
 };
-#endif // __BiConvexLens_H_INCLUDED__ 
+
+#endif  // SCENERY_PRIMITIVE_BICONVEXLENS_H_
