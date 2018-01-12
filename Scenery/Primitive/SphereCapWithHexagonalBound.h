955eca39a19e5f39ea693ac2d94fbda94be5c99c
sebastian <sebmuell@phys.ethz.ch>
2018-01-12 11:26:51 +0100

    cpplint sphere cap hex

--- Scenery/Primitive/SphereCapWithHexagonalBound.h
+++ Scenery/Primitive/SphereCapWithHexagonalBound.h
@@ -1,41 +1,32 @@
-//=================================
-// include guard
-#ifndef __SphereCapWithHexagonalBound_H_INCLUDED__
-#define __SphereCapWithHexagonalBound_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef SCENERY_PRIMITIVE_SPHERECAPWITHHEXAGONALBOUND_H_
+#define SCENERY_PRIMITIVE_SPHERECAPWITHHEXAGONALBOUND_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
+#include <vector>
+#include <string>
 #include "Scenery/Geometry/SphericalCapRayIntersectionEquation.h"
 #include "Scenery/Geometry/HexagonalPrismZ.h"
 #include "Scenery/Geometry/SurfaceWithOuterPrismBound.h"
-#include <vector>
-#include <string>
 
-//=================================
 class SphereCapWithHexagonalBound :public SurfaceWithOuterPrismBound {
-protected:	
-	HexagonalPrismZ hexBounds;
+ protected:
+    HexagonalPrismZ hexBounds;
+    double curvature_radius;
+    double outer_hex_radius;
 
-	double curvature_radius;
-	double outer_hex_radius;
-public:
-
-	void set_curvature_radius_and_outer_hex_radius(
-		const double curvature_radius,
-		const double outer_hex_radius
-	);
-	std::string str()const;
-	void calculate_intersection_with(
-        const Ray* ray, 
-        std::vector<Intersection> *intersections
-    )const;
+ public:
+    void set_curvature_radius_and_outer_hex_radius(
+        const double curvature_radius,
+        const double outer_hex_radius);
+    std::string str()const;
+    void calculate_intersection_with(
+        const Ray* ray,
+        std::vector<Intersection> *intersections)const;
     double get_focal_length()const;
-private:
 
-	void restrict_outer_hex_radius_to_curvature_radius();
-	void post_initialize_radius_of_enclosing_sphere();
+ private:
+    void restrict_outer_hex_radius_to_curvature_radius();
+    void post_initialize_radius_of_enclosing_sphere();
 };
-#endif // __SphereCapWithHexagonalBound_H_INCLUDED__
+
+#endif  // SCENERY_PRIMITIVE_SPHERECAPWITHHEXAGONALBOUND_H_
