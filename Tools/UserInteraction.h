da2469cf21f318beda4169c289c48c56949c08d6
sebastian <sebmuell@phys.ethz.ch>
2018-02-20 10:22:07 +0100

    cpplint Userinteraction

--- Tools/UserInteraction.h
+++ Tools/UserInteraction.h
@@ -1,21 +1,14 @@
-//=================================
-// include guard
-#ifndef __UserInteraction_H_INCLUDED__
-#define __UserInteraction_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef TOOLS_USERINTERACTION_H_
+#define TOOLS_USERINTERACTION_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
 #include "Core/Vec3.h"
 
 namespace UserInteraction {
-	
-	void print_welcome_screen();	
-	void ClearScreen();
-	bool is_Escape_key(const int user_input_key);
-	Vec3 get_Vec3();
-	static const unsigned int space_key = 32;
+    void print_welcome_screen();
+    void ClearScreen();
+    bool is_Escape_key(const int user_input_key);
+    Vec3 get_Vec3();
+    static const unsigned int space_key = 32;
 }
-#endif // __UserInteraction_H_INCLUDED__
+#endif  // TOOLS_USERINTERACTION_H_
