a560e99d0d9e128c08df97b5f27368a26c4139e7
sebastian <sebmuell@phys.ethz.ch>
2018-02-20 10:08:53 +0100

    cpplint Time, remove unused stuff, remove unused includes

--- Tools/Time.cpp
+++ Tools/Time.cpp
@@ -1,85 +1,56 @@
-#include "Time.h"
-#include <ctime>
-#include <ratio>
+// Copyright 2014 Sebastian A. Mueller
+#include "Tools/Time.h"
 #include <sstream>
 #include <iomanip>
-#include <iostream>
 using std::string;
 
 namespace Time {
-	Point now() {
-		return std::chrono::high_resolution_clock::now();
-	}
 
-	double periode(const Point &t1, const Point &t2) {
-		// return in SI unit Secoconds
-		return 0.001*
-			std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count();
-	}
+    Stamp::Stamp() {
+        update_now();
+    }
 
-	StopPeriode::StopPeriode() {
-		t1 = Time::now();
-	}
+    void Stamp::update_now() {
+        initialization_time = time(0);
+        now = localtime(&initialization_time);
+    }
 
-	double StopPeriode::stop()const {
-		std::chrono::high_resolution_clock::time_point t2 = Time::now();
-		return 0.001*std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count(); 
-	}
+    string Stamp::str()const {
+        std::stringstream out;
+        out << "time stamp: ";
+        out << yyyy() << "." << mm() << "." << dd() << " ";
+        out << HH() << ":" << MM() << ":" << SS();
+        return out.str();
+    }
 
-	StopWatch::StopWatch(const string what_to_time): 
-		stop_periode(StopPeriode()),
-		what(what_to_time)
-	{}
+    string Stamp::yyyy()const {
+        return zero_padding(now->tm_year+1900, 4);
+    }
 
-	void StopWatch::stop() {
-		std::cout << what << ": " << stop_periode.stop() << "s\n";
-	}
+    string Stamp::mm()const {
+        return zero_padding(now->tm_mon+1, 2);
+    }
 
-	Stamp::Stamp() {
-		update_now();
-	}
+    string Stamp::dd()const {
+        return zero_padding(now->tm_mday, 2);
+    }
 
-	void Stamp::update_now() {
-		initialization_time = time(0);
-		now = localtime(&initialization_time);		
-	}
+    string Stamp::HH()const {
+        return zero_padding(now->tm_hour, 2);
+    }
 
-	string Stamp::str()const {
-		std::stringstream out; 
-		out << "time stamp: ";
-		out <<yyyy()<<"."<<mm()<<"."<<dd()<<" "<<HH()<<":"<<MM()<<":"<<SS();
-		return out.str();
-	}
+    string Stamp::MM()const {
+        return zero_padding(now->tm_min, 2);
+    }
 
-	string Stamp::yyyy()const {
-		return fill_zeros_to_value_for_width(now->tm_year+1900, 4);
-	}
+    string Stamp::SS()const {
+        return zero_padding(now->tm_sec, 2);
+    }
 
-	string Stamp::mm()const {
-		return fill_zeros_to_value_for_width(now->tm_mon+1, 2);
-	}
-
-	string Stamp::dd()const {
-		return fill_zeros_to_value_for_width(now->tm_mday, 2);
-	}
-
-	string Stamp::HH()const {
-		return fill_zeros_to_value_for_width(now->tm_hour, 2);
-	}
-
-	string Stamp::MM()const {
-		return fill_zeros_to_value_for_width(now->tm_min, 2);
-	}
-
-	string Stamp::SS()const {
-		return fill_zeros_to_value_for_width(now->tm_sec, 2);
-	}
-
-	string Stamp::fill_zeros_to_value_for_width(
-		const unsigned int value, const unsigned int width
-	)const {
-		std::stringstream out;
-		out << std::setfill('0') << std::setw(width) << value;
-		return out.str();
-	}
-}//Time
+    string Stamp::zero_padding(
+        const unsigned int value, const unsigned int width)const {
+        std::stringstream out;
+        out << std::setfill('0') << std::setw(width) << value;
+        return out.str();
+    }
+}  // namespace Time
--- Tools/Time.h
+++ Tools/Time.h
@@ -1,59 +1,31 @@
-//=================================
-// include guard
-#ifndef __Time_H_INCLUDED__
-#define __Time_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef TOOLS_TIME_H_
+#define TOOLS_TIME_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
-#include <chrono>
+#include <ctime>
 #include <string>
 
-//=================================
 namespace Time {
-	#define Point std::chrono::high_resolution_clock::time_point
-	Point now();
-	double periode(const Point &t1, const Point &t2); 
-	//--------------------------------------------------------------------------
-	class StopPeriode {
-		const std::string what;
-		Point t1;
-	public:
-		StopPeriode();
-		double stop()const;
-	};
-	//--------------------------------------------------------------------------
-	class StopWatch {
-		StopPeriode stop_periode;
-		const std::string what;
-	public:
 
-		StopWatch(const std::string what_to_time);
-		void stop();
-	};
-	//--------------------------------------------------------------------------
-	class Stamp {
+class Stamp {
+    std::time_t initialization_time;
+    struct std::tm *now;
+ public:
+    Stamp();
+    void update_now();
+    std::string str()const;
+    std::string yyyy()const;
+    std::string mm()const;
+    std::string dd()const;
+    std::string HH()const;
+    std::string MM()const;
+    std::string SS()const;
+ private:
+    std::string zero_padding(
+        const unsigned int value,
+        const unsigned int width)const;
+};
 
-		std::time_t initialization_time;
-		struct std::tm *now;
-	public:
+}  // namespace Time
 
-		Stamp();
-		void update_now();
-		std::string str()const;
-		std::string yyyy()const;
-		std::string mm()const;
-		std::string dd()const;
-		std::string HH()const;
-		std::string MM()const;
-		std::string SS()const;
-	private:
-		std::string fill_zeros_to_value_for_width(
-			const unsigned int value, 
-			const unsigned int width
-		)const;
-	};
-}
-#endif // __Time_H_INCLUDED__
+#endif  // TOOLS_TIME_H_
