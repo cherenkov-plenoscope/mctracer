4188aef29584e1a41be8f43cf1ed02dadebeb3d3
sebastian <sebmuell@phys.ethz.ch>
2018-02-20 09:40:48 +0100

    cpplint HeaderBlocks

--- Tools/HeaderBlock.cpp
+++ Tools/HeaderBlock.cpp
@@ -1,4 +1,5 @@
-#include "HeaderBlock.h"
+// Copyright 2014 Sebastian A. Mueller
+#include "Tools/HeaderBlock.h"
 #include <fstream>
 #include <sstream>
 using std::string;
@@ -6,47 +7,46 @@ using std::vector;
 using std::array;
 
 namespace HeaderBlock {
-//------------------------------------------------------------------------------
-void write(array<float, 273> block, const string &path) {
 
+void write(array<float, 273> block, const string &path) {
     vector<array<float, 273>> blocks;
     blocks.push_back(block);
     write(blocks, path);
 }
-//------------------------------------------------------------------------------
+
 void write(vector<array<float, 273>> blocks, const string &path) {
-    
     std::ofstream file;
     file.open(path, std::ios::out | std::ios::binary);
-    if(!file.is_open()) {
+    if (!file.is_open()) {
         std::stringstream info;
         info << __FILE__ << " " << __LINE__ << "\n";
         info << "HeaderBlock: Unable to write file: '" << path << "'\n";
-        throw std::runtime_error(info.str());      
+        throw std::runtime_error(info.str());
     }
 
-    for(const array<float, 273> &block: blocks)
-        file.write((char*)&block, sizeof(array<float, 273>));
+    for (const array<float, 273> &block : blocks)
+        file.write(
+            reinterpret_cast<const char*>(&block),
+            sizeof(array<float, 273>));
 
     file.close();
 }
-//------------------------------------------------------------------------------
+
 vector<array<float, 273>> read(const string &path) {
-    
     std::ifstream file;
     file.open(path, std::ios::binary);
-    if(!file.is_open()) {
+    if (!file.is_open()) {
         std::stringstream info;
         info << __FILE__ << " " << __LINE__ << "\n";
         info << "HeaderBlock: Unable to read file: '" << path << "'\n";
-        throw std::runtime_error(info.str());      
+        throw std::runtime_error(info.str());
     }
 
     vector<array<float, 273>> blocks;
-    while(true) {
+    while (true) {
         array<float, 273> block;
-        file.read((char*)&block, sizeof(array<float, 273>));
-        if(!file.eof())
+        file.read(reinterpret_cast<char*>(&block), sizeof(array<float, 273>));
+        if (!file.eof())
             blocks.push_back(block);
         else
             break;
@@ -54,5 +54,5 @@ vector<array<float, 273>> read(const string &path) {
     file.close();
     return blocks;
 }
-//------------------------------------------------------------------------------
-}//HeaderBlock
+
+}  // namespace HeaderBlock
--- Tools/HeaderBlock.h
+++ Tools/HeaderBlock.h
@@ -1,21 +1,17 @@
-//=================================
-// include guard
-#ifndef __HeaderBlockInOut_H_INCLUDE__
-#define __HeaderBlockInOut_H_INCLUDE__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef TOOLS_HEADERBLOCK_H_
+#define TOOLS_HEADERBLOCK_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies 
 #include <string>
 #include <array>
 #include <vector>
 
 namespace HeaderBlock {
-	void write(std::array<float, 273> block, const std::string &path);
-	void write(std::vector<std::array<float, 273>> blocks, const std::string &path);
-	std::vector<std::array<float, 273>> read(const std::string &path);
+    void write(std::array<float, 273> block, const std::string &path);
+    void write(
+        std::vector<std::array<float, 273>> blocks,
+        const std::string &path);
+    std::vector<std::array<float, 273>> read(const std::string &path);
 }
 
-#endif // __HeaderBlockInOut_H_INCLUDE__
+#endif  // TOOLS_HEADERBLOCK_H_
