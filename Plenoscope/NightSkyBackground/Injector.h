66d4e2326c10215a3a9bce55d593e8e66303281a
sebastian <sebmuell@phys.ethz.ch>
2018-03-20 13:10:00 +0100

    cpplint nsb

--- Plenoscope/NightSkyBackground/Injector.h
+++ Plenoscope/NightSkyBackground/Injector.h
@@ -1,27 +1,26 @@
-//=================================
-// include guard
-#ifndef __PlenoscopeNightSkyBackgroundInjector_H_INCLUDED__
-#define __PlenoscopeNightSkyBackgroundInjector_H_INCLUDED__
+// Copyright 2016 Sebastian A. Mueller
+#ifndef PLENOSCOPE_NIGHTSKYBACKGROUND_INJECTOR_H_
+#define PLENOSCOPE_NIGHTSKYBACKGROUND_INJECTOR_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
+#include <vector>
 #include "Plenoscope/NightSkyBackground/Light.h"
 #include "SignalProcessing/PipelinePhoton.h"
 #include "Plenoscope/Calibration/LixelStatistics.h"
 
 namespace Plenoscope {
-	namespace NightSkyBackground {
+namespace NightSkyBackground {
 
-	void inject_nsb_into_photon_pipeline(
-		std::vector<std::vector<SignalProcessing::PipelinePhoton>> *photon_pipelines,
-		const double exposure_time,
-		const std::vector<Plenoscope::Calibration::LixelStatistic> *lixel_statistics,
-		const Light *nsb,
-		Random::Generator* prng
-	);
-	}// NightSkyBackground
-}// Plenoscope
-#endif // __PlenoscopeNightSkyBackgroundInjector_H_INCLUDED__ 
+void inject_nsb_into_photon_pipeline(
+    std::vector<std::vector<SignalProcessing::PipelinePhoton>> *
+        photon_pipelines,
+    const double exposure_time,
+    const std::vector<Plenoscope::Calibration::LixelStatistic> *
+        lixel_statistics,
+    const Light *nsb,
+    Random::Generator* prng
+);
+
+}  // namespace NightSkyBackground
+}  // namespace Plenoscope
+
+#endif  // PLENOSCOPE_NIGHTSKYBACKGROUND_INJECTOR_H_
--- Plenoscope/NightSkyBackground/Light.h
+++ Plenoscope/NightSkyBackground/Light.h
@@ -1,39 +1,32 @@
-//=================================
-// include guard
-#ifndef __PlenoscopeNightSkyBackgroundPhotons_H_INCLUDED__
-#define __PlenoscopeNightSkyBackgroundPhotons_H_INCLUDED__
+// Copyright 2016 Sebastian A. Mueller
+#ifndef PLENOSCOPE_NIGHTSKYBACKGROUND_LIGHT_H_
+#define PLENOSCOPE_NIGHTSKYBACKGROUND_LIGHT_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
+#include <string>
 #include "Plenoscope/LightFieldSensor/Geometry.h"
 #include "Core/Random/Random.h"
 #include "Core/Function/Function.h"
 #include "Core/Photon.h"
-#include <string>
 
 namespace Plenoscope {
-	namespace NightSkyBackground {
+namespace NightSkyBackground {
 
-	struct Light {
-	
-		const Function::Func1D* flux_vs_wavelength;
-		Random::SamplesFromDistribution wavelength_probability;
-		double rate;
-		double fov_radius;
-		double fov_solid_angle;
-		double aperture_radius;
-		double aperture_area;
-		const LightFieldSensor::Geometry *sensor_geometry;
-		Light(
-			const LightFieldSensor::Geometry *_sensor_geometry, 
-			const Function::Func1D* flux_vs_wavelength
-		);
-		std::string __repr__()const;
-	};
-	
-	}// NightSkyBackground
-}// Plenoscope
-#endif // __PlenoscopeNightSkyBackgroundPhotons_H_INCLUDED__ 
+struct Light {
+    const Function::Func1D* flux_vs_wavelength;
+    Random::SamplesFromDistribution wavelength_probability;
+    double rate;
+    double fov_radius;
+    double fov_solid_angle;
+    double aperture_radius;
+    double aperture_area;
+    const LightFieldSensor::Geometry *sensor_geometry;
+    Light(
+        const LightFieldSensor::Geometry *_sensor_geometry,
+        const Function::Func1D* flux_vs_wavelength);
+    std::string __repr__()const;
+};
+
+}  // namespace NightSkyBackground
+}  // namespace Plenoscope
+
+#endif  // PLENOSCOPE_NIGHTSKYBACKGROUND_LIGHT_H_
