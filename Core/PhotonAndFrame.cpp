cherenkov-plenoscope/mctracer

commit 5ce7d9eb0b6226535101ca6e6e2d265b879dca1f

prng must not be 'omp private', propagation environment is set up individually

Plenoscope/Calibration/Calibrator.cpp
@@ -8,6 +8,7 @@
 #include "Core/PhotonAndFrame.h"
 #include "Tools/HeaderBlock.h"
 #include "omp.h"
+#include <chrono>
 
 //#include "Cameras/FlyingCamera.h"
 using std::vector;
@@ -31,7 +32,6 @@ Calibrator::Calibrator(
 	set_up_photon_properties();
 	set_up_principal_aperture_range();
 	set_up_field_of_view_range();
-	set_up_plenoscope_environment();
 	run_calibration();
 }
 //------------------------------------------------------------------------------
@@ -54,13 +54,6 @@ void Calibrator::set_up_field_of_view_range() {
 	max_tilt_vs_optical_axis_to_throw_photons_in = 
 		NightSkyBackground::FOV_RADIUS_OVERHEAD*
 		plenoscope->light_field_sensor_geometry.max_FoV_radius();
-}
-//------------------------------------------------------------------------------
-void Calibrator::set_up_plenoscope_environment() {
-
-	plenoscope_environment.world_geometry = scenery;
-	plenoscope_environment.propagation_options = &settings;
-	plenoscope_environment.random_engine = &prng;		
 }
 //------------------------------------------------------------------------------
 Photon Calibrator::get_photon_given_pos_and_angle_on_principal_aperture(
@@ -88,12 +81,14 @@ void Calibrator::fill_calibration_block_to_table() {
 	
 	unsigned int i;
 	int HadCatch = 0;
+	const uint32_t master_seed =
+		std::chrono::system_clock::now().time_since_epoch().count();
 
 	#pragma omp parallel shared(HadCatch)
 	{
-		Random::Mt19937 thread_local_prng(omp_get_thread_num());
+		Random::Mt19937 thread_local_prng(master_seed + omp_get_thread_num());
 
-		#pragma omp for schedule(dynamic) private(i, thread_local_prng) 
+		#pragma omp for schedule(dynamic) private(i) 
 		for(i=0; i<config.photons_per_block; i++) {
 
 			try{
@@ -114,7 +109,10 @@ void Calibrator::fill_calibration_block_to_table() {
 				);
 
 				// propagate photon
-				PhotonAndFrame::Propagator(&ph, plenoscope_environment);
+				PropagationEnvironment my_env;
+				my_env.world_geometry = scenery;
+				my_env.random_engine = &thread_local_prng;
+				PhotonAndFrame::Propagator(&ph, &my_env);
 
 				PhotonSensor::FindSensorByFrame sensor_finder(
 					ph.get_final_intersection().get_object(),

Plenoscope/Calibration/Calibrator.h
@@ -33,10 +33,6 @@ namespace Plenoscope {
 		double distance_to_travel_before_intersecting_principal_aperture;
 		double callibration_photon_wavelenght;
 
-		PropagationConfig settings;
-		PropagationEnvironment plenoscope_environment;
-		Random::Mt19937 prng;
-
 		std::vector<CalibrationPhotonResult> photon_results;
 		LixelStatisticsFiller lixel_statistics_filler;
 	public:

commit 1bb1fd545a640908c0c5b0596eeea3d08555ba0b

Fix mayor bug introduced in commit: 5ce7d9e. The propagatioevironment is modified in each propagator-instance. The root of the scenery is replaced with a restricted sub-section of the scenery in certain cases, e.g. inside lenses. This must not effect other propagator-instances. The propagator-instances must not share the propagation-environment via a pointer. They all need local copies.

Core/PhotonAndFrame.cpp
@@ -5,7 +5,7 @@ namespace PhotonAndFrame {
 
 Propagator::Propagator(
     Photon* photon,
-    PropagationEnvironment* environment
+    PropagationEnvironment environment
 ): env(environment), ph(photon) {
     propagate();
 }
@@ -17,11 +17,11 @@ void Propagator::propagate() {
 
 bool Propagator::limit_of_interactions_is_not_reached_yet()const {
     return ph->get_number_of_interactions_so_far() <=
-        env->config->max_number_of_interactions_per_photon;
+        env.config->max_number_of_interactions_per_photon;
 }
 
 void Propagator::work_on_first_causal_intersection() {
-    isec = RayAndFrame::first_intersection(ph, env->root_frame);
+    isec = RayAndFrame::first_intersection(ph, env.root_frame);
 
     if (isec.does_intersect() && !absorbed_in_medium_before_reaching_surface())
         interact_with_object();
@@ -34,13 +34,13 @@ bool Propagator::absorbed_in_medium_before_reaching_surface()const {
         isec.half_way_depth_coming_from(ph->wavelength);
     const double survival_prob =
         exp(-isec.distance_to_ray_support()/one_over_e_way);
-    return env->prng->uniform() > survival_prob;
+    return env.prng->uniform() > survival_prob;
 }
 
 void Propagator::interact_with_object() {
     if (
         isec.facing_reflection_propability(ph->wavelength) >=
-        env->prng->uniform()
+        env.prng->uniform()
     )
         reflect_on_surface_and_propagate_on(reflection_on_surface);
     else
@@ -80,7 +80,7 @@ void Propagator::fresnel_refraction_and_reflection() {
         isec.refractive_index_coming_from(ph->wavelength),
         isec.refractive_index_going_to(ph->wavelength));
 
-    if (fresnel.reflection_propability() > env->prng->uniform())
+    if (fresnel.reflection_propability() > env.prng->uniform())
         reflect_on_surface_and_propagate_on(fresnel_reflection_on_surface);
     else
         pass_the_boundary_layer(fresnel);
@@ -107,10 +107,10 @@ void Propagator::propagate_on_after_boundary_layer(
     if (isec.get_object()->has_restrictions_on_frames_to_propagate_to() &&
         !isec.going_to_default_refractive_index()
     )
-        env->root_frame =
+        env.root_frame =
             isec.get_object()->get_allowed_frame_to_propagate_to();
     else
-        env->root_frame =
+        env.root_frame =
             isec.get_object()->get_root();
 
     ph->set_support_and_direction(

Core/PhotonAndFrame.h
@@ -12,12 +12,12 @@ namespace PhotonAndFrame {
 class Propagator {
  public:
     Intersection isec;
-    PropagationEnvironment* env;
+    PropagationEnvironment env;
     Photon* ph;
 
     Propagator(
         Photon* photon,
-        PropagationEnvironment* env);
+        PropagationEnvironment env);
     void propagate();
     bool limit_of_interactions_is_not_reached_yet()const;
     void work_on_first_causal_intersection();

Core/Photons.cpp
@@ -44,7 +44,7 @@ void propagate_photons_using_single_thread(
     env.prng = prng;
 
     for (unsigned int i = 0; i < photons->size(); i++)
-        PhotonAndFrame::Propagator(&photons->at(i), &env);
+        PhotonAndFrame::Propagator(&photons->at(i), env);
 }
 
 void propagate_photons_using_multi_thread(
@@ -76,7 +76,7 @@ void propagate_photons_using_multi_thread(
                 ray_counter++;
                 PhotonAndFrame::Propagator(
                     &photons->at(i),
-                    &env_for_this_thread_only);
+                    env_for_this_thread_only);
             } catch (std::exception &error) {
                 HadCatch++;
                 std::cerr << error.what();

Plenoscope/Calibration/Calibrator.cpp
@@ -108,7 +108,7 @@ void Calibrator::fill_calibration_block_to_table() {
                 PropagationEnvironment my_env;
                 my_env.root_frame = scenery;
                 my_env.prng = &thread_local_prng;
-                PhotonAndFrame::Propagator(&ph, &my_env);
+                PhotonAndFrame::Propagator(&ph, my_env);
 
                 PhotonSensor::FindSensorByFrame sensor_finder(
                     ph.get_final_intersection().get_object(),

Tests/BiConvexLensTest.cpp
@@ -96,7 +96,7 @@ TEST_F(BiConvexLensTest, send_photon_frontal_into_lens) {
     unsigned int number_of_photons_reaching_sensor_disc = 0;
     for (unsigned int i = 0; i < total_propagations; i++) {
         Photon blue_photon(Vec3(0.0, 0.0, 1.0), Vec3(0.0, 0.0, -1.0), 433e-9);
-        PhotonAndFrame::Propagator(&blue_photon, &lens_test_bench_environment);
+        PhotonAndFrame::Propagator(&blue_photon, lens_test_bench_environment);
         // blue_photon.propagate_in(lens_test_bench_environment);
 
         if (2.0 == blue_photon.get_accumulative_distance())

Tests/PhotonTest.cpp
@@ -89,7 +89,7 @@ TEST_F(PhotonTest, PropagationSimpleGeometry) {
     const unsigned int num_of_total_interactions = number_of_bounces + 1;
     for (int i = 0; i < 1; i++) {
         Photon P(Support, direction, wavelength);
-        PhotonAndFrame::Propagator(&P, &environment);
+        PhotonAndFrame::Propagator(&P, environment);
         EXPECT_EQ(
             number_of_bounces*1.0-0.5,
             P.get_accumulative_distance());

Tests/SphereIntersectionTest.cpp
@@ -53,7 +53,7 @@ TEST_F(SphereIntersectionTest, frontal) {
     Vec3 Support(x_pos, 0.0, 0.0);
     Vec3 direction(1.0, 0.0, 0.0);
     Photon P(Support , direction, wavelength);
-    PhotonAndFrame::Propagator(&P, &sphere_test_environment);
+    PhotonAndFrame::Propagator(&P, sphere_test_environment);
 
     ASSERT_EQ(2u, P.get_number_of_interactions_so_far() ) <<
     "There should be 2 "
@@ -73,7 +73,7 @@ TEST_F(SphereIntersectionTest, emmitting_close_above_surface_tangential) {
     Vec3 Support(0.0, 0.0, 1.0+1e-9);
     Vec3 direction(1.0, 0.0, 0.0);
     Photon P(Support, direction, wavelength);
-    PhotonAndFrame::Propagator(&P, &sphere_test_environment);
+    PhotonAndFrame::Propagator(&P, sphere_test_environment);
     ASSERT_EQ(absorption_in_void, P.get_final_interaction_type() );
 }
 
@@ -81,7 +81,7 @@ TEST_F(SphereIntersectionTest, emmitting_close_above_surface_straigtht_away) {
     Vec3 Support(0.0, 0.0, 1.0+1e-9);
     Vec3 direction(0.0, 0.0, 1.0);
     Photon P(Support, direction, wavelength);
-    PhotonAndFrame::Propagator(&P, &sphere_test_environment);
+    PhotonAndFrame::Propagator(&P, sphere_test_environment);
     ASSERT_EQ(absorption_in_void, P.get_final_interaction_type() );
 }
 
@@ -89,7 +89,7 @@ TEST_F(SphereIntersectionTest, tangential_intersection) {
     Vec3 Support(-5.0, 0.0, 1.0);
     Vec3 direction(1.0, 0.0, 0.0);
     Photon P(Support, direction, wavelength);
-    PhotonAndFrame::Propagator(&P, &sphere_test_environment);
+    PhotonAndFrame::Propagator(&P, sphere_test_environment);
     ASSERT_EQ(2u, P.get_number_of_interactions_so_far() );
     Vec3 normal = Vec3::UNIT_Z;
     EXPECT_NEAR(