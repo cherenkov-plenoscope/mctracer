c7d8f0b6cb1ca10d012824e7db6d1a1e60ae28d9
sebastian <sebmuell@phys.ethz.ch>
2018-07-09 19:02:00 +0200

    establish a test case for portable pix maps

---
+++ Tests/PortablePixMapTest.cpp
@@ -0,0 +1,9 @@
+// Copyright 2018 Sebastian A. Mueller
+#include "gtest/gtest.h"
+#include "Visual/PortablePixMap.h"
+
+class PortablePixMapTest : public ::testing::Test {};
+
+TEST_F(PortablePixMapTest, write_and_read) {
+
+}
