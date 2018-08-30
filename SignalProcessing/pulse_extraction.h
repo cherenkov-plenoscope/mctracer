a13daa207bb884d8b56171c321dfbb6fbad360bd
sebastian <sebmuell@phys.ethz.ch>
2018-08-30 15:27:16 +0200

    allow arrival_time_std

--- SignalProcessing/pulse_extraction.h
+++ SignalProcessing/pulse_extraction.h
@@ -6,6 +6,7 @@
 #include <vector>
 #include "SignalProcessing/ElectricPulse.h"
 #include "SignalProcessing/ExtractedPulse.h"
+#include "Core/Random/Generator.h"
 
 namespace SignalProcessing {
 
@@ -13,11 +14,15 @@ const int32_t NUMBER_TIME_SLICES = 100;
 
 std::vector<ExtractedPulse> extract_pulses(
     const std::vector<ElectricPulse> &electric_pulses,
-    const double time_slice_duration);
+    const double time_slice_duration,
+    const double arrival_time_std,
+    Random::Generator* prng);
 
 std::vector<std::vector<ExtractedPulse>> extract_pulses(
     const std::vector<std::vector<ElectricPulse>> &electric_pulses,
-    const double time_slice_duration);
+    const double time_slice_duration,
+    const double arrival_time_std,
+    Random::Generator* prng);
 
 }  // namespace SignalProcessing
 
