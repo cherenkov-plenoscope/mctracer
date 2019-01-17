54605864ffd858a97d27c36f36896c3d5b61151e
sebastian <sebmuell@phys.ethz.ch>
2019-01-17 21:13:03 +0100

    ccplint BasicAttributes.cpp

--- Xml/BasicAttributes.cpp
+++ Xml/BasicAttributes.cpp
@@ -1,29 +1,30 @@
-#include "BasicAttributes.h"
+// Copyright 2014 Sebastian A. Mueller
+#include "Xml/BasicAttributes.h"
 #include <sstream>
 using std::string;
 
 namespace Xml {
-//------------------------------------------------------------------------------
+
 string av(const string &att, const string &val) {
     std::stringstream out;
     out << att << "=\"" << val << "\"";
     return out.str();
 }
-//------------------------------------------------------------------------------
+
 string comment(const string &text) {
     std::stringstream out;
     out << "<!-- " << text << " -->";
     return out.str();
 }
-//------------------------------------------------------------------------------
+
 string to_string(const Vec3 &vec) {
     std::stringstream out;
     out << "[" << vec.x << "," << vec.y << "," <<  vec.z << "]";
     return out.str();
 }
-//------------------------------------------------------------------------------
+
 string to_string(const bool b) {
     return (b? "true" : "false");
 }
-//------------------------------------------------------------------------------
-}//Xml
+
+}  // namespace Xml
