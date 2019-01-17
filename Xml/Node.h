ba60c95a4e74bdaaeb0ca831bd38ce626c9b0a81
sebastian <sebmuell@phys.ethz.ch>
2019-01-17 21:28:12 +0100

    ccplint Xml/Node.h

--- Xml/Node.h
+++ Xml/Node.h
@@ -1,90 +1,83 @@
-//=================================
-// include guard
-#ifndef __XmlNode_H_INCLUDED__
-#define __XmlNode_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef XML_NODE_H_
+#define XML_NODE_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
-#include "pugi/pugixml.hpp"
 #include <string>
 #include <memory>
+#include "pugi/pugixml.hpp"
 #include "Tools/Tuple3.h"
 #include "Core/Vec3.h"
 #include "Core/Rot3.h"
 #include "Core/Color.h"
-using std::string;
-//------------------------------------------------------------------------------
+
 namespace Xml {
 
-	class Node {
-		friend class Problem;
+class Node {
+    friend class Problem;
+    pugi::xml_node node;
+    const std::shared_ptr<std::string> file;
+    const std::string path;
 
-		pugi::xml_node node;
-		const std::shared_ptr<string> file;
-		const string path;
-	public:
+ public:
+    void operator=(const Node eq);
+    Node(
+        const pugi::xml_node _node,
+        const std::shared_ptr<std::string> _file,
+        const std::string _path);
+    std::string name()const;
+    std::string attribute(const std::string attribute)const;
+    Node child(const std::string child)const;
+    Node first_child()const;
+    Node next_child();
+    operator bool()const {
+        return node ? true: false;
+    }
 
-		void operator=(const Node eq);
-		Node(
-			const pugi::xml_node _node, 
-			const std::shared_ptr<string> _file,
-			const string _path
-		);
-		string name()const;
-		string attribute(const string attribute)const;
-		Node child(const string child)const;
-		Node first_child()const;
-		Node next_child();
-		operator bool()const { 
-		    return node ? true: false; 
-		}
-	
-		bool has_child(const string child)const;
-		bool has_attribute(const string attribute)const;
-		string xml_path()const;
+    bool has_child(const std::string child)const;
+    bool has_attribute(const std::string attribute)const;
+    std::string xml_path()const;
 
-		int attribute2int(const string attribute_name)const;
-		double attribute2double(const string attribute_name)const;
-		bool attribute2bool(const string attribute_name)const;
-		Tuple3 attribute2Tuple3(const string attribute_name)const;
-		Vec3 attribute2Vec3(const string attribute_name)const;
-		Rot3 attribute2Rot3(const string attribute_name)const;
-		Color attribute2Color(const string attribute_name)const;
+    int attribute2int(const std::string attribute_name)const;
+    double attribute2double(const std::string attribute_name)const;
+    bool attribute2bool(const std::string attribute_name)const;
+    Tuple3 attribute2Tuple3(const std::string attribute_name)const;
+    Vec3 attribute2Vec3(const std::string attribute_name)const;
+    Rot3 attribute2Rot3(const std::string attribute_name)const;
+    Color attribute2Color(const std::string attribute_name)const;
 
-	    class AttributeIsNoInt : public std::invalid_argument {
-	        using invalid_argument::invalid_argument;
-	    }; 
+    class AttributeIsNoInt : public std::invalid_argument {
+        using invalid_argument::invalid_argument;
+    };
 
-	    class AttributeIsNoDouble : public std::invalid_argument {
-	        using invalid_argument::invalid_argument;
-	    }; 
+    class AttributeIsNoDouble : public std::invalid_argument {
+        using invalid_argument::invalid_argument;
+    };
 
-	    class AttributeIsNoBool : public std::invalid_argument {
-	        using invalid_argument::invalid_argument;
-	    }; 
+    class AttributeIsNoBool : public std::invalid_argument {
+        using invalid_argument::invalid_argument;
+    };
 
-	    class AttributeIsNoTuple3 : public std::invalid_argument {
-	        using invalid_argument::invalid_argument;
-	    }; 
+    class AttributeIsNoTuple3 : public std::invalid_argument {
+        using invalid_argument::invalid_argument;
+    };
 
-	    class AttributeIsNoVec3 : public std::invalid_argument {
-	        using invalid_argument::invalid_argument;
-	    }; 
-	    
-	    class AttributeIsNoRot3 : public std::invalid_argument {
-	        using invalid_argument::invalid_argument;
-	    }; 
-	    
-	    class AttributeIsNoColor : public std::invalid_argument {
-	        using invalid_argument::invalid_argument;
-	    }; 
-	private:
+    class AttributeIsNoVec3 : public std::invalid_argument {
+        using invalid_argument::invalid_argument;
+    };
 
-		void assert_child_exists(const string child)const;
-		void assert_attribute_exists(const string attribute)const;
-	};	
-}//Xml
-#endif // __XmlNode_H_INCLUDED__ 
+    class AttributeIsNoRot3 : public std::invalid_argument {
+        using invalid_argument::invalid_argument;
+    };
+
+    class AttributeIsNoColor : public std::invalid_argument {
+        using invalid_argument::invalid_argument;
+    };
+
+ private:
+    void assert_child_exists(const std::string child)const;
+    void assert_attribute_exists(const std::string attribute)const;
+};
+
+}  // namespace Xml
+
+#endif  // XML_NODE_H_
