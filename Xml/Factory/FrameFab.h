3b2a8c0a929c64e5e740eb2f8d5c9772b1231652
sebastian <sebmuell@phys.ethz.ch>
2019-02-02 12:58:32 +0100

    cpplint FrameFab.h

--- Xml/Factory/FrameFab.h
+++ Xml/Factory/FrameFab.h
@@ -1,32 +1,26 @@
-//=================================
-// include guard
-#ifndef __FrameFab_H_INCLUDED__
-#define __FrameFab_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef XML_FACTORY_FRAMEFAB_H_
+#define XML_FACTORY_FRAMEFAB_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
+#include <string>
 #include "Core/Frame.h"
 #include "Xml/Xml.h"
 
-//------------------------------------------------------------------------------
 class FrameFab {
-public:
-	std::string name;
-	Vec3 pos;
-	Rot3 rot;
+ public:
+    std::string name;
+    Vec3 pos;
+    Rot3 rot;
+    explicit FrameFab(const Xml::Node &node);
 
-	FrameFab(const Xml::Node &node);
-private:
-
-	void fab_frame(const Xml::Node &node);
-	Rot3 fab_rotation(const Xml::Node &node);
-	Rot3 fab_rotation_xyz_angles(const Xml::Node &node);
-	Rot3 fab_rotation_based_on_z_reflects(const Xml::Node &node);
-	Rot3 fab_rotation_based_on_axis_and_angle(const Xml::Node &node);
-	Vec3 fab_position(const Xml::Node &node);
-	std::string fab_name(const Xml::Node &node);
+ private:
+    void fab_frame(const Xml::Node &node);
+    Rot3 fab_rotation(const Xml::Node &node);
+    Rot3 fab_rotation_xyz_angles(const Xml::Node &node);
+    Rot3 fab_rotation_based_on_z_reflects(const Xml::Node &node);
+    Rot3 fab_rotation_based_on_axis_and_angle(const Xml::Node &node);
+    Vec3 fab_position(const Xml::Node &node);
+    std::string fab_name(const Xml::Node &node);
 };
-#endif // __FrameFab_H_INCLUDED__ 
+
+#endif  // XML_FACTORY_FRAMEFAB_H_
