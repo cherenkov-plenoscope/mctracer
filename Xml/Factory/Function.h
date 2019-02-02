1b720dd27a32acde68bcc3dd109e2b65b3f0b056
sebastian <sebmuell@phys.ethz.ch>
2019-02-02 13:02:54 +0100

    cpplint Function.h

--- Xml/Factory/Function.h
+++ Xml/Factory/Function.h
@@ -1,24 +1,26 @@
-//=================================
-// include guard
-#ifndef __XmlFunction_H_INCLUDED__
-#define __XmlFunction_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef XML_FACTORY_FUNCTION_H_
+#define XML_FACTORY_FUNCTION_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
 #include "Core/Function/Function.h"
 #include "Xml/Xml.h"
 #include "Scenery/FunctionMap.h"
 
-//------------------------------------------------------------------------------
 namespace Xml {
-	void init_function_with_node(Function::Constant* function, const Node &node);
-	void init_function_with_node(Function::Polynom3* function, const Node &node);
-	void init_function_with_node(Function::LinInterpol* function, const Node &node);
-	void init_function_with_node(Function::Concat* function, const Node &node, FunctionMap* functions);
-	Function::Limits limits_from_node(const Xml::Node &node);
-}//Xml
+    void init_function_with_node(
+        Function::Constant* function,
+        const Node &node);
+    void init_function_with_node(
+        Function::Polynom3* function,
+        const Node &node);
+    void init_function_with_node(
+        Function::LinInterpol* function,
+        const Node &node);
+    void init_function_with_node(
+        Function::Concat* function,
+        const Node &node,
+        FunctionMap* functions);
+    Function::Limits limits_from_node(const Xml::Node &node);
+}  // namespace Xml
 
-#endif // __XmlFunction_H_INCLUDED__ 
+#endif  // XML_FACTORY_FUNCTION_H_
