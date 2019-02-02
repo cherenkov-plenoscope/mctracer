f4bc3ffc643fd5f16e44d154954f518180c5f8f2
sebastian <sebmuell@phys.ethz.ch>
2019-02-02 13:06:17 +0100

    cpplint Function.cpp

--- Xml/Factory/Function.cpp
+++ Xml/Factory/Function.cpp
@@ -1,18 +1,23 @@
+// Copyright 2014 Sebastian A. Mueller
 #include "Xml/Factory/Function.h"
+#include <string>
+#include <vector>
 #include "Tools/StringTools.h"
 using std::vector;
 using std::string;
 
 
-//------------------------------------------------------------------------------
 namespace Xml {
-//------------------------------------------------------------------------------
-void init_function_with_node(Function::Constant* function, const Node &node) {
+
+void init_function_with_node(
+    Function::Constant* function,
+    const Node &node
+) {
     function->init(
         node.to_double("value"),
         limits_from_node(node));
 }
-//------------------------------------------------------------------------------
+
 void init_function_with_node(Function::Polynom3* function, const Node &node) {
     function->init(
         node.to_double("x3"),
@@ -21,16 +26,18 @@ void init_function_with_node(Function::Polynom3* function, const Node &node) {
         node.to_double("x0"),
         limits_from_node(node));
 }
-//------------------------------------------------------------------------------
-void init_function_with_node(Function::LinInterpol* function, const Node &node) {
 
+void init_function_with_node(
+    Function::LinInterpol* function,
+    const Node &node
+) {
     vector<vector<double>> table;
-    for(
-        Xml::Node child=node.first_child(); 
-        child; 
-        child=child.next_child()
+    for (
+        Xml::Node child = node.first_child();
+        child;
+        child = child.next_child()
     ) {
-        if(StringTools::is_equal(child.name(), "xy")) {
+        if (StringTools::is_equal(child.name(), "xy")) {
             vector<double> row = {
                 child.to_double("x"),
                 child.to_double("y")
@@ -38,28 +45,30 @@ void init_function_with_node(Function::LinInterpol* function, const Node &node) 
             table.push_back(row);
         }
     }
-    function->init(table);		
+    function->init(table);
 }
-//------------------------------------------------------------------------------
-void init_function_with_node(Function::Concat* function, const Node &node, FunctionMap* functions) {
-    string attribute = "f0";    
+
+void init_function_with_node(
+    Function::Concat* function,
+    const Node &node,
+    FunctionMap* functions
+) {
+    string attribute = "f0";
     vector<Function::Func1D*> funcs;
     unsigned int count = 0;
     while (node.has_attribute(attribute)) {
-
         funcs.push_back(functions->get(node.attribute(attribute)));
         count++;
         attribute = "f" + std::to_string(count);
-    };
+    }
 
     function->init(funcs);
 }
-//------------------------------------------------------------------------------
+
 Function::Limits limits_from_node(const Xml::Node &node) {
     return Function::Limits(
-        node.to_double("lower_limit"), 
-        node.to_double("upper_limit")
-    );    
+        node.to_double("lower_limit"),
+        node.to_double("upper_limit"));
 }
-//------------------------------------------------------------------------------
-}//Xml
+
+}  // namespace Xml
