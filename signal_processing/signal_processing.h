17a984c9a3a6095a150151a51028bb83ec229b4b
sebastian <sebmuell@phys.ethz.ch>
2019-02-23 22:40:55 +0100

    single include header for signal processing

---
+++ signal_processing/signal_processing.h
@@ -0,0 +1,12 @@
+// Copyright 2014 Sebastian A. Mueller
+#ifndef SIGNAL_PROCESSING_SIGNAL_PROCESSING_H_
+#define SIGNAL_PROCESSING_SIGNAL_PROCESSING_H_
+
+#include "PipelinePhoton.h"
+#include "PhotonStream.h"
+#include "ElectricPulse.h"
+#include "ExtractedPulse.h"
+#include "pulse_extraction.h"
+#include "PhotoElectricConverter.h"
+
+#endif  // SIGNAL_PROCESSING_SIGNAL_PROCESSING_H_
