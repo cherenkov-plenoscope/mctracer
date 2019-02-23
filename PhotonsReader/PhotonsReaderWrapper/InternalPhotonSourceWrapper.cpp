531b5fafd2b65649e88c03c2f3d9e64e97016058
sebastian <sebmuell@phys.ethz.ch>
2019-02-23 21:49:09 +0100

    cpplint

--- PhotonsReader/PhotonsReaderWrapper/InternalPhotonSourceWrapper.cpp
+++ PhotonsReader/PhotonsReaderWrapper/InternalPhotonSourceWrapper.cpp
@@ -1,25 +1,25 @@
+// Copyright 2014 Sebastian A. Mueller
 #include "InternalPhotonSourceWrapper.h"
 #include "json.h"
 using std::string;
 using std::vector;
 
 namespace relleums {
-//------------------------------------------------------------------------------
-InternalPhotonSourceWrapper::InternalPhotonSourceWrapper(const string _path): 
-	BasicWrapper(_path)
-{
-	the_single_event_was_already_read = false;
-	photons = json::load_photons(path);
+
+InternalPhotonSourceWrapper::InternalPhotonSourceWrapper(const string _path):
+    BasicWrapper(_path) {
+    the_single_event_was_already_read = false;
+    photons = json::load_photons(path);
 }
-//------------------------------------------------------------------------------
+
 bool InternalPhotonSourceWrapper::has_still_photons_left()const {
-	return the_single_event_was_already_read? false: true;
+    return the_single_event_was_already_read? false: true;
 }
-//------------------------------------------------------------------------------
+
 vector<Photon> InternalPhotonSourceWrapper::next(Random::Generator* prng) {
-	(void)*prng;
-	the_single_event_was_already_read = true;
-	return photons;
+    (void)*prng;
+    the_single_event_was_already_read = true;
+    return photons;
 }
-//------------------------------------------------------------------------------
+
 }  // namespace relleums
--- PhotonsReader/PhotonsReaderWrapper/InternalPhotonSourceWrapper.h
+++ PhotonsReader/PhotonsReaderWrapper/InternalPhotonSourceWrapper.h
@@ -1,30 +1,22 @@
-//=================================
-// include guard
-#ifndef __PhotonsReaderInternalLightSourceWrapper_H_INCLUDED__
-#define __PhotonsReaderInternalLightSourceWrapper_H_INCLUDED__
+// Copyright 2014 Sebastian A. Mueller
+#ifndef PHOTONSREADER_PHOTONSREADERWRAPPER_INTERNALPHOTONSOURCEWRAPPER_H_
+#define PHOTONSREADER_PHOTONSREADERWRAPPER_INTERNALPHOTONSOURCEWRAPPER_H_
 
-//=================================
-// forward declared dependencies
-
-//=================================
-// included dependencies
 #include "BasicWrapper.h"
 #include <vector>
 #include <string>
 
 namespace relleums {
-//=================================
+
 class InternalPhotonSourceWrapper: public BasicWrapper {
-
-	bool the_single_event_was_already_read;
-	std::vector<Photon> photons;
-public:
-
-	InternalPhotonSourceWrapper(const std::string path);
-	bool has_still_photons_left()const;
-	std::vector<Photon> next(Random::Generator* prng);	
+    bool the_single_event_was_already_read;
+    std::vector<Photon> photons;
+ public:
+    explicit InternalPhotonSourceWrapper(const std::string path);
+    bool has_still_photons_left()const;
+    std::vector<Photon> next(Random::Generator* prng);
 };
 
 }  // namespace relleums
 
-#endif // __PhotonsReaderInternalLightSourceWrapper_H_INCLUDED__ 
+#endif  // PHOTONSREADER_PHOTONSREADERWRAPPER_INTERNALPHOTONSOURCEWRAPPER_H_
