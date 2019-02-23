c8c1d317205f4196d7d6e44507300f5316ae9aab
sebastian <sebmuell@phys.ethz.ch>
2019-02-23 21:45:11 +0100

    cpplint photonsreader

--- PhotonsReader/PhotonsReader.cpp
+++ PhotonsReader/PhotonsReader.cpp
@@ -1,4 +1,5 @@
-#include "PhotonsReader.h"
+// Copyright 2014 Sebastian A. Mueller
+#include "PhotonsReader/PhotonsReader.h"
 #include <sstream>
 #include "PhotonsReaderWrapper/EventIoWrapper.h"
 #include "PhotonsReaderWrapper/AsciiIoWrapper.h"
@@ -8,48 +9,41 @@ using std::vector;
 
 namespace relleums {
 
-//------------------------------------------------------------------------------
 PhotonsReader::PhotonsReader(const string path) {
-    
     std::stringstream out;
 
-    try{
-
+    try {
         out << "___EventIo___\n";
         photons_from_file = new EventIoWrapper(path);
-    }catch(std::exception &error) {
+    } catch (std::exception &error) {
         out << error.what();
-        try{
-
+        try {
             out << "___AsciiIo___\n";
             photons_from_file = new AsciiIoWrapper(path);
-        }catch(std::exception &error) {
+        } catch (std::exception &error) {
             out << error.what();
-            try{
-            
+            try {
                 out << "___InternalLightSource___\n";
                 photons_from_file = new InternalPhotonSourceWrapper(path);
-            }catch(std::exception &error) {
-
+            } catch (std::exception &error) {
                 out << error.what();
-                out << 
+                out <<
                 "Can not read photons from file, it is neither "
                 "Corsika EventIo, Mmcs Corsika, nor Ascci text\n";
-
-                throw std::invalid_argument(out.str());   
-            }   
+                throw std::invalid_argument(out.str());
+            }
         }
     }
 }
-//------------------------------------------------------------------------------
+
 PhotonsReader::~PhotonsReader() {
     delete photons_from_file;
 }
-//------------------------------------------------------------------------------
+
 bool PhotonsReader::has_still_photons_left()const {
     return photons_from_file->has_still_photons_left();
 }
-//------------------------------------------------------------------------------
+
 vector<Photon> PhotonsReader::next(Random::Generator* prng) {
     return photons_from_file->next(prng);
 }
