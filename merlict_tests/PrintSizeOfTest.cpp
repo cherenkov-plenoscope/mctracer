a578cdeff72c244edd86b74ec0d10169d725c2ba
Sebastian Achim Mueller <sebmuell@phys.ethz.ch>
2019-04-01 13:27:49 +0200

    do not print sizes to stdout, but into a file

--- merlict_tests/PrintSizeOfTest.cpp
+++ merlict_tests/PrintSizeOfTest.cpp
@@ -9,114 +9,123 @@ namespace ml = merlict;
 
 
 template<typename T>
-void print_size(const std::string &name, T s) {
-    std::cout << ml::txt::fill_up_text_with_whitespaces_until_column(name, 36);
-    std::cout << sizeof(s) << '\n';
+std::string print_size(const std::string &name, T s) {
+    std::stringstream out;
+    out << ml::txt::fill_up_text_with_whitespaces_until_column(name, 36);
+    out << sizeof(s) << '\n';
+    return out.str();
 }
 
 
 TEST_CASE("SizeOfTest: sizes_of_mct", "[merlict]") {
-    std::cout << "Size in bytes\n";
-    std::cout << "--------------------------------------------" << "\n";
+    std::stringstream out;
+    out << "struct/name                size/bytes\n";
+    out << "--------------------------------------" << "\n";
 
     ml::Color col(123, 65, 12);
-    print_size("Color", col);
+    out << print_size("Color", col);
 
     ml::Vec2 v2;
-    print_size("Vec2", v2);
+    out << print_size("Vec2", v2);
 
     ml::Vec3 v3;
-    print_size("Vec3", v3);
+    out << print_size("Vec3", v3);
 
     ml::Rot3 r;
-    print_size("Rot3", r);
+    out << print_size("Rot3", r);
 
     ml::HomTra3 h;
-    print_size("HomTra3", h);
-    std::cout << "--------------------------------------------" << "\n";
+    out << print_size("HomTra3", h);
+    out << "\n";
 
     ml::Ray ray(ml::VEC3_ORIGIN, ml::VEC3_UNIT_Z);
-    print_size("Ray", ray);
+    out << print_size("Ray", ray);
 
     ml::visual::CameraRay camray(ml::VEC3_ORIGIN, ml::VEC3_UNIT_Z);
-    print_size("CameraRay", camray);
+    out << print_size("CameraRay", camray);
 
     ml::RayForPropagation rayfp(ml::VEC3_ORIGIN, ml::VEC3_UNIT_Z);
-    print_size("RayForPropagation", rayfp);
+    out << print_size("RayForPropagation", rayfp);
 
     ml::Photon ph(ml::VEC3_ORIGIN, ml::VEC3_UNIT_Z, 433e-9);
-    print_size("Photon", ph);
-    std::cout << "--------------------------------------------" << "\n";
+    out << print_size("Photon", ph);
+    out << "\n";
 
     ml::Intersection is;
-    print_size("Intersection", is);
+    out << print_size("Intersection", is);
 
     ml::PropagationEnvironment environment;
-    print_size("PropagationEnvironment", environment);
-    std::cout << "--------------------------------------------" << "\n";
+    out << print_size("PropagationEnvironment", environment);
+    out << "\n";
 
     ml::Frame tree;
     tree.set_name_pos_rot("0123456789", ml::VEC3_ORIGIN, ml::ROT3_UNITY);
-    print_size("Frame", tree);
+    out << print_size("Frame", tree);
 
     ml::SurfaceEntity surf("0123456789", ml::VEC3_ORIGIN, ml::ROT3_UNITY);
-    print_size("SurfaceEntity", surf);
+    out << print_size("SurfaceEntity", surf);
 
     ml::Annulus annulus;
-    print_size("Annulus", annulus);
+    out << print_size("Annulus", annulus);
 
     ml::BiConvexLens biConvexLens;
-    print_size("BiConvexLens", biConvexLens);
+    out << print_size("BiConvexLens", biConvexLens);
 
     ml::BiConvexLensHexBound biConvexLensHexBound;
-    print_size("BiConvexLensHexBound", biConvexLensHexBound);
+    out << print_size("BiConvexLensHexBound", biConvexLensHexBound);
 
     ml::Cylinder cylinder;
-    print_size("Cylinder", cylinder);
+    out << print_size("Cylinder", cylinder);
 
     ml::Disc disc;
-    print_size("Disc", disc);
+    out << print_size("Disc", disc);
 
     ml::EllipticalCapWithHexagonalBound ellipticalCapWithHexagonalBound;
-    print_size(
+    out << print_size(
         "EllipticalCapWithHexagonalBound", ellipticalCapWithHexagonalBound);
 
     ml::HexPlane hexPlane;
-    print_size("HexPlane", hexPlane);
+    out << print_size("HexPlane", hexPlane);
 
     ml::Plane plane;
-    print_size("Plane", plane);
+    out << print_size("Plane", plane);
 
     ml::PlaneDualSphericalBound planeDualSphericalBound;
-    print_size("PlaneDualSphericalBound", planeDualSphericalBound);
+    out << print_size("PlaneDualSphericalBound", planeDualSphericalBound);
 
     ml::Sphere sphere;
-    print_size("Sphere", sphere);
+    out << print_size("Sphere", sphere);
 
     ml::SphereCapWithCylinderBound sphereCapWithCylinderBound;
-    print_size("SphereCapWithCylinderBound", sphereCapWithCylinderBound);
+    out << print_size(
+        "SphereCapWithCylinderBound",
+        sphereCapWithCylinderBound);
 
     ml::SphereCapWithHexagonalBound sphereCapWithHexagonalBound;
-    print_size("SphereCapWithHexagonalBound", sphereCapWithHexagonalBound);
+    out << print_size(
+        "SphereCapWithHexagonalBound",
+        sphereCapWithHexagonalBound);
 
     ml::Triangle triangle;
-    print_size("Triangle", triangle);
-    std::cout << "--------------------------------------------" << "\n";
+    out << print_size("Triangle", triangle);
+    out << "\n";
 
     ml::sensor::PhotonArrival arr_info;
-    print_size("PhotonArrival", arr_info);
+    out << print_size("PhotonArrival", arr_info);
 
     ml::sensor::Sensor sensor(0u, &tree);
-    print_size("Sensor", sensor);
+    out << print_size("Sensor", sensor);
 
     std::vector<ml::sensor::Sensor*> raw_sensors;
     ml::sensor::Sensors sensors(raw_sensors);
-    print_size("Sensors", sensors);
+    out << print_size("Sensors", sensors);
 
     ml::sensor::FindSensorByFrame finder(&tree, &sensors.by_frame);
-    print_size("FindSensorByFrame", finder);
-    std::cout << "--------------------------------------------" << "\n";
+    out << print_size("FindSensorByFrame", finder);
+    out << "\n";
 
     signal_processing::PipelinePhoton pipe_ph;
-    print_size("PipelinePhoton", pipe_ph);
+    out << print_size("PipelinePhoton", pipe_ph);
+
+    ml::write_text_to_file(out.str(), "merlict_tests/sizeof.txt");
 }
