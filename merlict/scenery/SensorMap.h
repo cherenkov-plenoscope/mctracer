7ae286818795269a4bb3c2e87d1e5ea9369c1f59
Sebastian Achim Mueller <sebmuell@phys.ethz.ch>
2019-04-10 19:00:18 +0200

    the former policy of strictly ordered sensor-ids is not acceptable. Users have to be able to choose sensor-ids freely.

--- merlict/scenery/SensorMap.h
+++ merlict/scenery/SensorMap.h
@@ -11,8 +11,14 @@
 namespace merlict {
 
 struct SensorMap: public ResourceMap {
+    std::map<uint64_t, sensor::Sensor*> sensor_map;
+    std::vector<sensor::Sensor> sensors_storage;
     std::vector<sensor::Sensor*> sensors;
     void add(const uint64_t key, const Frame* frame);
+    bool has(const uint64_t key)const;
+    sensor::Sensor* get(const uint64_t key);
+    void assert_has(const uint64_t key)const;
+    void assert_not_in_use_yet(const uint64_t key);
     ~SensorMap();
 };
 
